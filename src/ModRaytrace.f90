c87ccff2dd13b9a91f675aba6579d5ca39b58a19
gtoth <>
2004-03-31 20:50:39 +0000

    New subroutine xyz_to_latlon converts xyz to latitude,longitude (degrees)
    Number of ray integrals is now 9.
    Index Closed_ is replaced with xEnd_,yEnd_,zEnd_ (ending position)
    
    RayResult_VII contains the sum of the RayIntegral_VII arrays.

--- src/ModRaytrace.f90
+++ src/ModRaytrace.f90
@@ -74,21 +74,65 @@ module ModRaytrace
 
   ! Name indexes
   integer, parameter :: &
-       ClosedRay_ = 0, InvB_=1, Z0x_=2, Z0y_=3, Z0b_=4, RhoInvB_=5, pInvB_=6 
+       InvB_=1, Z0x_=2, Z0y_=3, Z0b_=4, RhoInvB_=5, pInvB_=6, &
+       xEnd_=7, yEnd_=8, zEnd_=9
 
   ! Number of integrals
-  integer, parameter :: nRayIntegral = 6
+  integer, parameter :: nRayIntegral = 9
 
   ! Flow variables to be integrated (rho and P) other than the magnetic field
   real, dimension(2,-1:nI+2,-1:nJ+2,-1:nK+2,nBLK) :: Extra_VGB
 
   ! Integrals for a local ray segment
-  real :: RayIntegral_V(nRayIntegral)
+  real :: RayIntegral_V(InvB_:pInvB_)
 
   ! Integrals added up for all the local ray segments
   ! The fist index corresponds to the variables (index 0 shows closed vs. open)
   ! The second and third indexes correspond to the latitude and longitude of
   ! the IM/RCM grid
   real, allocatable :: RayIntegral_VII(:,:,:)
+  real, allocatable :: RayResult_VII(:,:,:)
+
+contains
+
+  subroutine xyz_to_latlon(Pos_D)
+
+    use ModNumConst, ONLY: cTiny, cRadToDeg
+
+    ! Convert xyz coordinates to latitude and longitude (in degrees)
+    ! Put the latitude and longitude into the 1st and 2nd elements
+    real, intent(inout) :: Pos_D(3)
+
+    real :: x, y, z
+
+    !--------------------------------------------------------------------------
+
+    ! Check if this direction is closed or not
+    if(Pos_D(1) > CLOSEDRAY)then
+
+       ! Store input coordinates
+       x = Pos_D(1); y = Pos_D(2); z = Pos_D(3)
+
+       ! Make sure that asin will work, -1<= z <=1
+       z = max(-1.0+cTiny, z)
+       z = min( 1.0-cTiny, z)
+
+       ! Calculate  -90 < latitude = asin(z)  <  90
+       Pos_D(1) = cRadToDeg * asin(z)
+
+       ! Calculate -180 < longitude = atan2(y,x) < 180
+       if(abs(x) < cTiny .and. abs(y) < cTiny) x = 1.0
+       Pos_D(2) = cRadToDeg * atan2(y,x)
+
+       ! Get rid of negative longitude angles
+       if(Pos_D(2) < 0.0) Pos_D(2) = Pos_D(2) + 360.0
+
+    else
+       ! Impossible values
+       Pos_D(1) = -100.
+       Pos_D(2) = -200.
+    endif
+
+  end subroutine xyz_to_latlon
 
 end module ModRaytrace
