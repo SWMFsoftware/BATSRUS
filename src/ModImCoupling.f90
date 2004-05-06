c5b479b849284ad7bb72417132dd95dedb585561
gtoth <>
2004-05-06 03:25:28 +0000

    Added subroutine apply_im_pressure (based on a similar subroutine which was in update_states_MHD)
    Other minor changes: capitalization, using ModUitilities::check_allocate,
                         simpler logic for closed field lines.

--- src/ModImCoupling.f90
+++ src/ModImCoupling.f90
@@ -3,9 +3,17 @@
 !==========================================================================
 module ModImPressure
 
-  integer :: isize,jsize
+  use ModUtilities, ONLY: check_allocate
 
-  real, dimension(:), allocatable :: RCM_lat, RCM_lon
+  save
+
+  ! The number of IM pressures obtained so far
+  integer :: iNewPIm = 0
+
+  ! The size of the IM grid
+  integer :: iSize, jSize
+
+  real, dimension(:), allocatable   :: RCM_lat, RCM_lon
   real, dimension(:,:), allocatable :: RCM_p
 
 contains
@@ -18,67 +26,139 @@ contains
     allocate(RCM_lat(iSize), RCM_lon(jSize), RCM_p(iSize,jSize), &
          stat=iError)
 
-    RCM_p = -1.0
-
-    if(iError/=0) call stop_mpi('GM:im_pressure_init could not allocate')
+    call check_allocate(iError, 'GM_im_pressure_init: RCM arrays')
 
   end subroutine im_pressure_init
 
 end module ModImPressure
+
 !==========================================================================
-subroutine get_im_pressure(iBLK,pIM)
+
+subroutine get_im_pressure(iBlock, pIm_C)
 
   use ModImPressure
-  use ModMain, ONLY : nI,nJ,nK,gcn
+  use ModMain,     ONLY : nI,nJ,nK,gcn
   use ModRaytrace, ONLY : ray
-  use ModPhysics, ONLY : unitSI_p
+  use ModPhysics,  ONLY : unitSI_p
   implicit none
 
-  integer, intent(in) :: iBLK
-  real, dimension(1-gcn:nI+gcn, 1-gcn:nJ+gcn, 1-gcn:nK+gcn), intent(out) &
-       :: pIM
+  integer, intent(in)  :: iBlock
+  real,    intent(out) :: pIm_C(1:nI, 1:nJ, 1:nK)
 
   integer :: i,j,k, n, i1,i2
 
   real :: rLat,rLon
   !--------------------------------------------------------------------------
-
-  pIM = -1.
-
-  ! If not allocated then there is no pressure obtained from IM yet
-  ! Return with negative pressure, which will be ignored
-  if(.not.allocated(RCM_p)) RETURN
-
   !\
   ! Check to see if cell centers are on closed fieldline
   !/
-  if(any(ray(3,1,1:nI,1:nJ,1:nK,iBLK)==3.))then
-     do i=1,nI; do j=1,nJ; do k=1,nK
-        if(ray(3,1,i,j,k,iBLK)==3.) then
-           rLat=ray(1,1,i,j,k,iBLK)
-           rLon=ray(2,1,i,j,k,iBLK)
+  do k=1,nK; do j=1,nJ; do i=1,nI
+     if(ray(3,1,i,j,k,iBlock)==3.) then
+        rLat=ray(1,1,i,j,k,iBlock)
+        rLon=ray(2,1,i,j,k,iBlock)
 
-           !NOTE: RCM_lat in decending order
-           i1=1
-           do n=2,isize
-              if(rLat<0.5*(RCM_lat(n-1)+RCM_lat(n)))then
-                 i1=n
-              end if
-           end do
+        !NOTE: RCM_lat in decending order
+        i1=1
+        do n=2,isize
+           if(rLat < 0.5*(RCM_lat(n-1)+RCM_lat(n))) i1 = n
+        end do
 
-           !NOTE: RCM_lon in accending order
-           i2=1
-           do n=2,jsize
-              if(rLon>0.5*(RCM_lon(n-1)+RCM_lon(n)))then
-                 i2=n
-              end if
-           end do
-           if(rLon>0.5*(RCM_lon(jsize)+RCM_lon(1)+360.)) i2=1
+        !NOTE: RCM_lon in accending order
+        i2=1
+        do n=2,jsize
+           if(rLon > 0.5*(RCM_lon(n-1)+RCM_lon(n))) i2 = n
+        end do
+        if(rLon > 0.5*(RCM_lon(jsize)+RCM_lon(1)+360.)) i2=1
 
-           pIM(i,j,k) = RCM_p(i1,i2)/unitSI_p
-        end if
-     end do; end do; end do
-  end if
+        pIm_C(i,j,k) = RCM_p(i1,i2)/unitSI_p
+     else
+        pIm_C(i,j,k) = -1.0
+     end if
+  end do; end do; end do
 
 end subroutine get_im_pressure
+
 !==========================================================================
+
+subroutine apply_im_pressure
+
+  use ModProcMH,  ONLY: iProc
+  use ModMain,    ONLY: nI, nJ, nK, nBlock, iNewGrid, TauCoupleIm, unusedBLK
+  use ModAdvance, ONLY: time_BLK, &
+       State_VGB, Rho_, RhoUx_, RhoUy_, RhoUz_, Bx_, By_, Bz_, p_, E_BLK
+  use ModPhysics, ONLY: unitSI_t, inv_gm1
+  use ModImPressure
+
+  implicit none
+
+  real :: Factor
+  real :: pIm_C(1:nI, 1:nJ, 1:nK)
+
+  integer :: iLastPIm = -1, iLastGrid = -1
+
+  integer :: iBlock
+  !----------------------------------------------------------------------------
+  if(iNewPIm < 1) RETURN ! No IM pressure has been obtained yet
+
+  ! redo ray tracing if necessary 
+  ! (load_balance takes care of new decomposition)
+  if(iNewPIm > iLastPIm .or. iNewGrid > iLastGrid) then
+     if(iProc==0)write(*,*)'GM_apply_im_pressure: call ray_trace ',&
+          'iNewPIm,iLastPIm,iNewGrid,iLastGrid=',&
+          iNewPIm,iLastPIm,iNewGrid,iLastGrid
+     call ray_trace
+  end if
+
+  ! Remember this call
+  iLastPIm = iNewPIm; iLastGrid = iNewGrid
+
+  ! Now use the pressure from the IM to nudge the pressure in the MHD code.  
+  ! This will happen only on closed magnetic field lines.
+  ! Determining which field lines are closed is done by using the ray
+  ! tracing.
+
+  if(TauCoupleIm < 1.0)then
+     ! Ramp up is based on number of iterations: p' = (p + tau*pIm)/(1+tau)
+     ! A typical value might be 0.01, to get close to the RCM pressure 
+     ! in 200 iterations
+     
+     Factor = 1.0/(1.0+TauCoupleIM)
+
+  else
+     ! Ramp up is based on physical time: p' = p + dt/tau * (pIM - p)
+     ! A typical value might be 5, to get close to the RCM pressure 
+     ! in 10 seconds
+
+     Factor = 1.0/(tauCoupleIM/unitSI_t)
+  end if
+
+  do iBlock = 1, nBlock
+     if(unusedBLK(iBlock)) CYCLE
+
+     call get_im_pressure(iBlock, pIm_C)
+
+     if(TauCoupleIm < 1.0)then
+        where(pIm_C > 0.0) &
+             State_VGB(P_,1:nI,1:nJ,1:nK,iBlock) = (1.0/(1.0+TauCoupleIM))* &
+             (State_VGB(P_,1:nI,1:nJ,1:nK,iBlock) + TauCoupleIM*pIm_C)
+     else
+        where(pIm_C > 0.0) &
+             State_VGB(P_,1:nI,1:nJ,1:nK,iBlock) = &
+             State_VGB(P_,1:nI,1:nJ,1:nK,iBlock)   &
+             + min(1.0, Factor * time_BLK(1:nI,1:nJ,1:nK,iBlock)) &
+             * (pIm_C - State_VGB(P_,1:nI,1:nJ,1:nK,iBlock))
+     end if
+
+     ! Now get the energy that corresponds to this new pressure
+     E_BLK(1:nI,1:nJ,1:nK,iBlock) = &
+          inv_gm1*State_VGB(P_,1:nI,1:nJ,1:nK,iBlock) + 0.5*( &
+          ( State_VGB(rhoUx_,1:nI,1:nJ,1:nK,iBlock)**2 &
+          + State_VGB(rhoUy_,1:nI,1:nJ,1:nK,iBlock)**2 &
+          + State_VGB(rhoUz_,1:nI,1:nJ,1:nK,iBlock)**2 &
+          )/State_VGB(rho_,1:nI,1:nJ,1:nK,iBlock)  &
+          + State_VGB(Bx_,1:nI,1:nJ,1:nK,iBlock)**2 &
+          + State_VGB(By_,1:nI,1:nJ,1:nK,iBlock)**2 &
+          + State_VGB(Bz_,1:nI,1:nJ,1:nK,iBlock)**2)
+  end do
+
+end subroutine apply_im_pressure
