bfb25a0819caaab3c3173c2073609a883c27e293
oferc <>
2005-01-24 22:05:52 +0000

    Moved original transformations from set_BCs.f90 to here. Added transformations relevant for the case SC and IH in different coordinate systems.

--- srcInterface/ModBuffer.f90
+++ srcInterface/ModBuffer.f90
@@ -40,17 +40,44 @@ contains
     end if
   end subroutine set_spher_buffer_grid
 end module ModBuffer
+
 !=============================================================!
+
 subroutine get_from_spher_buffer_grid(Xyz_D,nVar,State_V)
   use ModBuffer
-  use ModMain,ONLY:nDim,R_,Phi_,Theta_,x_,y_,z_
+  use ModMain,       ONLY: nDim, R_, Phi_, Theta_, x_, y_, z_,&
+       TypeCoordSystem, Time_Simulation
+  use ModAdvance,    ONLY: Rho_, RhoUx_, RhoUz_, Ux_, Uz_, Bx_, Bz_, p_
+  use CON_coupler,   ONLY: SC_, Grid_C
+  use CON_axes,      ONLY: transform_matrix, transform_velocity
+  use ModPhysics,    ONLY: UnitSI_rho,UnitSI_U,UnitSI_B,UnitSI_p,UnitSI_X
+
   implicit none
   integer,intent(in)::nVar
   real,intent(in)::Xyz_D(nDim)
   real,dimension(nVar),intent(out)::State_V
   real,dimension(nDim)::Sph_D
-  call xyz_to_spherical(Xyz_D(x_),Xyz_D(y_),Xyz_D(z_),&
+
+  character (len=3) :: TypeCoordSc
+  real              :: ScIh_DD(nDim, nDim), XyzSc_D(nDim)
+  !---------------------------------------------------------------------------
+
+  TypeCoordSc = Grid_C(SC_) % TypeCoord
+
+  if(TypeCoordSc /= TypeCoordSystem) then
+     ! Convert IH coordinates to SC coordinates
+     ScIh_DD = transform_matrix(Time_Simulation, TypeCoordSystem, TypeCoordSc)
+
+     XyzSc_D = matmul(ScIh_DD, Xyz_D)
+  else
+     XyzSc_D = Xyz_D
+  end if
+
+  ! Convert to left handed spherical coordinates !!!
+  call xyz_to_spherical(XyzSc_D(x_),XyzSc_D(y_),XyzSc_D(z_),&
        Sph_D(R_),Sph_D(Phi_),Sph_D(Theta_))
+
+  ! Get the SC state from the spherical buffer grid
   State_V=point_state_v(&
        NameBuffer,&
        nVar,    &
@@ -58,6 +85,24 @@ subroutine get_from_spher_buffer_grid(Xyz_D,nVar,State_V)
        Sph_D,   &
        LocalBufferGD,&
        bilinear_interpolation)
+
+  !Transform to primitive variables
+  State_V(Ux_:Uz_) = State_V(rhoUx_:rhoUz_)/State_V(rho_)
+
+  ! Transform vector variables from SC to IH
+  if(TypeCoordSc /= TypeCoordSystem)then
+     State_V(Ux_:Uz_) = transform_velocity(Time_Simulation,&
+          State_V(Ux_:Uz_), XyzSc_D * UnitSI_X, &
+          TypeCoordSc, TypeCoordSystem)
+     State_V(Bx_:Bz_) = matmul( State_V(Bx_:Bz_), ScIh_DD)
+  end if
+
+  !Convert from SI units to normalized units
+  State_V(rho_)      = State_V(rho_)   /UnitSI_rho
+  State_V(Ux_:Uz_)   = State_V(Ux_:Uz_)/UnitSI_U
+  State_V(Bx_:Bz_)   = State_V(Bx_:Bz_)/UnitSI_B
+  State_V(P_)        = State_V(P_)     /UnitSI_P
+
 end subroutine get_from_spher_buffer_grid
           
   
