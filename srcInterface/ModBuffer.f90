dd6cf7bf2b58f96e8dae1e48a8b708ad12a0480c
igorsok <>
2005-06-18 03:13:26 +0000

    All coordinate and vector transformations are removed: the Buffer is a local grid of a model and automatically all the vectors and coordinates are provided in the coordinate system of the given model.  Modified Files:  	GM/BATSRUS/srcInterface/ModBuffer.f90

--- srcInterface/ModBuffer.f90
+++ srcInterface/ModBuffer.f90
@@ -45,11 +45,8 @@ end module ModBuffer
 
 subroutine get_from_spher_buffer_grid(Xyz_D,nVar,State_V)
   use ModBuffer
-  use ModMain,       ONLY: nDim, R_, Phi_, Theta_, x_, y_, z_,&
-       TypeCoordSystem, Time_Simulation
+  use ModMain,       ONLY: nDim, R_, Phi_, Theta_, x_, y_, z_
   use ModAdvance,    ONLY: Rho_, RhoUx_, RhoUz_, Ux_, Uz_, Bx_, Bz_, p_
-  use CON_coupler,   ONLY: SC_, Grid_C
-  use CON_axes,      ONLY: transform_matrix, transform_velocity
   use ModPhysics,    ONLY: UnitSI_rho,UnitSI_U,UnitSI_B,UnitSI_p,UnitSI_X
 
   implicit none
@@ -58,26 +55,13 @@ subroutine get_from_spher_buffer_grid(Xyz_D,nVar,State_V)
   real,dimension(nVar),intent(out)::State_V
   real,dimension(nDim)::Sph_D
 
-  character (len=3) :: TypeCoordSc
-  real              :: ScIh_DD(nDim, nDim), XyzSc_D(nDim)
   !---------------------------------------------------------------------------
 
-  TypeCoordSc = Grid_C(SC_) % TypeCoord
-
-  if(TypeCoordSc /= TypeCoordSystem) then
-     ! Convert IH coordinates to SC coordinates
-     ScIh_DD = transform_matrix(Time_Simulation, TypeCoordSystem, TypeCoordSc)
-
-     XyzSc_D = matmul(ScIh_DD, Xyz_D)
-  else
-     XyzSc_D = Xyz_D
-  end if
-
   ! Convert to left handed spherical coordinates !!!
-  call xyz_to_spherical(XyzSc_D(x_),XyzSc_D(y_),XyzSc_D(z_),&
+  call xyz_to_spherical(Xyz_D(x_),Xyz_D(y_),Xyz_D(z_),&
        Sph_D(R_),Sph_D(Phi_),Sph_D(Theta_))
 
-  ! Get the SC state from the spherical buffer grid
+  ! Get the state from the spherical buffer grid
   State_V=point_state_v(&
        NameBuffer,&
        nVar,    &
@@ -88,14 +72,6 @@ subroutine get_from_spher_buffer_grid(Xyz_D,nVar,State_V)
 
   !Transform to primitive variables
   State_V(Ux_:Uz_) = State_V(rhoUx_:rhoUz_)/State_V(rho_)
-
-  ! Transform vector variables from SC to IH
-  if(TypeCoordSc /= TypeCoordSystem)then
-     State_V(Ux_:Uz_) = transform_velocity(Time_Simulation,&
-          State_V(Ux_:Uz_), XyzSc_D * UnitSI_X, &
-          TypeCoordSc, TypeCoordSystem)
-     State_V(Bx_:Bz_) = matmul( State_V(Bx_:Bz_), ScIh_DD)
-  end if
 
   !Convert from SI units to normalized units
   State_V(rho_)      = State_V(rho_)   /UnitSI_rho
