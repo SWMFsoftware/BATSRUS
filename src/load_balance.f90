31894800281f3c45632e1d4f2742124f643a3800
gtoth <>
2005-08-13 00:21:52 +0000

    This is a major rewrite. Replaced usage of implicitBLK and implicitBlock_BP arrays with iTypeAdvance_B and iTypeAdvance_BP. Generalized to load balance separately an arbitrary number of block types. Currently the maximum number of block types is 2, and they are distingished by iTypeAdvance exceeding or not iTypeAdvanceLimit. The iTypeAdvanceLimit is set based on the UsePartSteady and UsePartImplicit variables. The iTypeAdvance_B and iTypeAdvance_BP variables are initialized here. They are also modified in select_stepping and moved with the block during load balancing. Always call init_conservative_facefluxes. Eliminated a number of MPI_allgather and MPI_allreduce calls. Added a lot of testing info added to select_stepping.

--- src/load_balance.f90
+++ src/load_balance.f90
@@ -2,7 +2,10 @@
 subroutine load_balance(DoMoveCoord, DoMoveData, nBlockMoved)
   use ModProcMH
   use ModMain
-  use ModImplicit, ONLY : implicitBLK, UsePartImplicit !^CFG IF IMPLICIT
+  use ModImplicit, ONLY : UsePartImplicit !^CFG IF IMPLICIT
+  use ModAdvance, ONLY: iTypeAdvance_B, iTypeAdvance_BP, &
+       SkippedBlock_, SteadyBlock_, SteadyBoundBlock_, ExplBlock_, ImplBlock_
+  use ModPartSteady, ONLY: UsePartSteady
   use ModAMR, ONLY : availableBLKs
   use ModParallel
   use ModIO
@@ -26,9 +29,21 @@ subroutine load_balance(DoMoveCoord, DoMoveData, nBlockMoved)
   ! There is another declaration in subroutine move_block! Change together!!!
   logical, parameter :: DoMoveExtraData = .true.
 
+  ! Maximum number of block types to be load balanced separately
+  integer, parameter :: MaxBlockType = 2
+
+  ! Index for block type
+  integer :: iType
+
+  ! Global index for the various block types
+  integer :: iBlockALL_I(MaxBlockType)
+
+  ! Number of blocks for each type
+  integer :: nBlockALL_I(MaxBlockType)
+
   integer :: iError
   integer :: iBlockALL, iBlock
-  integer :: iBlockExplALL, iBlockImplALL     !^CFG IF IMPLICIT
+  integer :: iTypeAdvanceLimit
 
   integer :: iBlockFrom, iProcFrom, iBlockTo, iProcTo, iTry
 
@@ -41,8 +56,8 @@ subroutine load_balance(DoMoveCoord, DoMoveData, nBlockMoved)
   !---------------------------------------------------------------------------
   call set_oktest('load_balance',DoTest,DoTestMe)
 
-  if (DoTestMe) write(*,*)'load_balance: DoMoveCoord, DoMoveData=',&
-       DoMoveCoord, DoMoveData
+  if (DoTest) write(*,*)'load_balance: iProc, DoMoveCoord, DoMoveData=',&
+       iProc, DoMoveCoord, DoMoveData
 
   ! starting value of number of blocks moved between processors
   nBlockMoved = 0
@@ -56,6 +71,31 @@ subroutine load_balance(DoMoveCoord, DoMoveData, nBlockMoved)
   if(DoMoveData.and. .not.DoMoveCoord)call stop_mpi(&
        'ERROR in load_balance: DoMoveData=T and DoMoveCoord=F !!!')
 
+  ! Set the separator between the block types to be load balanced separately
+  if(UsePartSteady)then
+     ! Type1 is steady boundary + explicit, Type2 is steady
+     iTypeAdvanceLimit = SteadyBlock_
+  elseif(UsePartImplicit)then                !^CFG IF IMPLICIT
+     ! Type1 is implicit, type2 is explicit  !^CFG IF IMPLICIT
+     iTypeAdvanceLimit = ExplBlock_          !^CFG IF IMPLICIT
+  else
+     ! Type1 is explicit or implicit (for fully implicit), Type2 is empty
+     iTypeAdvanceLimit = SkippedBlock_
+  endif
+
+  ! Set initial block advance types here 
+  ! if data is not yet associated with the blocks.
+  if(.not. DoMoveData)then
+     where(unusedBLK)
+        iTypeAdvance_B = SkippedBlock_
+     elsewhere
+        iTypeAdvance_B = ExplBlock_
+     endwhere
+     ! Gather global information (note: UnusedBlock_BP is not set yet!!!)
+     call MPI_ALLGATHER(iTypeAdvance_B, MaxBlock, MPI_INTEGER, &
+          iTypeAdvance_BP, MaxBlock, MPI_INTEGER, iComm, iError)
+  end if
+
   !^CFG IF IMPLICIT BEGIN
   !\
   ! Select blocks for implicit/local time stepping
@@ -66,22 +106,25 @@ subroutine load_balance(DoMoveCoord, DoMoveData, nBlockMoved)
   call select_stepping(DoMoveCoord)
   !^CFG END IMPLICIT
 
-  if(DoTestMe)write(*,*)'load_balance starting: nBlockMax=',nBlockMax
-  if(DoTest)write(*,*)'load_balance starting: me, nBlock, nBlockUsed=',&
-       iProc, nBlock, count(.not.unusedBLK(1:nBlock))
-  if(UseImplicit)then                              !^CFG IF IMPLICIT BEGIN
-     if(DoTestMe)write(*,*)'load_balance starting: ',&
-          'me, nBlockExplALL, nBlockImplALL=',&
-          iProc,nBlockExplALL, nBlockImplALL
-     if(DoTest)write(*,*)&
-          'load_balance starting: me, nBlockImpl=',&
-          iProc, count(implicitBLK(1:nBlock))
-  end if                                           !^CFG END IMPLICIT
+  ! Number of blocks of type 1 and type 2
+  nBlockALL_I(1) = count(iTypeAdvance_BP(1:nBlockMax,:) >  iTypeAdvanceLimit)
+  nBlockALL_I(2) = count(iTypeAdvance_BP(1:nBlockMax,:) <= iTypeAdvanceLimit &
+       .and.             iTypeAdvance_BP(1:nBlockMax,:) >  SkippedBlock_ )
 
-  !DEBUG
-  !write(*,*)'START me, nBlock, iBlockALL_B=',iProc, &
-  !     nBlock, (global_block_number(iBlock),unusedBLK(iBlock),&
-  !     iBlock=1,nBlock)
+
+  if(DoTestMe)write(*,*)'load_balance starting: nBlockMax=',nBlockMax
+  if(DoTest)then
+     write(*,*)'load_balance starting: me, nBlock, nBlockUsed=',&
+          iProc, nBlock, count(.not.unusedBLK(1:nBlock))
+     if(iTypeAdvanceLimit > SkippedBlock_) then
+        write(*,*)'load_balance iProc, MaxType, MinType=',iProc,&
+             maxval(iTypeAdvance_BP(1:nBlockMax,:)),&
+             minval(iTypeAdvance_BP(1:nBlockMax,:))
+        write(*,*)'load_balance starting iProc, nBlockALL_I=',iProc,nBlockALL_I
+        write(*,*)'load_balance starting iProc, nType1=',&
+             iProc,count(iTypeAdvance_B(1:nBlockMax) > iTypeAdvanceLimit)
+     end if
+  end if
 
   if (nProc==1) RETURN
 
@@ -94,7 +137,7 @@ subroutine load_balance(DoMoveCoord, DoMoveData, nBlockMoved)
   TRY: do iTry=1,MaxTry
 
      skippedAnyBlock=.false.
-     iBlockExplALL = 0; iBlockImplALL = 0           !^CFG IF IMPLICIT
+     iBlockALL_I = 0
 
      TREE: do iBlockALL  = 1, nBlockALL
 
@@ -103,13 +146,16 @@ subroutine load_balance(DoMoveCoord, DoMoveData, nBlockMoved)
 
         iProcTo = (nProc*iBlockALL-1)/nBlockALL
 
-        if(implicitBlock_BP(iBlockFrom,iProcFrom))then  !^CFG IF IMPLICIT BEGIN
-           iBlockImplALL = iBlockImplALL + 1
-           iProcTo = (nProc*iBlockImplALL-1) / nBlockImplALL
+        ! Figure out the block type
+        if(iTypeAdvance_BP(iBlockFrom,iProcFrom) > iTypeAdvanceLimit)then
+           iType = 1
         else
-           iBlockExplALL = iBlockExplALL + 1
-           iProcTo = (nProc*iBlockExplALL-1) / nBlockExplALL
-        end if                                          !^CFG END IMPLICIT
+           iType = 2
+        endif
+
+        ! Increase the index for this block type and select target processor
+        iBlockALL_I(iType) = iBlockALL_I(iType) + 1
+        iProcTo = (nProc*iBlockALL_I(iType)-1) / nBlockALL_I(iType)
 
         !DEBUG
         !if(iProc==0)write(*,*)'iBlockALL,iBlockFrom,iProcFrom,iProcTo=',&
@@ -162,9 +208,7 @@ subroutine load_balance(DoMoveCoord, DoMoveData, nBlockMoved)
         if(DoMoveExtraData)then
            call set_b0_matrix(iBlock)                 !^CFG IF CARTESIAN
            !call calc_b0source_covar(iBlock)          !^CFG IF NOT CARTESIAN
-           
-           if(UsePartImplicit)&                            !^CFG IF IMPLICIT
-                call init_conservative_facefluxes(iBlock)  !^CFG IF IMPLICIT
+           call init_conservative_facefluxes(iBlock)
         else
            call calc_other_soln_vars(iBlock)
         end if
@@ -180,11 +224,12 @@ subroutine load_balance(DoMoveCoord, DoMoveData, nBlockMoved)
 
   if(DoTest)write(*,*)&
        'load_balance finished: me, nBlock, nBlockUsed=',&
-       iProc, nBlock, count(.not.unusedBLK(1:nBlock))
+       iProc, nBlock, count(.not.unusedBLK(1:nBlock)), &
+       count(iTypeAdvance_B /= SkippedBlock_)
 
-  if(DoTest.and.UseImplicit)write(*,*)&               !^CFG IF IMPLICIT
-       'load_balance finished: me, nBlockImpl=',&     !^CFG IF IMPLICIT
-       iProc, count(implicitBLK(1:nBlock))         !^CFG IF IMPLICIT
+  if(DoTest .and. iTypeAdvanceLimit > SkippedBlock_) &
+       write(*,*)'load_balance finished: iProc, nType1=',&
+       iProc,count(iTypeAdvance_B(1:nBlockMax) > iTypeAdvanceLimit)
 
 end subroutine load_balance
 
@@ -205,7 +250,8 @@ subroutine move_block(DoMoveCoord, DoMoveData, iBlockALL, &
        B0xCell_BLK, B0yCell_BLK, B0zCell_BLK, &
        B0xFace_x_BLK, B0yFace_x_BLK, B0zFace_x_BLK, &
        B0xFace_y_BLK, B0yFace_y_BLK, B0zFace_y_BLK, &
-       B0xFace_z_BLK, B0yFace_z_BLK, B0zFace_z_BLK
+       B0xFace_z_BLK, B0yFace_z_BLK, B0zFace_z_BLK, &
+       iTypeAdvance_B, iTypeAdvance_BP, SkippedBlock_
   use ModGeometry, ONLY : dx_BLK,dy_BLK,dz_BLK,xyzStart_BLK
   use ModParallel
   use ModImplicit                                         !^CFG IF IMPLICIT
@@ -258,7 +304,7 @@ subroutine move_block(DoMoveCoord, DoMoveData, iBlockALL, &
   if (iProc == iProcFrom) then
      if (DoMoveCoord) call send_block_data
      unusedBLK(iBlockFrom)   = .true.
-     implicitBLK(iBlockFrom) = .false.      !^CFG IF IMPLICIT
+     iTypeAdvance_B(iBlockFrom) = SkippedBlock_
      do
         if (nBlock==0) EXIT
         if (.not.unusedBLK(nBlock)) EXIT
@@ -268,17 +314,16 @@ subroutine move_block(DoMoveCoord, DoMoveData, iBlockALL, &
 
   if (iProc == iProcTo) then
      unusedBLK(iBlockTo) = .false.
-     implicitBLK(iBlockTo) = implicitBlock_BP(iBlockFrom,iProcFrom) !^CFG IF IMPLICIT
+     iTypeAdvance_B(iBlockTo) = iTypeAdvance_BP(iBlockFrom,iProcFrom)
+
      if (DoMoveCoord) call recv_block_data
      nBlock = max(nBlock, iBlockTo)
      global_block_number(iBlockTo) = iBlockALL
   end if
 
-  !^CFG IF IMPLICIT BEGIN
-  ! Update global implicit block information
-  implicitBlock_BP(iBlockTo, iProcTo) = implicitBlock_BP(iBlockFrom,iProcFrom)
-  implicitBlock_BP(iBlockFrom, iProcFrom) = .false.
-  !^CFG END IMPLICIT
+  ! Update global advance type info
+  iTypeAdvance_BP(iBlockTo, iProcTo) = iTypeAdvance_BP(iBlockFrom,iProcFrom)
+  iTypeAdvance_BP(iBlockFrom,iProcFrom) = SkippedBlock_
 
   ! Fix pointers
   call move_octree_block(iBlockFrom,iProcFrom,iBlockTo,iProcTo)
@@ -591,10 +636,11 @@ subroutine select_stepping(DoPartSelect)
 
   use ModProcMH
   use ModMain
+  use ModAdvance,  ONLY : iTypeAdvance_B, iTypeAdvance_BP, &
+       SkippedBlock_, ExplBlock_, ImplBlock_
   use ModGeometry, ONLY : Rmin_BLK
   use ModImplicit, ONLY : UseFullImplicit,UsePartImplicit, &
-       implicitBLK,ImplCritType,explCFL,Rimplicit
-  use ModParallel, ONLY : implicitBlock_BP
+       ImplCritType, ExplCFL,rImplicit
   use ModMpi
   implicit none
 
@@ -606,29 +652,50 @@ subroutine select_stepping(DoPartSelect)
   !---------------------------------------------------------------------------
   call set_oktest('select_stepping',oktest,oktest_me)
 
-  if(oktest_me)write(*,*) 'select_stepping starting with ',&
-       'UseFullImplicit, UsePartImplicit, UsePartLocal, DoPartSelect=',&
-       UseFullImplicit, UsePartImplicit, UsePartLocal, DoPartSelect
+  if(oktest)then
+     write(*,*) 'select_stepping starting with iProc ',&
+          'UseFullImplicit, UsePartImplicit, UsePartLocal, DoPartSelect=',&
+          iProc, UseFullImplicit, UsePartImplicit, UsePartLocal, DoPartSelect
+     write(*,*)'select_stepping starting with ',&
+          'iProc,nBlockExpl, nBlockImpl, nBlockSkipped=', iProc, &
+          count(iTypeAdvance_B == ExplBlock_),&
+          count(iTypeAdvance_B == ImplBlock_),&
+          count(iTypeAdvance_B == SkippedBlock_)
+     write(*,*)'select_stepping starting with ',&
+          'iProc,nBlockExplALL, nBlockImplALL, nBlockSkippedALL=', iProc, &
+          count(iTypeAdvance_BP == ExplBlock_),&
+          count(iTypeAdvance_BP == ImplBlock_),&
+          count(iTypeAdvance_BP == SkippedBlock_)
+     write(*,*)'select_stepping starting with ',&
+          'iProc,min(advance),max(advance)=',iProc, &
+          minval(iTypeAdvance_B),maxval(iTypeAdvance_B)
+     write(*,*)'select_stepping starting with ',&
+          'iProc,min(advanceall),max(advanceall)=',iProc, &
+          minval(iTypeAdvance_BP),maxval(iTypeAdvance_BP)
+  end if
 
   if(((UsePartLocal .or. UsePartImplicit) .and. .not. DoPartSelect) &
        .or. .not. (UseImplicit .or. UsePartLocal))then
      nBlockExplALL    = nBlockALL
      nBlockImplALL    = 0
-     implicitBLK(1:nBlock)           = .false.
-     implicitBlock_BP(1:nBlockMax,:) = .false.
-
+     where(iTypeAdvance_BP(1:nBlockMax,:) == ImplBlock_) &
+          iTypeAdvance_BP(1:nBlockMax,:) = ExplBlock_
+     iTypeAdvance_B(1:nBlockMax) = iTypeAdvance_BP(1:nBlockMax,iProc)
+          
   elseif(UseFullImplicit)then
      nBlockExplALL = 0
      nBlockImplALL = nBlockALL
-     implicitBLK(1:nBlock)           = .not.unusedBLK(1:nBlock)
-
-     call MPI_ALLGATHER(implicitBLK,      nBLK, MPI_LOGICAL, &
-          implicitBlock_BP, nBLK, MPI_LOGICAL, &
-          iComm, iError)
+     where(iTypeAdvance_BP(1:nBlockMax,:) /= SkippedBlock_) &
+          iTypeAdvance_BP(1:nBlockMax,:) = ImplBlock_
+     iTypeAdvance_B(1:nBlockMax) = iTypeAdvance_BP(1:nBlockMax,iProc)
   else
 
      if(iProc==0.and.lVerbose>0)&
           write(*,*)'select_stepping: ImplCritType=',ImplCritType
+
+     ! First set all blocks to be explicit
+     where(iTypeAdvance_B(1:nBlockMax) /= SkippedBlock_) &
+          iTypeAdvance_B(1:nBlockMax) = ExplBlock_
 
      ! Select implicitly treated blocks
      select case(ImplCritType)
@@ -639,58 +706,67 @@ subroutine select_stepping(DoPartSelect)
 
         ! Set implicitBLK based on the time step.
         do globalBLK=1,nBlockMax
-           if(unusedBLK(globalBLK))then
-              implicitBLK(globalBLK)=.false.
-           else
-              ! Obtain the time step based on CFL condition
+           if(unusedBLK(globalBLK)) CYCLE
 
-              ! For first iteration calculate dt_BLK when inside time loop,
-              ! otherwise use the available dt_BLK from previous time step,
-              ! or from the restart file, or simply 0 set in read_inputs.
-              ! The latter two choices will be overruled later anyways.
-              if(iteration_number==0 .and. time_loop)then
-                 ! For first iteration in the time loop
-                 ! calculate stable time step
-                 call calc_facevalues(.false.)
-                 call calc_facefluxes(.false.)
-                 call calc_timestep
-              end if
-              ! If the smallest allowed timestep is below the fixed DtFixed
-              ! then only implicit scheme will work
-              implicitBLK(globalBLK) = dt_BLK(globalBLK)*explCFL <= DtFixed
+           ! Obtain the time step based on CFL condition
+
+           ! For first iteration calculate dt_BLK when inside time loop,
+           ! otherwise use the available dt_BLK from previous time step,
+           ! or from the restart file, or simply 0 set in read_inputs.
+           ! The latter two choices will be overruled later anyways.
+           if(iteration_number==0 .and. time_loop)then
+              ! For first iteration in the time loop
+              ! calculate stable time step
+              call calc_facevalues(.false.)
+              call calc_facefluxes(.false.)
+              call calc_timestep
            end if
+
+           ! If the smallest allowed timestep is below the fixed DtFixed
+           ! then only implicit scheme will work
+           if(dt_BLK(globalBLK)*explCFL <= DtFixed) &
+                iTypeAdvance_B(globalBLK) = ImplBlock_
         end do
 
         if(oktest_me)write(*,*)&
-             'SELECT: unused,implicit,dt_BLK,explCFL,dt=',&
-             unusedBLK(BLKtest),implicitBLK(BLKtest),dt_BLK(BLKtest),&
-             explCFL,dt
+             'SELECT: advancetype,dt_BLK,explCFL,dt=',&
+             iTypeAdvance_B(BLKtest),dt_BLK(BLKtest),explCFL,dt
 
      case('r','R')
-        ! implicitly treated blocks are within Rimplicit and not unused
-        implicitBLK(1:nBlockMax) = &
-             Rmin_BLK(1:nBlockMax) <= Rimplicit .and. .not.unusedBLK(1:nBlockMax)
+        ! implicitly treated blocks are within rImplicit and not Unused
+        where(rMin_BLK(1:nBlockMax) <= rImplicit .and. &
+             .not.UnusedBLK(1:nBlockMax)) &
+             iTypeAdvance_B(1:nBlockMax) = ImplBlock_
      case('test')
-        implicitBLK(1:nBlockMax) = .false.
-        if(iProc==PROCtest) implicitBLK(BLKtest) = .true.
+        if(iProc==PROCtest) iTypeAdvance_B(BLKtest) = ImplBlock_
      end select
 
-     nBlockImpl = count(implicitBLK(1:nBlock))
-     nBlockExpl = count(.not.(unusedBLK(1:nBlock).or.implicitBLK(1:nBlock)))
+     ! Gather global information
+     call MPI_ALLGATHER(iTypeAdvance_B, MaxBlock, MPI_INTEGER, &
+          iTypeAdvance_BP, MaxBlock, MPI_INTEGER, iComm, iError)
 
-     call MPI_allreduce(nBlockImpl, nBlockImplALL, 1, MPI_INTEGER, MPI_SUM, &
-          iComm, iError)
+     nBlockImplALL = count(iTypeAdvance_BP == ImplBlock_)
+     nBlockExplALL = count(iTypeAdvance_BP == ExplBlock_)
 
-     call MPI_allreduce(nBlockExpl, nBlockExplALL, 1, MPI_INTEGER, MPI_SUM, &
-          iComm, iError)
-
-     call MPI_ALLGATHER(implicitBLK,      nBLK, MPI_LOGICAL, &
-          implicitBlock_BP, nBLK, MPI_LOGICAL, &
-          iComm, iError)
      if(iProc==0.and.lVerbose>0)oktest_me=.true. ! report for part implicit
   end if
-  if(oktest_me)write(*,*)'select_stepping finished with ',&
-       'nBlockExplALL, nBlockImplALL=',nBlockExplALL, nBlockImplALL
-
+  if(oktest)then
+     write(*,*)'select_stepping finished with ',&
+          'iProc,nBlockExpl, nBlockImpl, nBlockSkipped=', iProc, &
+          count(iTypeAdvance_B == ExplBlock_),&
+          count(iTypeAdvance_B == ImplBlock_),&
+          count(iTypeAdvance_B == SkippedBlock_)
+     write(*,*)'select_stepping finished with ',&
+          'iProc,nBlockExplALL, nBlockImplALL, nBlockSkippedALL=', iProc, &
+          count(iTypeAdvance_BP == ExplBlock_),&
+          count(iTypeAdvance_BP == ImplBlock_),&
+          count(iTypeAdvance_BP == SkippedBlock_)
+     write(*,*)'select_stepping finished with ',&
+          'iProc,min(advance),max(advance)=',iProc, &
+          minval(iTypeAdvance_B),maxval(iTypeAdvance_B)
+     write(*,*)'select_stepping finished with ',&
+          'iProc,min(advanceall),max(advanceall)=',iProc, &
+          minval(iTypeAdvance_BP),maxval(iTypeAdvance_BP)
+  end if
 end subroutine select_stepping
 !^CFG END IMPLICIT
