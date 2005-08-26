c1b263580a09aa175c9eb5b2efe19c248979b4ea
gtoth <>
2005-08-26 04:14:16 +0000

    Removed set_block_geometry_obsolete Added separator lines, line breaks, spaces, fixed alignment.

--- src/set_block_geometry.f90
+++ src/set_block_geometry.f90
@@ -75,27 +75,9 @@ subroutine set_root_block_geometry
   availableBLKs(0,0)=nBlockMax+1
 
 end subroutine set_root_block_geometry
-
-subroutine set_block_geometry_obsolete(iBLK)
-  use ModGeometry, ONLY : dx_BLK,dy_BLK,dz_BLK,dxyz,xyzStart,xyzStart_BLK
-  implicit none
-
-  integer, intent(in) :: iBLK
-
-  !\
-  ! Set dx, dy, and dz for the new coarsened block.
-  !/
-  dx_BLK(iBLK) = dxyz(1)
-  dy_BLK(iBLK) = dxyz(2)
-  dz_BLK(iBLK) = dxyz(3)
-  xyzStart_BLK(:,iBLK)=xyzStart(:)
-
-  call fix_block_geometry(iBLK)
-
-end subroutine set_block_geometry_obsolete
-
-
+!==============================================================================
 subroutine fix_block_geometry(iBLK)
+
   use ModMain, ONLY : body1,body1_,body2_,ExtraBc_,&
        UseExtraBoundary,DoFixExtraBoundary,unusedBLK                       
   use ModMain, ONLY : UseBody2                       !^CFG IF SECONDBODY
@@ -111,13 +93,13 @@ subroutine fix_block_geometry(iBLK)
 
   integer :: i,j,k, iBoundary,MinVolumeCounter,MaxVolumeCounter,iVolumeCounter
   real :: dx,dy,dz,fAx,fAy,fAz,cV,VInv
-
+  !---------------------------------------------------------------------------
   dx = dx_BLK(iBLK)
   dy = dy_BLK(iBLK)
   dz = dz_BLK(iBLK)
 
-  select case(TypeGeometry)                                       !^CFG IF NOT CARTESIAN
-  case('cartesian')                                               !^CFG IF NOT CARTESIAN
+  select case(TypeGeometry)                        !^CFG IF NOT CARTESIAN
+  case('cartesian')                                !^CFG IF NOT CARTESIAN
      !\
      ! Assign cell centered coordinates.
      !/
@@ -135,7 +117,7 @@ subroutine fix_block_geometry(iBLK)
            end do
         end do
      end do
-     cV = dx*dy*dz                                                 !^CFG IF CARTESIAN BEGIN
+     cV = dx*dy*dz                                  !^CFG IF CARTESIAN BEGIN
 
      fAx = dy*dz       !Cell face areas
      fAy = dz*dx 
@@ -146,20 +128,19 @@ subroutine fix_block_geometry(iBLK)
      fAy_BLK(iBLK) = fAy
      fAz_BLK(iBLK) = fAz
      cV_BLK(iBLK) = cV                                            
-     VolumeInverse_I(iBLK) = cOne/cV                               !^CFG END CARTESIAN
-!     call set_covar_cartesian_geometry(iBLK)                      !^CFG IF NOT CARTESIAN 
-  case('spherical')                                                !^CFG IF NOT CARTESIAN 
-!     call set_covar_spherical_geometry(iBLK)                      !^CFG IF NOT CARTESIAN 
-  case('spherical_lnr')                                            !^CFG IF NOT CARTESIAN 
-!     call set_covar_spherical2_geometry(iBLK)                     !^CFG IF NOT CARTESIAN 
-  case('cylindrical')                                              !^CFG IF NOT CARTESIAN 
-!     call set_covar_cylindrical_geometry(iBLK)                    !^CFG IF NOT CARTESIAN
-  case default                                                     !^CFG IF NOT CARTESIAN BEGIN
+     VolumeInverse_I(iBLK) = cOne/cV              !^CFG END CARTESIAN
+!     call set_covar_cartesian_geometry(iBLK)     !^CFG IF NOT CARTESIAN 
+  case('spherical')                               !^CFG IF NOT CARTESIAN 
+!     call set_covar_spherical_geometry(iBLK)     !^CFG IF NOT CARTESIAN 
+  case('spherical_lnr')                           !^CFG IF NOT CARTESIAN 
+!     call set_covar_spherical2_geometry(iBLK)    !^CFG IF NOT CARTESIAN 
+  case('cylindrical')                             !^CFG IF NOT CARTESIAN 
+!     call set_covar_cylindrical_geometry(iBLK)   !^CFG IF NOT CARTESIAN
+  case default                                    !^CFG IF NOT CARTESIAN BEGIN
      call stop_mpi('Unknown TypeGeometry = '//TypeGeometry)
-  end select                                                       !^CFG END CARTESIAN 
+  end select                                      !^CFG END CARTESIAN 
 
-  !^CFG IF SECONDBODY BEGIN
-  if (.not. UseBody2) then   
+  if (.not. UseBody2) then                        !^CFG IF SECONDBODY BEGIN
      ! if no second body set R2 to zero
      R2_BLK(:,:,:,iBLK) = cZero
   else                    
@@ -168,11 +149,10 @@ subroutine fix_block_geometry(iBLK)
           (x_BLK(:,:,:,iBLK)-xBody2)**2 + &
           (y_BLK(:,:,:,iBLK)-yBody2)**2 + &
           (z_BLK(:,:,:,iBLK)-zBody2)**2)
-  end if
-  !^CFG END SECONDBODY
+  end if                                          !^CFG END SECONDBODY
 
   Rmin_BLK(iBLK)  = minval(R_BLK(:,:,:,iBLK))
-  Rmin2_BLK(iBLK) = minval(R2_BLK(:,:,:,iBLK))  !^CFG IF SECONDBODY
+  Rmin2_BLK(iBLK) = minval(R2_BLK(:,:,:,iBLK))    !^CFG IF SECONDBODY
 
   ! Compute node geometry
   !^CFG IF CARTESIAN BEGIN
@@ -182,49 +162,58 @@ subroutine fix_block_geometry(iBLK)
      NodeZ_IIIB(i,j,k,iBLK) = cEighth*sum(z_BLK(i:i+1,j:j+1,k:k+1,iBLK))
   end do; end do; end do
   !^CFG END CARTESIAN
-!  call calc_node_coords_covar            !^CFG IF NOT CARTESIAN
+  !call calc_node_coords_covar            !^CFG IF NOT CARTESIAN
 
   far_field_BCs_BLK(iBLK) = &
        (((xyzStart_BLK(1,iBLK)-dx_BLK(iBLK))<XyzMin_D(1).or.&
-       (xyzStart_BLK(1,iBLK)+nI*dx_BLK(iBLK))>XyzMax_D(1)).and..not.periodic3D(1)) .or. &
+       (xyzStart_BLK(1,iBLK)+nI*dx_BLK(iBLK))>XyzMax_D(1)) &
+       .and. .not.periodic3D(1)) .or. &
        (((xyzStart_BLK(2,iBLK)-dy_BLK(iBLK))<XyzMin_D(2).or.&
-       (xyzStart_BLK(2,iBLK)+nJ*dy_BLK(iBLK))>XyzMax_D(2)).and..not.periodic3D(2)).or. &
+       (xyzStart_BLK(2,iBLK)+nJ*dy_BLK(iBLK))>XyzMax_D(2)) &
+       .and. .not.periodic3D(2)).or. &
        (((xyzStart_BLK(3,iBLK)-dz_BLK(iBLK))<XyzMin_D(3).or.&
-       (xyzStart_BLK(3,iBLK)+nK*dz_BLK(iBLK))>XyzMax_D(3)).and..not.periodic3D(3))  
+       (xyzStart_BLK(3,iBLK)+nK*dz_BLK(iBLK))>XyzMax_D(3)) &
+       .and. .not.periodic3D(3))  
 
   !\
-  ! TRUE_CELL: if not inside a body
+  ! TRUE_CELL: if not inside a body or outside the outer face boundary
   !/
   true_cell(:,:,:,iBLK)=.true.
   IsBoundaryBlock_IB(:,iBLK)=.false.
-  do iBoundary=MinBoundary,MaxBoundary
-     IsBoundaryBlock_IB(iBoundary,iBLK)=.true.
+  do iBoundary = MinBoundary, MaxBoundary
+     IsBoundaryBlock_IB(iBoundary,iBLK) = .true.
   end do
   IsBoundaryBlock_IB(ExtraBc_,iBLK) = &
-       DoFixExtraBoundary &                                          !^CFG IF FACEOUTERBC BEGIN
-       .and.TypeGeometry=='cartesian' &                              !^CFG IF NOT CARTESIAN
-       .or. &                                                        !^CFG END FACEOUTERBC
+       DoFixExtraBoundary &                         !^CFG IF FACEOUTERBC BEGIN
+       .and.TypeGeometry=='cartesian' &             !^CFG IF NOT CARTESIAN
+       .or. &                                       !^CFG END FACEOUTERBC
        UseExtraBoundary
+
+  ! set true_cell array
   call set_boundary_cells(iBLK)
 
-  do iBoundary=MinBoundary,min(MaxBoundary,Body1_)
+  do iBoundary = MinBoundary, min(MaxBoundary,Body1_)
      IsBoundaryBlock_IB(iBoundary,iBLK)=any(IsBoundaryCell_GI(:,:,:,iBoundary))
-     true_cell(:,:,:,iBLK)=true_cell(:,:,:,iBLK).and..not.IsBoundaryCell_GI(:,:,:,iBoundary)
+     true_cell(:,:,:,iBLK) = &
+          true_cell(:,:,:,iBLK) .and. .not.IsBoundaryCell_GI(:,:,:,iBoundary)
   end do
 
   BodyFlg_B(iBLK) = .not. all(true_cell(:,:,:,iBLK))
-                                                                    !^CFG IF FACEOUTERBC BEGIN
-  if(TypeGeometry=='cartesian')     &                               !^CFG IF NOT CARTESIAN
-  DoFixExtraBoundary_B(iBLK)=DoFixExtraBoundary.and.any(IsBoundaryCell_GI(:,:,:,ExtraBc_))
-                                                                    !^CFG END FACEOUTERBC
-  IsBoundaryCell_GI(:,:,:,ExtraBc_)=UseExtraBoundary.and.IsBoundaryCell_GI(:,:,:,ExtraBc_)
+                                                    !^CFG IF FACEOUTERBC BEGIN
+  if(TypeGeometry=='cartesian')     &               !^CFG IF NOT CARTESIAN
+       DoFixExtraBoundary_B(iBLK) = DoFixExtraBoundary &
+       .and.any(IsBoundaryCell_GI(:,:,:,ExtraBc_))
+                                                    !^CFG END FACEOUTERBC
+  IsBoundaryCell_GI(:,:,:,ExtraBc_) = &
+       UseExtraBoundary .and. IsBoundaryCell_GI(:,:,:,ExtraBc_)
 
   do iBoundary=ExtraBc_,MaxBoundary
      IsBoundaryBlock_IB(iBoundary,iBLK)=any(IsBoundaryCell_GI(:,:,:,iBoundary))
-     true_cell(:,:,:,iBLK)=true_cell(:,:,:,iBLK).and..not.IsBoundaryCell_GI(:,:,:,iBoundary)
+     true_cell(:,:,:,iBLK) = true_cell(:,:,:,iBLK) &
+          .and. .not.IsBoundaryCell_GI(:,:,:,iBoundary)
   end do
   
-  BodyFlg_B(iBLK)= BodyFlg_B(iBLK).and.any(true_cell(:,:,:,iBLK))
+  BodyFlg_B(iBLK)= BodyFlg_B(iBLK) .and. any(true_cell(:,:,:,iBLK))
 
   !\
   ! body_BLK: if any cell INCLUDING ghost cells is inside body(ies)
@@ -234,53 +223,57 @@ subroutine fix_block_geometry(iBLK)
   ! TRUE_BLK: if all cells EXCLUDING ghost cells are outside body(ies)
   !/
   true_BLK(iBLK) = all(true_cell(1:nI,1:nJ,1:nK,iBLK))
+
 contains
+  !===========================================================================
   subroutine calc_node_coords_covar
     implicit none
-    real,dimension(3,3)::A_DD,A1_DD
-    real,dimension(3)::B_D
-    real::DetInv
-  do i=0,nI; do j=0,nJ; do k=0,nK
+    real,dimension(3,3):: A_DD, A1_DD
+    real,dimension(3)  :: B_D
+    real               :: DetInv
+    !------------------------------------------------------------------------
+    do i=0,nI; do j=0,nJ; do k=0,nK
 
-    A_DD(1,1)=x_BLK(i+1,j,k,iBLK)-x_BLK(i,j,k,iBLK)
-    A_DD(1,2)=y_BLK(i+1,j,k,iBLK)-y_BLK(i,j,k,iBLK)
-    A_DD(1,3)=z_BLK(i+1,j,k,iBLK)-z_BLK(i,j,k,iBLK)
+       A_DD(1,1)=x_BLK(i+1,j,k,iBLK)-x_BLK(i,j,k,iBLK)
+       A_DD(1,2)=y_BLK(i+1,j,k,iBLK)-y_BLK(i,j,k,iBLK)
+       A_DD(1,3)=z_BLK(i+1,j,k,iBLK)-z_BLK(i,j,k,iBLK)
 
-    A_DD(2,1)=x_BLK(i,j+1,k,iBLK)-x_BLK(i,j,k,iBLK)
-    A_DD(2,2)=y_BLK(i,j+1,k,iBLK)-y_BLK(i,j,k,iBLK)
-    A_DD(2,3)=z_BLK(i,j+1,k,iBLK)-z_BLK(i,j,k,iBLK)
+       A_DD(2,1)=x_BLK(i,j+1,k,iBLK)-x_BLK(i,j,k,iBLK)
+       A_DD(2,2)=y_BLK(i,j+1,k,iBLK)-y_BLK(i,j,k,iBLK)
+       A_DD(2,3)=z_BLK(i,j+1,k,iBLK)-z_BLK(i,j,k,iBLK)
 
-    A_DD(3,1)=x_BLK(i,j,k+1,iBLK)-x_BLK(i,j,k,iBLK)
-    A_DD(3,2)=y_BLK(i,j,k+1,iBLK)-y_BLK(i,j,k,iBLK)
-    A_DD(3,3)=z_BLK(i,j,k+1,iBLK)-z_BLK(i,j,k,iBLK)
+       A_DD(3,1)=x_BLK(i,j,k+1,iBLK)-x_BLK(i,j,k,iBLK)
+       A_DD(3,2)=y_BLK(i,j,k+1,iBLK)-y_BLK(i,j,k,iBLK)
+       A_DD(3,3)=z_BLK(i,j,k+1,iBLK)-z_BLK(i,j,k,iBLK)
 
-    DetInv=cOne/det(A_DD)
+       DetInv=cOne/det(A_DD)
 
-    B_D(1)=cHalf*(R_BLK(i+1,j,k,iBLK)-R_BLK(i,j,k,iBLK))*&
-                 (R_BLK(i+1,j,k,iBLK)+R_BLK(i,j,k,iBLK))
-    B_D(2)=cHalf*(R_BLK(i,j+1,k,iBLK)-R_BLK(i,j,k,iBLK))*&
-                 (R_BLK(i,j+1,k,iBLK)+R_BLK(i,j,k,iBLK))
-    B_D(3)=cHalf*(R_BLK(i,j,k+1,iBLK)-R_BLK(i,j,k,iBLK))*&
-                 (R_BLK(i,j,k+1,iBLK)+R_BLK(i,j,k,iBLK))
+       B_D(1)=cHalf*(R_BLK(i+1,j,k,iBLK)-R_BLK(i,j,k,iBLK))*&
+            (R_BLK(i+1,j,k,iBLK)+R_BLK(i,j,k,iBLK))
+       B_D(2)=cHalf*(R_BLK(i,j+1,k,iBLK)-R_BLK(i,j,k,iBLK))*&
+            (R_BLK(i,j+1,k,iBLK)+R_BLK(i,j,k,iBLK))
+       B_D(3)=cHalf*(R_BLK(i,j,k+1,iBLK)-R_BLK(i,j,k,iBLK))*&
+            (R_BLK(i,j,k+1,iBLK)+R_BLK(i,j,k,iBLK))
 
-    A1_DD(:,2:3)=A_DD(:,2:3)
-    A1_DD(:,1)=B_D
+       A1_DD(:,2:3)=A_DD(:,2:3)
+       A1_DD(:,1)=B_D
 
-    NodeX_IIIB(i,j,k,iBLK) = det(A1_DD)*DetInv
+       NodeX_IIIB(i,j,k,iBLK) = det(A1_DD)*DetInv
 
-    A1_DD(:,1)=A_DD(:,1)
-    A1_DD(:,3)=A_DD(:,3)
-    A1_DD(:,2)=B_D
+       A1_DD(:,1)=A_DD(:,1)
+       A1_DD(:,3)=A_DD(:,3)
+       A1_DD(:,2)=B_D
 
-    NodeY_IIIB(i,j,k,iBLK) = det(A1_DD)*DetInv
+       NodeY_IIIB(i,j,k,iBLK) = det(A1_DD)*DetInv
+       
+       A1_DD(:,1:2)=A_DD(:,1:2)
+       A1_DD(:,3)=B_D
 
-    A1_DD(:,1:2)=A_DD(:,1:2)
-    A1_DD(:,3)=B_D
-
-    NodeZ_IIIB(i,j,k,iBLK) = det(A1_DD)*DetInv
-  end do; end do; end do
+       NodeZ_IIIB(i,j,k,iBLK) = det(A1_DD)*DetInv
+    end do; end do; end do
     
   end subroutine calc_node_coords_covar
+  !===========================================================================
   real function det(A_DD)
     implicit none
     real,dimension(3,3),intent(in)::A_DD
@@ -292,9 +285,8 @@ contains
          A_DD(2,2)*A_DD(3,1))
   end function det
 
-
 end subroutine fix_block_geometry
-
+!=============================================================================
 subroutine set_xyzminmax_cart
   use ModGeometry, ONLY:x1,x2,y1,y2,z1,z2,XyzMin_D,XyzMax_D
   use ModMain,ONLY:x_,y_,z_
@@ -309,6 +301,7 @@ subroutine set_xyzminmax_cart
 end subroutine set_xyzminmax_cart
 
 !^CFG IF NOT CARTESIAN BEGIN
+!=============================================================================
 subroutine set_xyzminmax_sph
   use ModGeometry, ONLY: x1,x2,y1,y2,z1,z2,XyzMin_D,XyzMax_D
   use ModNumConst, ONLY: cZero,cTwoPI,cPi
@@ -322,6 +315,7 @@ subroutine set_xyzminmax_sph
   XyzMax_D(Phi_)   = cTwoPi
   XyzMax_D(Theta_) = cPi
 end subroutine set_xyzminmax_sph
+!=============================================================================
 subroutine set_xyzminmax_sph2
   use ModGeometry, ONLY: x1,x2,y1,y2,z1,z2,XyzMin_D,XyzMax_D
   use ModNumConst, ONLY: cZero,cTwoPI,cPi,cHalf
@@ -336,6 +330,7 @@ subroutine set_xyzminmax_sph2
   XyzMax_D(Phi_)   = cTwoPi
   XyzMax_D(Theta_) = cPi
 end subroutine set_xyzminmax_sph2
+!=============================================================================
 subroutine set_xyzminmax_cyl
   use ModGeometry, ONLY:x1,x2,y1,y2,z1,z2,XyzMin_D,XyzMax_D
   use ModNumConst, ONLY:cZero,cTwoPI
