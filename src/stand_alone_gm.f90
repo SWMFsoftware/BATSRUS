169c08f445d49a9f19efb844d6f8520fc6613a71
gtoth <>
2006-03-06 07:08:34 +0000

    Set NameThisComp and UseRotatingBc only, all else is done in main.f90.

--- src/stand_alone_gm.f90
+++ src/stand_alone_gm.f90
@@ -4,17 +4,11 @@ subroutine stand_alone
   ! Set defaults for the stand alone code.
   ! This version behaves like the GM component of SWMF.
 
-  use ModProcMH, ONLY: iProc
   use ModMain,   ONLY: NameThisComp, UseRotatingBc
-  use ModIO,     ONLY: NamePlotDir, NameRestartInDir, NameRestartOutDir
 
   implicit none
 
   NameThisComp  = 'GM'
   UseRotatingBc = .true.
-
-  NamePlotDir       = 'GM/'//NamePlotDir
-  NameRestartInDir  = 'GM/'//NameRestartInDir
-  NameRestartOutDir = 'GM/'//NameRestartOutDir
 
 end subroutine stand_alone
--- src/stand_alone_ih.f90
+++ src/stand_alone_ih.f90
@@ -4,21 +4,11 @@ subroutine stand_alone
   ! Set defaults for the stand alone code.
   ! This version behaves like the IH component of SWMF.
 
-  use ModProcMH, ONLY: iProc
-  use ModMain,   ONLY: NameThisComp, UseNewParam, UseNewAxes, UseRotatingBc,&
-       time_accurate
-  use ModIO,     ONLY: NamePlotDir, NameRestartInDir, NameRestartOutDir
+  use ModMain,   ONLY: NameThisComp, UseRotatingBc
 
   implicit none
 
   NameThisComp  = 'IH'
-  UseNewParam   = .true.
-  UseNewAxes    = .true.
-  time_accurate = .true.
   UseRotatingBc = .true.
-
-  NamePlotDir       = 'IH/'//NamePlotDir
-  NameRestartInDir  = 'IH/'//NameRestartInDir
-  NameRestartOutDir = 'IH/'//NameRestartOutDir
 
 end subroutine stand_alone
