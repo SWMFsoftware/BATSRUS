38daa055551f1ef6ee48f7adee278dcb5ba3ff31
gtoth <>
2007-03-27 14:54:32 +0000

    Fixed bugs (no call for allocate and deallocate) and made sure that Buffer_I is set on all processors.

--- srcInterface/GM_get_for_pw.f90
+++ srcInterface/GM_get_for_pw.f90
@@ -3,7 +3,7 @@ subroutine GM_get_for_pw(nTotalLine, Buffer_I)
   use ModVarIndexes, ONLY: p_
   use ModPwGrid,     ONLY: CoordXyzPw_DI
   use ModMain,       ONLY: nBlock
-  use ModProcMH,     ONLY: iComm
+  use ModProcMH,     ONLY: nProc, iComm
   use ModMpi
   use ModPhysics, ONLY: No2Si_V, UnitP_
   implicit none
@@ -14,21 +14,23 @@ subroutine GM_get_for_pw(nTotalLine, Buffer_I)
   integer             :: iLine,iError
   !----------------------------------------------------------------------------
 
-  call allocate(LocalBuffer_I(nTotalLine))
+  allocate(LocalBuffer_I(nTotalLine))
 
   !Get the pressure at each field line location.
   do iLine=1,nTotalLine
      call get_point_data(&
           0.0, CoordXyzPW_DI(:,iLine), 1, nBlock, p_, p_, LocalBuffer_I(iLine))
   enddo
+
+  Buffer_I = LocalBuffer_I
   
   ! Sum contributions from all PE-s to processor 0
-  call MPI_reduce(LocalBuffer_I, Buffer_I, nTotalLine,&
+  if(nProc > 1) call MPI_reduce(LocalBuffer_I, Buffer_I, nTotalLine,&
        MPI_REAL, MPI_SUM, 0, iComm, iError)
   
   ! Convert pressure to SI units
   Buffer_I = Buffer_I * No2Si_V(UnitP_)
    
-  call deallocate(LocalBuffer_I)
+  deallocate(LocalBuffer_I)
 
  end subroutine GM_get_for_pw
