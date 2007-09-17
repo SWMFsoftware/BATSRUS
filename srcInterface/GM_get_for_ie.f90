8590867e248da6f7a80fc7a4986da41044a58685
darrens <>
2007-09-17 18:41:06 +0000

    Small bug fix in commented code.

--- srcInterface/GM_get_for_ie.f90
+++ srcInterface/GM_get_for_ie.f90
@@ -240,7 +240,7 @@ subroutine GM_get_for_ie(Buffer_IIV,iSize,jSize,nVar,NameVar)
      if(nVar>1) then
         ! Load grid and convert to lat-lon in degrees
 ! This should be correct for southern hemisphere when ray tracing there works.
-!         IE_half_lat = 180.0 - cRadToDeg * ThetaIono_I(iSize:2*iSize-1)
+!!        IE_half_lat = 90.0 - cRadToDeg * ThetaIono_I(iSize:2*iSize-1)
 ! This is needed to mirror the northern hemisphere for now
         IE_half_lat = cRadToDeg * ThetaIono_I(iSize:2*iSize-1) - 90.
         IE_half_lon =         cRadToDeg * PhiIono_I
