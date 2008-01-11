6f08074014b2834a84d29e65276273832cd62b99
dwelling <>
2008-01-11 19:51:55 +0000

    Added two subroutines for passing information about satellites that are being traced in GM and IM from GM to IM.
    dtw, Jan 2008

--- srcInterface/GM_get_for_im.f90
+++ srcInterface/GM_get_for_im.f90
@@ -88,5 +88,82 @@ subroutine GM_get_for_im(Buffer_IIV,iSizeIn,jSizeIn,nVar,NameVar)
      Buffer_IIV(:,:,pInvB_)   = MHD_SUM_p
   end if
 
-  !^CFG END RCM
 end subroutine GM_get_for_im
+
+!==========================================================================
+subroutine GM_satinit_for_im(nSats)
+
+  !This subroutine collects the number of satellite files for use in 
+  !SWMF GM and IM coupling.   !!!DTW 2007
+
+  !Module variables to use:
+  use ModMain,   ONLY: DoImSatTrace
+  use ModIO,     ONLY: nSatellite
+  use ModProcMH, ONLY: iProc
+
+  implicit none
+
+  !Subroutine Arguments:
+  integer,           intent(out) :: nSats
+!--------------------------------------------------------------------------
+  
+  !If IM sat tracing is on, collect the number of satellites to trace.
+  !If IM sat tracing is off, set nSats to zero.
+  if (DoImSatTrace) then
+     nSats = nSatellite
+  else 
+     nSats = 0
+  endif
+
+end subroutine GM_satinit_for_im
+
+!==========================================================================
+subroutine GM_get_sat_for_im(Buffer_III, Buffer_I, nSats)
+
+  ! Subroutine to update and collect satellite locations for IM tracing
+  ! !!!DTW 2007
+  
+  !Modules
+  use ModProcMH, ONLY: iProc, iComm
+  use ModIO,     ONLY: Satellite_name, Xsatellite
+  use ModMPI
+  
+  implicit none
+  
+  !Arguments
+  integer, intent(in)               :: nSats
+  real, intent(out)                 :: Buffer_III(3,2,nSats)
+  character (len=100), intent(out)  :: Buffer_I(nSats)
+  
+  !Internal variables
+  character (len=*), parameter :: NameSub='GM_get_sat_for_im'
+
+  real :: sat_RayVars(6), sat_RayVarsSum(6)
+  
+  integer :: iSat, iError
+  !--------------------------------------------------------------------------
+  ! Store satellite names in Buffer_I
+  if (iProc == 0) then
+     Buffer_I = Satellite_name(1:nSats)
+  end if
+
+  do iSat=1, nSats
+     ! Update satellite position.
+     call set_satellite_flags(iSat)
+     call sat_get_ray(iSat, sat_RayVars)
+
+     ! Reduce values from all 
+     call MPI_reduce(sat_RayVars, sat_RayVarsSum, 6, MPI_REAL, MPI_SUM, &
+          0, iComm, iError)
+     
+     ! Store results in Buffer_III
+     if (iProc == 0) then 
+        Buffer_III(:,1,iSat) = Xsatellite(iSat,:)
+        Buffer_III(:,2,iSat) = sat_RayVarsSum(1:3)
+     end if
+  end do
+
+  !^CFG END RCM      
+end subroutine GM_get_sat_for_im
+
+!==========================================================================
