c5e7ea06b23b9d676d12d78d34eb7d777a5de315
rilie <>
2008-05-19 20:35:32 +0000

    Added some usage info.

--- Scripts/cat_sat_log.pl
+++ Scripts/cat_sat_log.pl
@@ -1,2 +1,4 @@
 #!/usr/bin/perl -p
+# Usage:   cat_sat_log.pl logfile1 logfile2 .... > log_all.log
+# Example: cat_sat_log.pl log_*.log > log_all.log
 $_ = '' unless ($. == 1 or $. == 2 or /^[\s\d\.eEdD\+\-]+$/);
