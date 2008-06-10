3df0eaa87c316d885a2a26e6ec8629c5e8eba09b
gtoth <>
2008-06-10 21:00:46 +0000

    Added #MULTIION command. Both #MULTIION and #COLLISION commands call multi_ion_set_parameters method in ModMultiIon.f90

--- src/MH_set_parameters.f90
+++ src/MH_set_parameters.f90
@@ -50,7 +50,7 @@ subroutine MH_set_parameters(TypeAction)
        zSizeBoxHall, DzSizeBoxHall
   use ModResistivity                              !^CFG IF DISSFLUX
   use ModMultiFluid, ONLY: MassIon_I
-  use ModMultiIon, ONLY: CollisionCoefDim
+  use ModMultiIon, ONLY: multi_ion_set_parameters
   use ModSolarwind, ONLY: UseSolarwindFile, NameSolarwindFile, &
        read_solar_wind_file, normalize_solar_wind_data
 
@@ -1650,8 +1650,8 @@ subroutine MH_set_parameters(TypeAction)
         call read_var('SpeciesPercentCheck',SpeciesPercentCheck)
      case("#MULTIFLUID")
         call read_var('UseTotalSpeed',UseTotalSpeed)
-     case("#COLLISION")
-        call read_var('CollisionCoefDim',CollisionCoefDim)
+     case("#MULTIION", "#COLLISION")
+        call multi_ion_set_parameters(NameCommand)
      case('#USERBOUNDARY', '#EXTRABOUNDARY')
         if(.not.is_first_session())CYCLE READPARAM
         call read_var('UseExtraBoundary',UseExtraBoundary)
