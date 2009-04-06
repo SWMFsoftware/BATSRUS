7ad73ca604a95239441811a0f31f3ee9919962b2
gtoth <>
2009-04-06 19:30:51 +0000

    This is work with Jiqun.
    Fixed two major bugs:
    1.   In FAC = B.J/B the B was in SM while J in GSM coordinates.
    2.   In FAC_r = FAC*(B.r)/(B*r) B was at rCurrents and r at the ionosphere.
    Added UseWrongB logical to allow reproducing earlier results. Switch on with
    
    #TEST
    FACBUG
    
    in the GM section of the PARAM.in file.
    
    Added optional argument LatBoundary (instead of putting it into Fac_II).
    Added optional arguments Theta_I and Phi_I for a non-uniform grid.
    
    
    Former-commit-id: c497b115fee0670029ee53aba37bd1765ae3dcb9

--- src/ModCurrent.f90
+++ src/ModCurrent.f90
@@ -1,54 +1,80 @@
 !^CFG COPYRIGHT UM      
 module ModCurrent
 
+
+  use ModCoordTransform, ONLY: sph_to_xyz
+  use CON_axes,          ONLY: transform_matrix
+
   implicit none
 
 contains
-  !============================================================================  
-  subroutine calc_field_aligned_current(nTheta, nPhi, rIn, Fac_II, b_DII)
+  !===========================================================================
+  subroutine calc_field_aligned_current(nTheta, nPhi, rIn, Fac_II, bSm_DII, &
+       LatBoundary, Theta_I, Phi_I)
 
     use ModVarIndexes,     ONLY: Bx_, Bz_, nVar
-    use ModMain,           ONLY: Time_Simulation, TypeCoordSystem, nBlock
-    use ModPhysics,        ONLY: rCurrents,UnitB_,Si2No_V, UnitJ_,No2Si_V,No2Io_V
+    use ModMain,           ONLY: Time_Simulation, TypeCoordSystem, nBlock, &
+         Test_String
+    use ModPhysics,        ONLY: rCurrents,UnitB_,Si2No_V, UnitJ_,No2Si_V, &
+         No2Io_V
     use CON_planet_field,  ONLY: get_planet_field, map_planet_field
     use ModProcMH,         ONLY: iProc, iComm
-    use ModNumConst, ONLY: cHalfPi, cTwoPi, cPi
+    use ModNumConst,       ONLY: cHalfPi, cTwoPi, cPi
     use ModMpi
-    use ModCoordTransform, ONLY: sph_to_xyz
-    use CON_axes,          ONLY: transform_matrix
-    
 
     ! Map the grid points from the rIn radius to rCurrents.
     ! Calculate the field aligned currents there, use the ratio of the
-    ! magnetic field strength, and project to the radial direction.
-    ! The result is saved into Fac_II, b_DII.
+    ! magnetic field strength.
+    ! The result is saved into Fac_II, bSm_DII.
 
+    ! Size of spherical grid at rIn
     integer, intent(in) :: nTheta, nPhi
+
+    ! Radius of spherical grid where FAC is needed
     real, intent(in) :: rIn
-    real, intent(out), dimension(nTheta,nPhi):: Fac_II
-    real, intent(out), dimension(3,nTheta,nPhi):: b_DII
+
+    ! Field aligned current at rIn
+    real, intent(out):: Fac_II(nTheta,nPhi)
+
+    ! Magnetic field at rIn in SM coordinates
+    real, intent(out):: bSm_DII(3,nTheta,nPhi)
+
+    ! Lowest latitude that maps up to rCurrents
+    real, intent(out), optional :: LatBoundary
+
+    ! Coordinate arrays allow non-uniform grid
+    real, intent(in), optional:: Theta_I(nTheta)
+    real, intent(in), optional:: Phi_I(nPhi)
+
+
     real, allocatable :: bCurrentLocal_VII(:,:,:), bCurrent_VII(:,:,:)
+
     integer :: i, j, iHemisphere, iError
     real    :: Phi, Theta, Xyz_D(3),XyzIn_D(3),B0_D(3)
     real    :: b_D(3), b, bRcurrents,Fac, Jr_D(3),bUnit_D(3)
-    real    :: GmSmg_DD(3,3),GsmtoSmg_DD(3,3)
+    real    :: bIn_D(3), bIn
+    real    :: GmSmg_DD(3,3)
     real    :: State_V(Bx_-1:nVar+3)
-    real    :: LatBoundary, dPhi, dTheta
+    real    :: dPhi, dTheta
     logical :: DoMap
-    !-------------------------------------------------------------------------
 
-    if(.not.allocated(bCurrentLocal_VII))then
-       allocate(bCurrentLocal_VII(0:6,nTheta,nPhi))
-       allocate(bCurrent_VII(0:6,nTheta,nPhi))
-    end if
+    logical :: UseWrongB = .false.
+
+    !-------------------------------------------------------------------------
+    UseWrongB = index(Test_String,'FACBUG') > 0
+
+    if(.not.allocated(bCurrentLocal_VII)) allocate( &
+         bCurrentLocal_VII(0:6,nTheta,nPhi), &
+         bCurrent_VII(0:6,nTheta,nPhi))
 
     bCurrentLocal_VII = 0.0
-    bCurrent_VII = 0.0
+    bCurrent_VII      = 0.0
+
+    Fac_II = 0.0
 
     GmSmg_DD = transform_matrix(Time_Simulation, 'SMG', TypeCoordSystem)
-!    GsmtoSmg_DD = transform_matrix(TIme_Simulation,TypeCoordSystem,'SMG')
 
-    LatBoundary = 100.0
+    if(present(LatBoundary)) LatBoundary = 100.0
 
     if (abs(rIn-rCurrents)<1.0e-3)then
        DoMap = .false.
@@ -60,35 +86,48 @@ contains
     dTheta = cPi /(nTheta-1)
 
     do j = 1, nPhi
-       Phi = (j-1) * dPhi
+       if(present(Phi_I))then
+          Phi = Phi_I(j)
+       else
+          Phi = (j-1) * dPhi
+       end if
        do i = 1, nTheta
-          Theta = (i-1) * dTheta
-          if (i==1)Theta = 1.0E-4
+          if(present(Theta_I))then
+             Theta = Theta_I(i)
+          else
+             Theta = (i-1) * dTheta
+          end if
 
           call sph_to_xyz(rIn,Theta,Phi, XyzIn_D)
-       
+
           if (DoMap)then
              ! if not reach to the mapped position, then skip this line
              call map_planet_field(Time_Simulation, XyzIn_D, 'SMG NORM', &
                   rCurrents, Xyz_D, iHemisphere)
-             
+
              if(iHemisphere == 0) then
-             ! Assign weight 1, magnetic field of 1,0,0 and current 0,0,0 
-                bCurrentLocal_VII(:,i,j) = (/1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0/)
+                ! Assign weight 1, magnetic field of 1,0,0 and current 0,0,0 
+                bCurrentLocal_VII(:,i,j) = &
+                     (/1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0/)
                 CYCLE
              end if
           else
              Xyz_D = XyzIn_D
           end if
 
-          LatBoundary = min( abs(Theta - cHalfPi), LatBoundary )
-          
-          ! Get the B0 field at the mapped position                               
-          call get_planet_field(Time_Simulation, Xyz_D,'SMG NORM',B0_D)
-          B0_D = B0_D*Si2No_V(UnitB_)            
-
           ! Convert to GM coordinates
           Xyz_D = matmul(GmSmg_DD, Xyz_D)
+
+         if(present(LatBoundary)) &
+              LatBoundary = min( abs(Theta - cHalfPi), LatBoundary )
+
+          ! Get the B0 field at the mapped position
+          call get_planet_field(Time_Simulation, Xyz_D, &
+               TypeCoordSystem//' NORM', B0_D)
+
+          if(UseWrongB) B0_D = matmul(B0_D, GmSmg_DD)
+
+          B0_D = B0_D*Si2No_V(UnitB_)
 
           ! Extract currents and magnetic field for this position
           call get_point_data(0.0, Xyz_D, 1, nBlock, Bx_, nVar+3, State_V)
@@ -97,21 +136,19 @@ contains
           bCurrentLocal_VII(1:3,i,j) = State_V(Bx_:Bz_) + &  ! B1 and B0
                State_V(Bx_-1)*B0_D
           bCurrentLocal_VII(4:6,i,j) = State_V(nVar+1:nVar+3) ! Currents
-          ! convert the vectors to SMG coordinates
-!          bCurrentLocal_VII(1:3,i,j) = matmul(GsmtoSmg_DD,bCurrentLocal_VII(1:3,i,j))
-           bCurrentLocal_VII(1:3,i,j) = matmul(bCurrentLocal_VII(1:3,i,j),GmSmg_DD)
 
           if(.false. .and. i==6 .and. j==6)then
              write(*,*)'iHemispher=',iHemisphere
              write(*,*)'Phi,Theta=',Phi,Theta
+             write(*,*)'XyzIn_D  =', XyzIn_D
              write(*,*)'Xyz_D    =',Xyz_D
-             write(*,*)'rCurrents=',rCurrents
+             write(*,*)'rCurrents=',rCurrents, sqrt(sum(Xyz_D**2))
+             write(*,*)'B0_D     =',B0_D
              write(*,*)'bCurrentLocal_VII =',bCurrentLocal_VII(:,i,j)
              call stop_mpi('DEBUG')
           end if
        end do
     end do
-
 
     call MPI_reduce(bCurrentLocal_VII,bCurrent_VII,nTheta*nPhi*7, &
          MPI_REAL,MPI_SUM,0,iComm,iError)
@@ -123,56 +160,68 @@ contains
     if(iProc==0)then
 
        do j = 1, nPhi
-          Phi = (j-1) * dPhi
+          if(present(Phi_I))then
+             Phi = Phi_I(j)
+          else
+             Phi = (j-1) * dPhi
+          end if
           do i = 1, nTheta
-            Theta = (i-1) * dTheta
-             if(i==1)Theta = 1.0E-4
+             if(present(Theta_I))then
+                Theta = Theta_I(i)
+             else
+                Theta = (i-1) * dTheta
+             end if
+
              ! Divide MHD values by the total weight if it exceeds 1.0 
              if(bCurrent_VII(0,i,j) > 1.0) bCurrent_VII(:,i,j) = &
                   bCurrent_VII(:,i,j) / bCurrent_VII(0,i,j)
-             
+
              ! Extract magnetic field and current
              b_D  = bCurrent_VII(1:3,i,j)
              Jr_D = bCurrent_VII(4:6,i,j)
-             
+
              ! The strength of the field
              bRcurrents = sqrt(sum(b_D**2))
-             
+
              ! Convert b_D into a unit vector
              bUnit_D = b_D / bRcurrents
              ! get the field aligned current
              Fac = sum(bUnit_D*Jr_D)
-             
 
              if(DoMap)then
-                ! Calculate magnetic field strength at the rIn grid points
-                call sph_to_xyz(rIn,Theta,Phi, XyzIn_D)
-                call get_planet_field(Time_Simulation, XyzIn_D,'SMG NORM',b_D)
-                
-                ! Convert to GM units and get magnitude
-                b_D = b_D*Si2No_V(UnitB_)
-                b   = sqrt(sum(b_D**2))
-                
-                ! Multiply by the ratio of the magnetic field strengths
-                Fac = b / bRcurrents * Fac
+                ! Calculate magnetic field strength at the rIn grid point
+                call sph_to_xyz(rIn, Theta, Phi, XyzIn_D)
 
+                ! Convert to GM coordinates
+                XyzIn_D = matmul(GmSmg_DD, XyzIn_D)
+                
+                call get_planet_field(Time_Simulation, XyzIn_D, &
+                     TypeCoordSystem//' NORM', bIn_D)
+                
+                ! Convert to normalized units and get magnitude
+                bIn_D = bIn_D*Si2No_V(UnitB_)
+                bIn   = sqrt(sum(bIn_D**2))
+
+                ! Multiply by the ratio of the magnetic field strengths
+                Fac = bIn / bRcurrents * Fac
+             else
+                bIn_D = b_D
              end if
-            !store the field alinged current
+             !store the field alinged current
              Fac_II(i,j) = Fac 
-             !store the B field
-             b_DII(:,i,j) = b_D
-             
+             !store the B field in SM coordinates !!
+
+             if(UseWrongB)then
+                bSm_DII(:,i,j) = b_D
+             else
+                bSm_DII(:,i,j) = matmul(bIn_D, GmSmg_DD)
+             end if
+
           end do
        end do
-       ! Save the latitude boundary information to the equator.                     
-       ! The reason to use Si2No_V(UnitJ_) is in order to be converted back
-       ! in the coupler (ie.,IE). For other purpose (not for the coupler), 
-       ! Please avoid this point !
-       Fac_II(nTheta/2:nTheta/2+1,1) = LatBoundary * Si2No_V(UnitJ_)
-
     end if
-    deallocate(bCurrentLocal_VII,bCurrent_VII)
-    
+    deallocate(bCurrentLocal_VII, bCurrent_VII)
+
   end subroutine calc_field_aligned_current
 
 end module ModCurrent
