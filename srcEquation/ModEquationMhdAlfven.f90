274fcd6512880f4d042f3d4b0f84384082817c0c
bartvand <>
2009-08-14 20:06:28 +0000

    The equations module for corona with heat conduction is upgraded to the wave infrastructure
    Former-commit-id: 5df89b410fd60d80b4f45ff4c9190ed4a3233f8d

--- srcEquation/ModEquationMhdAlfven.f90
+++ srcEquation/ModEquationMhdAlfven.f90
@@ -1,6 +1,6 @@
 module ModVarIndexes
 
-  use ModSingleFluid, Redefine1 => pAlfven1_, Redefine2 => pAlfven2_
+  use ModSingleFluid, Redefine1 => WaveFirst_, Redefine2 => WaveLast_
 
   implicit none
 
@@ -9,34 +9,29 @@ module ModVarIndexes
   ! This equation module contains the standard MHD equations.
   character (len=*), parameter :: NameEquation='MHD with WKB Alfven waves'
 
-  ! The variables numbered from 1 to nVar are:
-  !
-  ! 1. defined in set_ICs.
-  ! 2. prolonged and restricted in AMR
-  ! 3. saved into the restart file
-  ! 4. sent and recieved in the exchange message
-  ! 5. filled in the outer ghostcells by the program set_outer_BCs
-  ! 5. integrated by subroutine integrate_all for saving to logfile
-  ! 6. should be updated by advance_*
+  ! loop variable for implied do-loop over spectrum
+  integer :: iWave
 
-  integer, parameter :: nVar = 10
+  ! Number of frequency bins in spectrum
+  integer, parameter :: nWave = 2
+  integer, parameter :: nVar = 8 + nWave
 
   ! Named indexes for State_VGB and other variables
   ! These indexes should go subsequently, from 1 to nVar+1.
   ! The energy is handled as an extra variable, so that we can use
   ! both conservative and non-conservative scheme and switch between them.
   integer, parameter :: &
-       Rho_      = 1,    &
-       RhoUx_    = 2,    &
-       RhoUy_    = 3,    &
-       RhoUz_    = 4,    &
-       Bx_       = 5,    &
-       By_       = 6,    &
-       Bz_       = 7,    &
-       pAlfven1_ = 8,    &
-       pAlfven2_ = 9,    &
-       p_        = nVar, &
-       Energy_   = nVar+1
+       Rho_       = 1,                  &
+       RhoUx_     = 2,                  &
+       RhoUy_     = 3,                  &
+       RhoUz_     = 4,                  &
+       Bx_        = 5,                  &
+       By_        = 6,                  &
+       Bz_        = 7,                  &
+       WaveFirst_ = 8,                  &
+       WaveLast_  = WaveFirst_+nWave-1, &
+       p_         = nVar,               &
+       Energy_    = nVar+1
 
   ! This allows to calculate RhoUx_ as rhoU_+x_ and so on.
   integer, parameter :: RhoU_ = RhoUx_-1, B_ = Bx_-1
@@ -59,36 +54,35 @@ module ModVarIndexes
        0.0, & ! Bx_
        0.0, & ! By_
        0.0, & ! Bz_
-       0.0, & ! pAlfven1_
-       0.0, & ! pAlfven2_
+       (0.0, iWave=WaveFirst_,WaveLast_), &
        1.0, & ! p_
        1.0 /) ! Energy_
 
   ! The names of the variables used in i/o
   character(len=*), parameter :: NameVar_V(nVar+1) = (/ &
-       'Rho', & ! Rho_
-       'Mx ', & ! RhoUx_
-       'My ', & ! RhoUy_
-       'Mz ', & ! RhoUz_
-       'Bx ', & ! Bx_
-       'By ', & ! By_
-       'Bz ', & ! Bz_
-       'Pap', & ! pAlfven1_
-       'Pam', & ! pAlfven2_
-       'p  ', & ! p_
-       'e  ' /) ! Energy_
+       'Rho ', & ! Rho_
+       'Mx  ', & ! RhoUx_
+       'My  ', & ! RhoUy_
+       'Mz  ', & ! RhoUz_
+       'Bx  ', & ! Bx_
+       'By  ', & ! By_
+       'Bz  ', & ! Bz_
+       'Ew01', & ! WaveFirst_
+       'Ew02', & ! WaveLast_
+       'p   ', & ! p_
+       'e   ' /) ! Energy_
 
   ! The space separated list of nVar conservative variables for plotting
   character(len=*), parameter :: NameConservativeVar = &
-       'rho mx my mz bx by bz pap pam e'
+       'rho mx my mz bx by bz Ew01 Ew02 e'
 
   ! The space separated list of nVar primitive variables for plotting
   character(len=*), parameter :: NamePrimitiveVar = &
-       'rho ux uy uz bx by bz pap pam p'
+       'rho ux uy uz bx by bz Ew01 Ew02 p'
 
   ! The space separated list of nVar primitive variables for TECplot output
   character(len=*), parameter :: NamePrimitiveVarTec = &
-       '"`r", "U_x", "U_y", "U_z", "B_x", "B_y", "B_z", "p_ap", "p_am", "p"'
+       '"`r", "U_x", "U_y", "U_z", "B_x", "B_y", "B_z", "Ew01", "Ew02", "p"'
 
   ! Names of the user units for IDL and TECPlot output
   character(len=20) :: &
@@ -101,7 +95,7 @@ module ModVarIndexes
   integer, parameter :: U_ = RhoU_, Ux_ = RhoUx_, Uy_ = RhoUy_, Uz_ = RhoUz_
 
   ! There are no extra scalars
-  integer, parameter :: ScalarFirst_ = 2, ScalarLast_ = 1
+  integer, parameter :: ScalarFirst_ = WaveFirst_, ScalarLast_ = WaveLast_
 
   ! There are no multi-species
   logical, parameter :: UseMultiSpecies = .false.
@@ -115,6 +109,11 @@ contains
   subroutine init_mod_equation
 
     call init_mhd_variables
+
+    ! Set the unit and unit name for the wave energy variable
+    UnitUser_V(WaveFirst_:WaveLast_)        = UnitUser_V(Energy_)
+    NameUnitUserTec_V(WaveFirst_:WaveLast_) = NameUnitUserTec_V(Energy_)
+    NameUnitUserIdl_V(WaveFirst_:WaveLast_) = NameUnitUserIdl_V(Energy_)
 
   end subroutine init_mod_equation
 
