0e4e216359d7fa6e1b0bfbec5a310ecdf2f6d6bf
bartvand <>
2009-08-17 15:00:42 +0000

    adiabatic heating for electrons
    Former-commit-id: e6cf9b8f6a11d21a273cbcf315a5215046578a02

--- src/calc_sources.f90
+++ src/calc_sources.f90
@@ -127,6 +127,18 @@ subroutine calc_sources
      end do; end do; end do
   end if
 
+  if(UseElectronEnergy)then
+     ! Adiabatic heating for electron energy: -(g-1)*Ee*Div(U)
+     ! Using velocity of fluid 1
+     do k = 1, nK; do j = 1, nJ; do i = 1, nI
+        Source_VC(Ee_,i,j,k) = Source_VC(Ee_,i,j,k) &
+             -(g - 1)*State_VGB(Ee_,i,j,k,iBlock)*vInv_CB(i,j,k,iBlock) &
+             *(uDotArea_XI(i+1,j,k,1) - uDotArea_XI(i,j,k,1) &
+             + uDotArea_YI(i,j+1,k,1) - uDotArea_YI(i,j,k,1) &
+             + uDotArea_ZI(i,j,k+1,1) - uDotArea_ZI(i,j,k,1))
+     end do; end do; end do
+  end if
+
   if(UseElectronPressure)then
      ! Adiabatic heating for electron pressure: -(g-1)*Pe*Div(U)
      do k=1,nK; do j=1,nJ; do i=1,nI
