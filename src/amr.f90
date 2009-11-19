be62f4489c4d05476bdbb20768b1dedd2cdc98dd
gtoth <>
2009-11-19 17:08:07 +0000

    Do prespecified and user defined AMR with BATL when UseBatl is true. Physics based (with sorted criteria) AMR is not implemented yet.
    Former-commit-id: 9ce4bdad7d4a2019e1f1d8015f0fcaf5738d8db7

--- src/amr.f90
+++ src/amr.f90
@@ -2,39 +2,90 @@
 subroutine amr(idepth)
   use ModProcMH
   use ModMain, ONLY : nIJK,nBLK,nBlock,nBlockMax,nBlockALL,MaxBlock,&
-       unusedBLK,lVerbose,UseB,UseB0
+       unusedBLK,lVerbose,UseB,UseB0, UseBatl, Dt_BLK
   use ModGeometry, ONLY : minDXvalue,maxDXvalue,dx_BLK
   use ModAMR, ONLY : automatic_refinement
-  use ModAdvance, ONLY : DivB1_GB, iTypeAdvance_B, iTypeAdvance_BP
+  use ModAdvance, ONLY : DivB1_GB, iTypeAdvance_B, iTypeAdvance_BP, &
+       nVar, State_VGB
   use ModBlockData, ONLY: clean_block_data
   use ModIO, ONLY : write_prefix, iUnitOut
   use ModMpi
+
+  use ModParallel,      ONLY: UsePlotMessageOptions
+  use BATL_lib,         ONLY: regrid_batl, Unused_B, iNode_B, &
+       iStatusNew_A, Refine_, Coarsen_
+  use ModBatlInterface, ONLY: set_batsrus_grid
+  use ModUser,          ONLY: user_amr_criteria
+
   implicit none
 
   integer, intent(in) :: idepth
 
+  logical:: IsFound
+  real :: UserCriteria
   integer :: iBlock, iError
-  real :: minDX,maxDX    
-  logical :: local_refine(nBLK)
+  real :: minDX, maxDX    
+  logical :: DoRefine_B(nBLK)
+  !----------------------------------------------------------------------------
+  if(UseBatl)then
+     ! Do message passing with second order accurate ghost cells
+     UsePlotMessageOptions = .true.
+     call exchange_messages
+     if(automatic_refinement) then
+        do iBlock = 1, nBlock
+           if(Unused_B(iBlock)) CYCLE
+           call user_amr_criteria(iBlock, UserCriteria, 'user', IsFound)
+           if(UserCriteria > 0.5)then
+              iStatusNew_A(iNode_B(iBlock)) = Refine_
+           else
+              iStatusNew_A(iNode_B(iBlock)) = Coarsen_
+           end if
+        end do
+        call regrid_batl(nVar, State_VGB, Dt_BLK)
+        call set_batsrus_grid
 
-  !--------------------------------------------------------------------
+        if(iProc==0 .and. lVerbose>0)then
+           ! Write block/cell summary after AMR
+           call write_prefix; write(iUnitOut,*) '|'
+           call write_prefix; write(iUnitOut,*) &
+                '|  AMR:  nBlockMax = ',nBlockMax,' nBLK = ',nBLK
+           call write_prefix; write(iUnitOut,*) &
+                '|  AMR:  Total number of blocks used = ', nBlockALL
+           call write_prefix; write(iUnitOut,*) &
+                '|  AMR:  Total number of cells = ', nBlockALL*nIJK
+           call write_prefix; write(iUnitOut,*) &
+                '|  Smallest cell dx: ', minDXvalue, &
+                '  Largest cell dx: ',   maxDXvalue
+           call write_prefix; write(iUnitOut,*) '|'
+        end if
+
+     else
+        call specify_refinement(DoRefine_B)
+        call regrid_batl(nVar, State_VGB, Dt_BLK, DoRefine_B)
+        call set_batsrus_grid
+
+        if(iProc == 0 .and. lVerbose>0) then
+           call write_prefix; write(iUnitOut,*) &
+                'specify_refinement,  new number of blocks = ', nBlockALL
+        end if
+     end if
+
+     ! redo message passing
+     UsePlotMessageOptions = .false.
+     call exchange_messages
+     RETURN !!! TODO: iTypeAdvance, B0, ModBlockData...
+  end if
+
   ! Ensure ghostcells are up to date.
-  !
   call exchange_messages
 
   if (automatic_refinement) then
-     !-----------------------------------------------------------------
      ! Physics based refinement.
-     !
      call amr_physics
-
   else
-     !-----------------------------------------------------------------
      ! Prespecified refinement.
-     !
-     call specify_refinement(local_refine)
-     call refine_grid(local_refine)
-
+     call specify_refinement(DoRefine_B)
+     call refine_grid(DoRefine_B)
   end if
 
   ! Find new min and max dx
@@ -48,7 +99,7 @@ subroutine amr(idepth)
 
   ! Update the global advance info
   call MPI_allgather(iTypeAdvance_B, MaxBlock, MPI_INTEGER, &
-         iTypeAdvance_BP, MaxBlock, MPI_INTEGER, iComm, iError)
+       iTypeAdvance_BP, MaxBlock, MPI_INTEGER, iComm, iError)
 
   ! Clean all dynamically stored block data
   call clean_block_data
