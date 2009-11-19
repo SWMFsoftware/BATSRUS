60226e391416bc71730aa0176fdbc4e2a4b042b4
gtoth <>
2009-11-19 17:13:51 +0000

    Added optional arguments Dt_B, DoRefine_B and DoCoarsen_B to regrid_batl Added public variables from BATL_amr. Removed local use statements. Added detailed description for the methods.
    Former-commit-id: a18fd255ea098cc42fc9025171f823be20775c28

--- srcBATL/BATL_lib.f90
+++ srcBATL/BATL_lib.f90
@@ -7,6 +7,7 @@ module BATL_lib
   use BATL_tree
   use BATL_geometry
   use BATL_grid
+  use BATL_amr
   use BATL_pass_cell
   use BATL_pass_face
 
@@ -49,6 +50,10 @@ module BATL_lib
   public:: CellFace_DB, CellFace_DFB
   public:: CellVolume_B, CellVolume_GB
 
+  ! Inherited from BATL_amr
+  public:: iAmrChange_B
+  public:: AmrRemoved_, AmrUnchanged_, AmrMoved_, AmrRefined_, AmrCoarsened_
+
   ! Inherited from BATL_pass_cell
   public:: message_pass_cell
 
@@ -73,6 +78,30 @@ contains
     character(len=*), optional, intent(in):: TypeGeometryIn
     ! Periodicity of grid boundaries per dimention
     logical,          optional, intent(in):: IsPeriodicIn_D(nDim)
+
+    ! Initialize the block-adaptive tree and the domain. 
+    !
+    ! CoordMin_D and CoordMax_D are the domain boundaries. For non-Cartesian
+    ! grids this is meant in generalized coordinates.
+    ! 
+    ! MaxBlockIn gives the maximum number of blocks per processor
+    ! during the whole run.
+    ! At the base level the domain is decomposed into the root blocks.
+    !
+    ! The optional nRootIn_D argument provides the number of root blocks 
+    ! in each dimension. The default is a single root block at the base level.
+    !
+    ! The optional TypeGeometry describes the geometry of the coordinate
+    ! system. Default is "Cartesian". Currently the only other option is
+    ! RZ geometry. 
+    ! 
+    ! The optional IsPeriodicIn_D argument tells if a certain direction
+    ! is periodic or not. 
+    !
+    ! At the completion of this subroutine, the domain and the block-tree 
+    ! are initialized, the root blocks are distributed and their coordinates
+    ! cell volumes, face areas, etc. are all set.
+    !
     !-------------------------------------------------------------------------
     call init_tree(MaxBlockIn)
     call init_geometry(TypeGeometryIn, IsPeriodicIn_D)
@@ -84,18 +113,24 @@ contains
   end subroutine init_batl
   !============================================================================
   subroutine clean_batl
+
+    ! Free up memory by cleaning the grid and tree data
     call clean_grid
     call clean_tree
+
   end subroutine clean_batl
 
   !============================================================================
 
   subroutine init_grid_batl(DoRefine_B)
 
-    use BATL_tree, ONLY: adapt_tree, distribute_tree
-    use BATL_grid, ONLY: create_grid
-
     logical, optional, intent(in):: DoRefine_B(MaxBlock)
+
+    ! Initialize the grid. The grid does not contain any data at this point.
+    ! The optional DoRefine_B argument allows initial refinement of the grid.
+    ! After completion, the grid nodes are distributed over the processors,
+    ! using Morton ordering, and the coordinates and related variables
+    ! (cell volume, face areas, etc.) are all set.
 
     integer:: iBlock
     !------------------------------------------------------------------------
@@ -114,34 +149,86 @@ contains
 
   !============================================================================
 
-  subroutine regrid_batl(nVar, State_VGB, DoBalanceEachLevelIn)
+  subroutine regrid_batl(nVar, State_VGB, Dt_B, DoRefine_B, DoCoarsen_B, &
+       DoBalanceEachLevelIn)
 
-    use BATL_tree, ONLY: adapt_tree, distribute_tree, move_tree, &
-         iTree_IA, Level_
-    use BATL_amr,  ONLY: do_amr
-    use BATL_grid, ONLY: create_grid
-
-    integer, intent(in)   :: nVar
-    real,    intent(inout):: &
+    integer, intent(in)   :: nVar                         ! number of variables
+    real,    intent(inout):: &                            ! state variables
          State_VGB(nVar,MinI:MaxI,MinJ:MaxJ,MinK:MaxK,MaxBlock)
 
-    logical, intent(in), optional:: DoBalanceEachLevelIn
+    real, intent(inout), optional:: Dt_B(MaxBlock)        ! time step limit
+
+    logical, intent(in), optional:: DoRefine_B(MaxBlock)  ! request to refine
+    logical, intent(in), optional:: DoCoarsen_B(MaxBlock) ! request to coarsen
+    logical, intent(in), optional:: DoBalanceEachLevelIn  ! balance per level?
+
+    ! Refine, coarsen and load balance the blocks containing the nVar 
+    ! state variables in State_VGB. Use second order accurate conservative
+    ! restriction and prolongation operators. Load balance by Morton ordering.
+    ! If DoBalanceEachLevelIn is true, balance each AMR level independently.
+    !
+    ! Refinement and coarsening is primarily based on the iStatusNew_A array 
+    ! (available via the BATL_lib module) that is indexed by nodes 
+    ! (so a processor can request refinement for a non-local block). 
+    ! It should be set to the values Refine_ and Coarsen_. The AMR algorithm
+    ! checks whether the requests can be done while keeping resolution
+    ! changes at most a factor of 2 (proper nesting). Refinement requests
+    ! take precedence over coarsening requests when multiple processors
+    ! set the iStatusNew_A for the same node. 
+    !
+    ! The optional arguments DoRefine_B and DoCoarsen_B are provided 
+    ! for convenience. They can be used to request refinement and 
+    ! coarsening for local blocks only.
+    !
+    ! The optional Dt_B argument contains the time step limit per block. 
+    ! This information is moved together with the block during load balance,
+    ! The time step limit is divided by 2 for prolonged blocks, and 
+    ! multiplied by 2 (and minimum is taken over the children) for coarsened
+    ! blocks.
+    !
+    ! Note that this estimate of the time step limit for prolonged/restricted
+    ! blocks is only an approximate solution, and for partial AMR
+    ! it is not correct at all. One can always recalculate the time step
+    ! limit for the coarsened and prolonged blocks.
+    !
+    ! The iAmrChange_B array provides information about the changes due to AMR
+    ! in the local blocks. Its value is set to AmrRemoved_ for blocks that
+    ! became unused, to AmrUnchanged_ for no change, to AmrMoved_ for blocks
+    ! that moved from one processor to another, AmrRefined_ for blocks that
+    ! were just refined, and AmrCoarsened_ for blocks that were just coarsened.
 
     logical:: DoBalanceEachLevel
+    integer:: iBlock
     !------------------------------------------------------------------------
     DoBalanceEachLevel = .false.
     if(present(DoBalanceEachLevelIn)) DoBalanceEachLevel = DoBalanceEachLevelIn
 
+    if(present(DoCoarsen_B))then
+       do iBlock = 1, nBlock
+          if(Unused_B(iBlock)) CYCLE
+          if(DoCoarsen_B(iBlock)) iStatusNew_A(iNode_B(iBlock)) = Coarsen_
+       end do
+    end if
+
+    if(present(DoRefine_B))then
+       do iBlock = 1, nBlock
+          if(Unused_B(iBlock)) CYCLE
+          if(DoRefine_B(iBlock)) iStatusNew_A(iNode_B(iBlock)) = Refine_
+       end do
+    end if
+
     ! Coarsen and refine the tree nodes
     call adapt_tree
+
     ! Load balance the tree
     if(DoBalanceEachLevel)then
-       call distribute_tree(DoMove=.false.,iTypeNode_A=iTree_IA(Level_,:)+1)
+       call distribute_tree(DoMove=.false., iTypeNode_A=iTree_IA(Level_,:)+1)
     else
        call distribute_tree(DoMove=.false.)
     end if
-    ! Coarsen, refine and load balance the flow variables
-    call do_amr(nVar,State_VGB)
+
+    ! Coarsen, refine and load balance the flow variables, and set Dt_B.
+    call do_amr(nVar, State_VGB, Dt_B)
 
     ! Finalize the tree information
     call move_tree
