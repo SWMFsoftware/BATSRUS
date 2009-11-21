55deebec6472897a91012115a0692cff39cc37d6
gtoth <>
2009-11-21 23:17:42 +0000

    The ignored dimensions are now set by setting nJ or nK to 1. The AMR dimensions are set by the new -r flag of Config.pl that set iRatio,jRatio and kRatio in BATL_size.f90. The nDim and nDimAmr variables are calculated from the primary variables nI, nJ, nK, iRatio, jRatio, kRatio. This allows setting any subset of the non-ignored dimensions to be adaptive. Still needs to be tested if it actually works.
    Former-commit-id: 45a6e72cd7215376e4fa60aa29c36b7863476a44

--- srcBATL/BATL_size_orig.f90
+++ srcBATL/BATL_size_orig.f90
@@ -4,44 +4,44 @@ module BATL_size
 
   SAVE
 
-  ! Dimensionality of grid and AMR
-  integer, parameter :: MaxDim = 3    ! This has to be 3 all the time
-  integer, parameter :: nDim   = 3    ! Number of not ignored dimensions
-  integer, parameter :: nDimAmr = 3  ! Number of refined dimensions
+  ! Maximum dimensionality of grid is 3 (cannot be modified)
+  integer, parameter :: MaxDim = 3
 
-  ! Note: 3 = MaxDim >= nDim >= nDimAmr >= 1
-
-  ! Maximum number of blocks per processor
-  integer :: MaxBlock = 0
-
-  ! Largest block index
-  integer :: nBlock = 0
-
-  ! Number of cells per block in each direction
+  ! Number of cells per block in each direction. 
+  ! These values are set by the Config.pl script.
+  ! Set 1 for ignored directions! 
   integer, parameter :: nI = 8, nJ = 8, nK = 8
 
-  ! Number of cells per block
-  integer, parameter :: nIJK = nI*nJ*nK
+  ! Number of not ignored dimensions
+  integer, parameter :: nDim = min(1,nI-1) + min(1,nJ-1) + min(1,nK-1)
 
-  ! Array for block size
+  ! Refinement ratios in the 3 dimensions. Either 1 or 2.
+  ! The values are set by the Config.pl script.
   integer, parameter:: &
-       nIJK_D(MaxDim) = (/ nI, nJ, nK /)
+       iRatio = min(2,nI), jRatio = min(2,nJ), kRatio = min(2,nK)
 
-  ! number of ghost cells
+  ! Number of dimensions in which grid adaptation is done
+  integer, parameter :: nDimAmr = iRatio + jRatio + kRatio - 3
+
+  ! Maximum number of ghost cells
   integer, parameter :: nG = 2
 
-  integer, parameter:: nGI = nG
-  integer, parameter:: nGJ = nG*min(1,nDim-1)
-  integer, parameter:: nGK = nG*max(0,nDim-2)
-  
+  ! Number of ghost cells in each direction
+  integer, parameter:: nGI = nG*min(1,nI-1)
+  integer, parameter:: nGJ = nG*min(1,nJ-1)
+  integer, parameter:: nGK = nG*min(1,nK-1)
+
+  ! Cell index ranges including ghost cells
   integer, parameter :: &
        MinI = 1 - nGI, MaxI = nI + nGI, &
        MinJ = 1 - nGJ, MaxJ = nJ + nGJ, &
        MinK = 1 - nGK, MaxK = nK + nGK
 
-  ! Refinement ratios in the 3 dimensions (depends on nDimAmr)
-  integer, parameter:: &
-       iRatio = 2, jRatio = min(2,nDimAmr), kRatio = max(1,nDimAmr-1)
+  ! Number of cells per block
+  integer, parameter:: nIJK = nI*nJ*nK
+
+  ! Array for block size
+  integer, parameter:: nIJK_D(MaxDim) = (/ nI, nJ, nK /)
 
   ! Array of refinement ratios
   integer, parameter:: iRatio_D(MaxDim) = (/ iRatio, jRatio, kRatio /)
@@ -51,6 +51,12 @@ module BATL_size
   
   ! Inverse volume ratio for Cartesian case
   real, parameter:: InvIjkRatio = 1.0/IjkRatio
+
+  ! Maximum number of blocks per processor is set dynamically
+  integer :: MaxBlock = 0
+
+  ! Largest used block index on a processor at any given time
+  integer :: nBlock = 0
 
 end module BATL_size
 
