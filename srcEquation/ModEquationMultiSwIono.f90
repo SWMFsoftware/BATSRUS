c87402f114bcc6684f6fe51fcb4e47eb068da785
dwelling <>
2010-01-22 23:51:23 +0000

    Based ModEquationMultiIonSwIono on the wrong multifluid equation file. Fixed this and changed name to prevent confusion but be different than K.C.'s ModEquationMhdSwIono.
    Former-commit-id: f1f0d3e28da55a793c3e8feefd99b55ba7388186

--- srcEquation/ModEquationMultiSwIono.f90
+++ srcEquation/ModEquationMultiSwIono.f90
@@ -5,59 +5,63 @@ module ModVarIndexes
   implicit none
 
   save
-
   ! This equation module declares two Ions (solar wind H and ionosphere H)
   ! for the purposes of tracking plasma entry into the magnetosphere in
   ! multifluid MHD.  It requires changes to the #MAGNETOSPHERE command
   ! in order to set the Iono species as the dominant outflow species at
   ! the inner boundary.  
-  ! Based on ModEquationMultiIon and ModEquationMhdSwIono(Hansen, 2006)
+  ! Based on ModEquationMhdIons and ModEquationMhdSwIono(Hansen, 2006)
   ! Welling, 2010.
 
   ! This equation module contains the standard MHD equations.
-  character (len=*), parameter :: NameEquation='Multi-ion (Sw and Iono H+)'
+  character (len=*), parameter :: NameEquation='MHD with SW and Iono Hydrogen'
 
-  integer, parameter :: nVar = 13
+  integer, parameter :: nVar = 18
 
-  ! There are two ion fluids but no total ion fluid
-  integer, parameter :: nFluid    = 2
-  integer, parameter :: IonFirst_ = 1
-  integer, parameter :: IonLast_  = 2
-  logical, parameter :: IsMhd     = .false.
-  real               :: MassFluid_I(nFluid) = (/ 1.0, 1.0 /) !both are H+.
+  integer, parameter :: nFluid    = 3
+  integer, parameter :: IonFirst_ = 2
+  integer, parameter :: IonLast_  = 3
+  logical, parameter :: IsMhd     = .true.
+  real               :: MassFluid_I(2:3) = (/ 1.0, 1.0 /)
 
-  character (len=3), parameter :: NameFluid_I(nFluid) = (/ 'Sw', 'Iono' /)
+  character (len=3), parameter :: NameFluid_I(nFluid)= (/ 'All', 'Sw ', 'Iono '/)
 
   ! Named indexes for State_VGB and other variables
   ! These indexes should go subsequently, from 1 to nVar+nFluid.
   ! The energies are handled as an extra variable, so that we can use
   ! both conservative and non-conservative scheme and switch between them.
-  integer, parameter ::            &
-       Rho_         =  1,          &
-       RhoUx_       =  2, Ux_ = 2, &
-       RhoUy_       =  3, Uy_ = 3, &
-       RhoUz_       =  4, Uz_ = 4, &
-       Bx_          =  5,          &
-       By_          =  6,          &
-       Bz_          =  7,          &
-       p_           =  8,          &
-       IonoRho_     =  9,          &
-       IonoRhoUx_   = 10,          &
-       IonoRhoUy_   = 11,          &
-       IonoRhoUz_   = 12,          &
-       IonoP_       = 13,          &
-       Energy_      = nVar+1,      &
-       IonoEnergy_  = nVar+2
+  integer, parameter :: &
+       Rho_       =  1,          &
+       RhoUx_     =  2, Ux_ = 2, &
+       RhoUy_     =  3, Uy_ = 3, &
+       RhoUz_     =  4, Uz_ = 4, &
+       Bx_        =  5,     &
+       By_        =  6,     &
+       Bz_        =  7,     &
+       p_         =  8,     &
+       SwRho_     =  9,     &
+       SwRhoUx_   = 10,     &
+       SwRhoUy_   = 11,     &
+       SwRhoUz_   = 12,     &
+       SwP_       = 13,     &
+       IonoRho_     = 14,   &
+       IonoRhoUx_   = 15,   &
+       IonoRhoUy_   = 16,   &
+       IonoRhoUz_   = 17,   &
+       IonoP_       = 18,   &
+       Energy_    = nVar+1, &
+       SwEnergy_  = nVar+2, &
+       IonoEnergy_  = nVar+3
 
   ! This allows to calculate RhoUx_ as RhoU_+x_ and so on.
   integer, parameter :: U_ = Ux_ - 1, RhoU_ = RhoUx_-1, B_ = Bx_-1
 
   ! These arrays are useful for multifluid
-  integer, parameter :: iRho_I(nFluid)   = (/Rho_,   IonoRho_/)
-  integer, parameter :: iRhoUx_I(nFluid) = (/RhoUx_, IonoRhoUx_/)
-  integer, parameter :: iRhoUy_I(nFluid) = (/RhoUy_, IonoRhoUy_/)
-  integer, parameter :: iRhoUz_I(nFluid) = (/RhoUz_, IonoRhoUz_/)
-  integer, parameter :: iP_I(nFluid)     = (/p_,     IonoP_/)
+  integer, parameter :: iRho_I(nFluid)   = (/Rho_,   SwRho_,   IonoRho_/)
+  integer, parameter :: iRhoUx_I(nFluid) = (/RhoUx_, SwRhoUx_, IonoRhoUx_/)
+  integer, parameter :: iRhoUy_I(nFluid) = (/RhoUy_, SwRhoUy_, IonoRhoUy_/)
+  integer, parameter :: iRhoUz_I(nFluid) = (/RhoUz_, SwRhoUz_, IonoRhoUz_/)
+  integer, parameter :: iP_I(nFluid)     = (/p_,     SwP_,     IonoP_/)
 
   ! The default values for the state variables:
   ! Variables which are physically positive should be set to 1,
@@ -71,13 +75,19 @@ module ModVarIndexes
        0.0, & ! By_
        0.0, & ! Bz_
        1.0, & ! p_
+       1.0, & ! SwRho_
+       0.0, & ! SwRhoUx_
+       0.0, & ! SwRhoUy_
+       0.0, & ! SwRhoUz_
+       1.0, & ! SwP_
        1.0, & ! IonoRho_
        0.0, & ! IonoRhoUx_
        0.0, & ! IonoRhoUy_
        0.0, & ! IonoRhoUz_
        1.0, & ! IonoP_
        1.0, & ! Energy_
-       1.0 /) ! IonoEnergy_
+       1.0, & ! SwEnergy_
+       1.0  /)! IonoEnergy_
 
   ! The names of the variables used in i/o
   character(len=*), parameter :: NameVar_V(nVar+nFluid) = (/ &
@@ -89,27 +99,37 @@ module ModVarIndexes
        'By   ',   & ! By_
        'Bz   ',   & ! Bz_
        'P    ',   & ! p_
+       'SwRho',   & ! SwRho_
+       'SwMx ',   & ! SwRhoUx_
+       'SwMy ',   & ! SwRhoUy_
+       'SwMz ',   & ! SwRhoUz_
+       'SwP  ',   & ! SwP_
        'IonoRho', & ! IonoRho_
        'IonoMx ', & ! IonoRhoUx_
        'IonoMy ', & ! IonoRhoUy_
        'IonoMz ', & ! IonoRhoUz_
        'IonoP  ', & ! IonoP_
-       'SwE  ',   & ! Energy_
+       'E    ',   & ! Energy_
+       'SwE  ',   & ! SwEnergy_
        'IonoE  ' /) ! IonoEnergy_
 
   ! The space separated list of nVar conservative variables for plotting
   character(len=*), parameter :: NameConservativeVar = &
-       'Rho Mx My Mz Bx By Bz E IonoRho IonoMx IonoMy IonoMz IonoE'
+       'Rho Mx My Mz Bx By Bz E '// &
+       'SwRho SwMx SwMy SwMz SwE '// &
+       'IonoRho IonoMx IonoMy IonoMz IonoE'
 
   ! The space separated list of nVar primitive variables for plotting
   character(len=*), parameter :: NamePrimitiveVar = &
-       'Rho Ux Uy Uz Bx By Bz P IonoRho IonoUx IonoUy IonoUz IonoP'
+       'Rho Ux Uy Uz Bx By Bz P '// &
+       'SwRho SwUx SwUy SwUz SwP '// &
+       'IonoRho IonoUx IonoUy IonoUz IonoP'
 
   ! The space separated list of nVar primitive variables for TECplot output
   character(len=*), parameter :: NamePrimitiveVarTec = &
-       '"`r^Sw^+", "U_x^Sw^+", "U_y^Sw^+", "U_z^Sw^+", "B_x", "B_y", ' // &
-       '"B_z", "p", "`r^Iono^+", "U_x^Iono^+", "U_y^Iono^+", ' // &
-       '"U_z^Iono^+", "P^Iono^+"'
+       '"`r", "U_x", "U_y", "U_z", "B_x", "B_y", "B_z", "p", ' // &
+       '"`r^SwH^+", "U_x^SwH^+", "U_y^SwH^+", "U_z^SwH^+", "P^SwH^+", ' // &
+       '"`r^IonoH^+", "U_x^IonoH^+", "U_y^IonoH^+", "U_z^IonoH^+", "P^IonoH^+"'
 
   ! Names of the user units for IDL and TECPlot output
   character(len=20) :: &
