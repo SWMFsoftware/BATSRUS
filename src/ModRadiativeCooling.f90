9a32d8120f8edd12a798050660dfb3bdbfac5627
igorsok <>
2010-02-08 19:32:22 +0000

    Revision in ModRadiativeCooling: 1. Separate all chromosphere- and transition-region-related parameters to a separate module ModChromosphere 2. New routine to calculate the cooling function integral 3. Rename DoModifyHeatConduction to DoExtendTransitionRegion.
    Former-commit-id: 5a9910b12018db4ad4b6350aee80b5d8d5bf8861

--- src/ModRadiativeCooling.f90
+++ src/ModRadiativeCooling.f90
@@ -1,6 +1,60 @@
 !^CFG COPYRIGHT UM
 !============================================================================
+module ModChromosphere
+  !Here all parameters relating to chromosphere and tansition region are
+  !collected
+  
+  implicit none
+  save
+  
+  !The use of short-scale exponential heat function with 
+  !the decay length = (30 m/K)*TeCromosphere SI
+  logical:: UseChromosphereHeating    = .false. 
+  real   :: NumberDensChromosphereCgs = 1.0e+12 ![cm^{-3}  
+  real   :: TeChromosphereSi = 1.0e4            ![K]
+
+  !\
+  ! TRANSITION REGION
+  !/
+  ! 
+  
+  logical :: DoExtendTransitionRegion = .false.
+
+  !The following variables are meaningful if
+  !DoExtendTransitionRegion=.true.
+
+  real :: TeModSi = 3.0E+5                !K
+  real :: DeltaTeModSi = 1E+4             !K
+
+  !The following variable is meaningful if 
+  !DoExtendTransitionRegion = .false. . Al long as
+  !the unextended transition region cannot be resolved
+  !we set the 'corona base temperature' equal to the
+  !temperature at the top of the transition region and 
+  !use the integral ralationship across the transition
+  !region to find the number density at the top of the
+  !transition region
+
+  real :: TeTransitionRegionTopSi = 4.0e+5 ![K]
+
+  real, parameter :: TeModMinSi = 2.0E+4  !K
+contains
+  !================================
+  real function extension_factor(TeSi)
+    real, intent(in) :: TeSi    !Dimensionless
+    
+    real :: FractionSpitzer
+    !--------------------------------
+    FractionSpitzer = 0.5*(1.0+tanh((TeSi-TeModSi)/DeltaTeModSi))
+    
+    extension_factor = FractionSpitzer + &
+         (1.0 - FractionSpitzer)*(TeModSi/TeSi)**2.5
+  end function extension_factor
+  
+end module ModChromosphere
+!=========================
 module ModRadiativeCooling
+  use ModChromosphere
   use ModSize
   implicit none
   save
@@ -9,17 +63,7 @@ module ModRadiativeCooling
   logical:: UseRadCoolingTable =.false.
   integer,private:: iTableRadCool      =-1
 
-  ! Same use as the variables in ModHeatConduction
-  ! but need them here as well
-  logical :: DoModHeatConduction = .false.
-
-  real :: TeModSi = 3.0E+5                !K
-  real :: DeltaTeModSi = 1E+4             !K
-  real :: TeMod=0.0, DeltaTeMod=1.0
-
-  real, parameter :: TeModMinSi = 2.0E+4  !K
-  real ::  TeModMin
-
+  
   real :: AuxTeSi
   !\
   ! Recommended usage of get_radiative_cooling function
@@ -32,16 +76,23 @@ module ModRadiativeCooling
   ! in RadCooling  
 
   real :: RadCooling_C(1:nI,1:nJ,1:nK)
+
+  real, parameter :: Cgs2SiEnergyDens = &
+                               1.0e-7&   !erg = 1e-7 J
+                              /1.0e-6    !cm3 = 1e-6 m3 
 contains
   !==============================
   subroutine read_modified_cooling
     use ModReadParam
     !---------------
-    call read_var('DoModHeatConduction',DoModHeatConduction)
-    if(.not.DoModHeatConduction)return
+    call read_var('DoExtendTransitionRegion',DoExtendTransitionRegion)
+    if(DoExtendTransitionRegion)then
 
-    call read_var('TeModSi',TeModSi)
-    call read_var('DeltaTeModSi',DeltaTeModSi)
+       call read_var('TeModSi',TeModSi)
+       call read_var('DeltaTeModSi',DeltaTeModSi)
+    else
+       call read_var('TeTransitionRegionTopSi',TeTransitionRegionTopSi)
+    end if
   end subroutine read_modified_cooling
   !==============================
 
@@ -53,19 +104,12 @@ contains
     iTableRadCool = i_lookup_table('radcool')
     UseRadCoolingTable = iTableRadCool>0
 
-    TeMod      = TeModSi      * Si2No_V(UnitTemperature_)
-    DeltaTeMod = DeltaTeModSi * Si2No_V(UnitTemperature_)
-    TeModMin   = TeModMinSi   * Si2No_V(UnitTemperature_)
-
   end subroutine check_cooling_param
   !==============================
   subroutine get_radiative_cooling(i, j, k, iBlock, TeSiIn, RadiativeCooling)
 
-    use ModMultiFluid,    ONLY: MassIon_I
-    use ModPhysics,       ONLY: No2Si_V, Si2No_V, UnitT_, UnitN_, &
-         UnitEnergyDens_, UnitTemperature_
-    use ModVarIndexes,    ONLY: nVar, Rho_, P_
-    use ModLookupTable,   ONLY: interpolate_lookup_table
+    use ModPhysics,       ONLY: Si2No_V, No2Si_V, UnitN_
+    use ModVarIndexes,    ONLY: Rho_
     use ModAdvance,       ONLY: State_VGB
 
     integer, intent(in) :: i, j, k, iBlock
@@ -74,13 +118,8 @@ contains
     real,   intent(out) :: RadiativeCooling
 
 
-    real :: Te, CoolingFunctionCgs, NumberDensCgs
-    real :: Log10TeSi, Log10NeCgs, CoolingTableOut(2)
-    real, parameter :: RadNorm = 1.0E+22
-    real, parameter :: Cgs2SiEnergyDens = &
-                               1.0e-7&   !erg = 1e-7 J
-                              /1.0e-6    !cm3 = 1e-6 m3 
-    real :: TeFactor, FractionSpitzer
+    real :: NumberDensCgs
+
     !--------------------------------------------------------------------------
 
 
@@ -88,6 +127,34 @@ contains
     ! currently proton plasma only
     NumberDensCgs = State_VGB(Rho_, i, j, k, iBlock) * No2Si_V(UnitN_) * 1.0e-6
 
+    RadiativeCooling = - radiative_cooling(TeSiIn, NumberDensCgs)
+
+    ! include multiplicative factors to make up for extention of
+    ! perpendicular heating at low temperatures (as per Abbett 2007).
+    ! Need this to strech transition region to larger scales
+    ! Also, need radcool modification to become const below TeModMin
+    if(DoExtendTransitionRegion) then
+       if(TeSiIn >= TeModMinSi) then
+          RadiativeCooling = RadiativeCooling / extension_factor(TeSiIn)
+       else
+          RadiativeCooling = RadiativeCooling * (TeModMinSi / TeModSi)**2.5
+       endif
+    end if
+  end subroutine get_radiative_cooling
+  !============================================================
+  real function radiative_cooling(TeSiIn, NumberDensCgs)
+    use ModMultiFluid,    ONLY: MassIon_I
+    use ModPhysics,       ONLY: Si2No_V, UnitT_, &
+         UnitEnergyDens_
+    use ModLookupTable,   ONLY: interpolate_lookup_table
+
+    !Imput - dimensional, output: dimensionless
+    real, intent(in):: TeSiIn, NumberDensCgs
+ 
+    real :: CoolingFunctionCgs
+    real :: Log10TeSi, Log10NeCgs, CoolingTableOut(2)
+    real, parameter :: RadNorm = 1.0E+22
+    !-----------------------------------
     if(UseRadCoolingTable) then
        Log10TeSi = log10(TeSiIn)
        Log10NeCgs = log10(NumberDensCgs)
@@ -108,60 +175,72 @@ contains
     ! Avoid extrapolation past zero
     CoolingFunctionCgs = max(CoolingFunctionCgs,0.0)
 
-    RadiativeCooling = - NumberDensCgs**2*CoolingFunctionCgs &
+    radiative_cooling = NumberDensCgs**2*CoolingFunctionCgs &
          * Cgs2SiEnergyDens * Si2No_V(UnitEnergyDens_)/Si2No_V(UnitT_)
 
-    ! include multiplicative factors to make up for extention of
-    ! perpendicular heating at low temperatures (as per Abbett 2007).
-    ! Need this to strech transition region to larger scales
-    ! Also, need radcool modification to become const below TeModMin
-    if(DoModHeatConduction) then
-       Te = TeSiIn * Si2No_V(UnitTemperature_)
-
-       FractionSpitzer = 0.5*(1.0+tanh((Te-TeMod)/DeltaTeMod))
-       TeFactor = (FractionSpitzer * Te**2.5 &
-            + (1.0 - FractionSpitzer)*TeMod**2.5)
-       if(Te >= TeModMin) then
-          RadiativeCooling = RadiativeCooling * Te**2.5 / TeFactor
-       else
-          RadiativeCooling = RadiativeCooling * (TeModMin / TeMod)**2.5
-       endif
+  end function radiative_cooling
+!===============================
+  subroutine get_cooling_function_fit(TeSi, CoolingFunctionCgs)
+    
+    ! Based on Rosner et al. (1978) and Peres et al. (1982)
+    ! Need to be replaced by Chianti tables
+    
+    real, intent(in) :: TeSi
+    real, intent(out):: CoolingFunctionCgs
+    !-----------------------------------------------------------
+    
+    if(TeSi <= 8e3)then
+       CoolingFunctionCgs = (1.0606e-6*TeSi)**11.7
+    elseif(TeSi <= 2e4)then
+       CoolingFunctionCgs = (1.397e-8*TeSi)**6.15
+    elseif(TeSi <= 10**4.575)then
+       CoolingFunctionCgs = 10**(-21.85)
+    elseif(TeSi <= 10**4.9)then
+       CoolingFunctionCgs = 10**(-31.0)*TeSi**2
+    elseif(TeSi <= 10**5.4)then
+       CoolingFunctionCgs = 10**(-21.2)
+    elseif(TeSi <= 10**5.77)then
+       CoolingFunctionCgs = 10**(-10.4)/TeSi**2
+    elseif(TeSi <= 10**6.315)then
+       CoolingFunctionCgs = 10**(-21.94)
+    elseif(TeSi <= 10**7.60457)then
+       CoolingFunctionCgs = 10**(-17.73)/TeSi**(2.0/3.0)
+    else
+       CoolingFunctionCgs = 10**(-26.6)*sqrt(TeSi)
     end if
+    
+  end subroutine get_cooling_function_fit
+  !=============================================================
+  real function cooling_function_integral_si(TeTransitionRegionSi)
+    
+    real,intent(in):: TeTransitionRegionSi
+    
+    integer,parameter:: nStep = 1000
+    integer:: iStep
+    real:: DeltaTeSi, IntegralCgs, IntegralSi
+    real, dimension(0:nStep):: X_I, Y_I, Simpson_I
+    !-----------------
+    !Calculate the integral, \int{\sqrt{T_e}\Lambda{T_e}dT_e}:
+    !
+    DeltaTeSi = (TeTransitionRegionSi - TeChromosphereSi) / nStep
 
-  contains
+    X_I(0) = TeChromosphereSi
+    call get_cooling_function_fit(X_I(0), Y_I(0))
+    Simpson_I(0) = 2.0
 
-    subroutine get_cooling_function_fit(TeSi, CoolingFunctionCgs)
+    do iStep = 1,nStep
+       X_I(iStep) = X_I(iStep - 1) + DeltaTeSi
+       call get_cooling_function_fit(X_I(iStep), Y_I(iStep))
+       Simpson_I(iStep) = 6.0 - Simpson_I(iStep - 1)
+    end do
+    Simpson_I(0) = 1.0; Simpson_I(nStep) = 1.0
+    Y_I = Y_I * sqrt(X_I)
+    IntegralCgs = sum(Simpson_I*Y_I) * DeltaTeSi / 3.0
+    !\
+    !Transform to SI. Account for the transformation coefficient for n_e^2
+    !/
+    IntegralSi = IntegralCgs * Cgs2SiEnergyDens * 1.0e-12 
+    cooling_function_integral_si = IntegralSi
+  end function cooling_function_integral_si
 
-      ! Based on Rosner et al. (1978) and Peres et al. (1982)
-      ! Need to be replaced by Chianti tables
-
-      real, intent(in) :: TeSi
-      real, intent(out):: CoolingFunctionCgs
-      !------------------------------------------------------------------------
-
-      if(TeSi <= 8e3)then
-         CoolingFunctionCgs = (1.0606e-6*TeSi)**11.7
-      elseif(TeSi <= 2e4)then
-         CoolingFunctionCgs = (1.397e-8*TeSi)**6.15
-      elseif(TeSi <= 10**4.575)then
-         CoolingFunctionCgs = 10**(-21.85)
-      elseif(TeSi <= 10**4.9)then
-         CoolingFunctionCgs = 10**(-31.0)*TeSi**2
-      elseif(TeSi <= 10**5.4)then
-         CoolingFunctionCgs = 10**(-21.2)
-      elseif(TeSi <= 10**5.77)then
-         CoolingFunctionCgs = 10**(-10.4)/TeSi**2
-      elseif(TeSi <= 10**6.315)then
-         CoolingFunctionCgs = 10**(-21.94)
-      elseif(TeSi <= 10**7.60457)then
-         CoolingFunctionCgs = 10**(-17.73)/TeSi**(2.0/3.0)
-      else
-         CoolingFunctionCgs = 10**(-26.6)*sqrt(TeSi)
-      end if
-
-    end subroutine get_cooling_function_fit
-
-  end subroutine get_radiative_cooling
-
-  !============================================================================
 end module ModRadiativeCooling
