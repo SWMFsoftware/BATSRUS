b5394268f9b44686a9c420f94e7c6dfa0c02d3b3
xingm <>
2010-02-22 19:02:41 +0000

    Removed Pperp_.
    Former-commit-id: 9471a6900c30c0df17397d24bfbae5c3a6647763

--- src/ModExtraVariables.f90
+++ src/ModExtraVariables.f90
@@ -3,7 +3,7 @@ module ModExtraVariables
   ! Define indexes that are not used in most equation modules
 
   ! Set impossible value for indexes
-  integer, parameter :: Pe_ = 1, Ppar_ = 1, Pperp_ = 1
+  integer, parameter :: Pe_ = 1, Ppar_ = 1
   integer, parameter :: Hyp_ = 1, Erad_ = 1, ExtraEint_ = 1
   integer, parameter :: Ew_  = 1
 
