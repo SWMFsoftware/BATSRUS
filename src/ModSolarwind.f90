7cfa6af7f9aa590aa5d4a70a2626a634d757d97c
gtoth <>
2010-03-24 16:23:47 +0000

    Electron temperature (ratio) was incorrectly added to total ION pressure.
    Former-commit-id: 703bcae4241e3bf7f02e6fe1a8aa016b63d4e065

--- src/ModSolarwind.f90
+++ src/ModSolarwind.f90
@@ -403,7 +403,7 @@ contains
 
     use ModPhysics, ONLY: &
          Io2No_V, UnitTemperature_, UnitN_, UnitRho_, UnitP_, UnitU_, UnitB_, &
-         AverageIonCharge, ElectronTemperatureRatio,LowDensityRatio
+         AverageIonCharge, LowDensityRatio
     use ModMultiFluid
     use ModConst
 
@@ -467,10 +467,6 @@ contains
              Solarwind_V(p_) = Solarwind_V(p_)*Io2No_V(UnitP_)
           end if
        end if
-
-       ! Modify pressure with electron pressure
-       if(UseTemperature) Solarwind_V(p_) = Solarwind_V(p_) * &
-            (1.0 + AverageIonCharge*ElectronTemperatureRatio)
 
        ! Set or normalize other fluids for multi-fluid equations
        do iFluid = 2, nFluid
