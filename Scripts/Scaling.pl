44d58658bc829eee97fc6a8fff92684c3f94846c
gtoth <>
2010-09-21 21:42:56 +0000

    Added hera specific things to the scaling scripts. Modified Files: 	Scaling.pl Added Files: 	Scaling.job.hera
    Former-commit-id: 4015392217878df1dca5b77ff9bd9a1976d70257

--- Scripts/Scaling.pl
+++ Scripts/Scaling.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl -s -i
 
-my $CreateRundir = $create;
+my $CompileCode  = $compile;
+my $CreateRundir = $rundir;
 my $SetupRun     = $setup;
 my $SubmitRun    = $submit;
 my $Help         = ($h or $help);
@@ -10,31 +11,51 @@ use strict;
 if($Help){
     print "
 Edit Scripts/Scaling.pl and Scripts/Scaling.job* files as necessary.
-To create run directories, setup PARAM.in and job files, and submit runs:
+To compile the code, create run directories, setup PARAM.in and job files, 
+and submit all the runs:
 
-Scripts/Scaling.pl -create -setup -submit
+Scripts/Scaling.pl -compile -rundir -setup -submit
 
 You can do these steps separately too.
+The machine name is determined automatically.
+The script stops if it is not recognized.
 ";
     exit;
 }
 
+my $Machine = `hostname`;
+$Machine =~ s/\d*\n//;
+print "Machine=$Machine\n";
+my $IsHera;
+my $IsPfe;
+$IsHera = 1  if $Machine eq "hera";
+$IsPfe =  1  if $Machine eq "pfe";
+
+die "Unknown machine=$Machine\n" unless $IsHera or $IsPfe;
 
 # Number of cores to run on
 my @nCore;
 @nCore  = (8,64,512,4096);
 
 # Where to put the run directories
-my $Dir = "/nobackup/gtoth1/SCALING";
-`mkdir -p $Dir`;
+# my $Dir = "/nobackup/gtoth1/SCALING";
+my $Dir = "SCALING";
+
+my $JobScript = "Scripts/Scaling.job".$Machine;
+
+if($CompileCode){
+    `Config.pl -e=HdCrash -u=Crash -g=4,4,4,1000,1`;
+    `make CRASH PIDL`;
+}
 
 my $nCore;
 if($CreateRundir){
+    `mkdir -p $Dir`;
     foreach $nCore (@nCore){
 	`make rundir DEFAULT_EXE=CRASH.exe`;
 	`gunzip -c dataCRASH/input/hyades2d_1.3ns.out.gz > run/hyades2d_1.3ns.out`;
 	`cp Param/CRASH/PARAM.in.hydro_scaling run/PARAM.in`;
-	`cp Scripts/Scaling.job.pfe run/job`;
+	`cp $JobScript run/job`;
 	`mv run $Dir/run_n$nCore`;
     }
 }
@@ -52,12 +73,20 @@ if($SetupRun){
 	    print;
 	}
 
-	`cp Scripts/Scaling.job.pfe $rundir/job`;
-	my $nNode = $nCore/8;
 	@ARGV = ("$rundir/job");
-	while(<>){
-	    s/(\#PBS -l select)=\d+/$1=$nNode/;
-	    print;
+	if($IsPfe){
+	    my $nNode = int($nCore/8+0.99);
+	    while(<>){
+		s/(\#PBS -l select)=\d+/$1=$nNode/;
+		print;
+	    }
+	}elsif($IsHera){
+	    my $nNode = int($nCore/16+0.99);
+	    while(<>){
+                s/(\#MSUB -l nodes)=\d+/$1=$nNode/;
+		s/NNN/$nCore/;
+                print;
+            }
 	}
     }
 }
@@ -65,6 +94,7 @@ if($SetupRun){
 if($SubmitRun){
     foreach $nCore (@nCore){
 	my $rundir = "$Dir/run_n$nCore";
-	`cd $rundir; qsub job`;
+	`cd $rundir; qsub job` if $IsPfe;
+	`cd $rundir; msub job | tail -1 > jobid`;
     }
 }
