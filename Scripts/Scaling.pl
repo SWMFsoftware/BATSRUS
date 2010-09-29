91db4f8448ae439f4f4f368ae9308d995c7085ba
gtoth <>
2010-09-29 20:08:14 +0000

    A cleaned-up version with help message and improved set of options.
    Former-commit-id: bf8cb49dba1626352d99961ebe7e5853da94d971

--- Scripts/Scaling.pl
+++ Scripts/Scaling.pl
@@ -1,27 +1,16 @@
 #!/usr/bin/perl -s -i
 
+my $Help         = ($h or $help);
+&print_help if $Help;
+
+my $Verbose      = $v;
+my $Dryrun       = $n;
+my $WeakScaling  = $weak;
 my $CompileCode  = $compile;
 my $CreateRundir = $rundir;
-my $SetupRun     = $setup;
 my $SubmitRun    = $submit;
-my $Help         = ($h or $help);
 
 use strict;
-
-if($Help){
-    print "
-Edit Scripts/Scaling.pl and Scripts/Scaling.job* files as necessary.
-To compile the code, create run directories, setup PARAM.in and job files, 
-and submit all the runs:
-
-Scripts/Scaling.pl -compile -rundir -setup -submit
-
-You can do these steps separately too.
-The machine name is determined automatically.
-The script stops if it is not recognized.
-";
-    exit;
-}
 
 my $Machine = `hostname`;
 $Machine =~ s/\d*\n//;
@@ -33,71 +22,186 @@ $IsPfe =  1  if $Machine eq "pfe";
 
 die "Unknown machine=$Machine\n" unless $IsHera or $IsPfe;
 
-# Number of cores to run on
+# Number of nodes and cores to run on
+my $nNode;
+my $nCore;
 my @nCore;
-@nCore  = (8,64,512,4096);
+if($WeakScaling){
+    @nCore  = (8,64,512,4096);
+}else{
+    @nCore  = (128,256,512,1024,2048,4096,8192);
+}
+
+# Input file
+my $HyadesFile = "hyades2d_1.3ns.out";
 
 # Where to put the run directories
 # my $Dir = "/nobackup/gtoth1/SCALING";
 my $Dir = "SCALING";
+&shell("mkdir -p $Dir");
+my $Rundir = "SCALING/run";
 
-my $JobScript = "Scripts/Scaling.job".$Machine;
+my $JobScript = "Scripts/Scaling.job.".$Machine;
 
-if($CompileCode){
-    `Config.pl -e=HdCrash -u=Crash -g=4,4,4,700,1`;
-    `make CRASH PIDL`;
-}
+if(not $WeakScaling){
 
-my $nCore;
-if($CreateRundir){
-    `mkdir -p $Dir`;
-    foreach $nCore (@nCore){
-	`make rundir DEFAULT_EXE=CRASH.exe`;
-	`gunzip -c dataCRASH/input/hyades2d_1.3ns.out.gz > run/hyades2d_1.3ns.out`;
-	`cp Param/CRASH/PARAM.in.hydro_scaling run/PARAM.in`;
-	`cp $JobScript run/job`;
-	`mv run $Dir/run_n$nCore`;
-    }
-}
-
-if($SetupRun){
-    foreach $nCore (@nCore){
-	my $res = $nCore**(1/3);
-	my $nRootX   = $res*40;
-	my $nRootYZ  = $res*4;
-	my $rundir = "$Dir/run_n$nCore";
-
-	# Set grid size in PARAM.in
-	@ARGV = ("$rundir/PARAM.in");
-	while(<>){
-	    s/\d+(\s+nRootBlockX)/$nRootX$1/;
-	    s/\d+(\s+nRootBlock[YZ])/$nRootYZ$1/;
-	    print;
-	}
-
-	# Set number of nodes/cores in job
-	@ARGV = ("$rundir/job");
-	if($IsPfe){
-	    my $nNode = int($nCore/8+0.99);
+    # Strong scaling uses a single run directory with many 
+    # executables, plot directories and job scripts
+    if($CreateRundir){
+	&make_rundir;
+	# Modify PARAM.in
+	if($Dryrun){
+	    print "edit $Rundir/PARAM.in\n";
+	}else{
+	    @ARGV = ("$Rundir/PARAM.in");
 	    while(<>){
-		s/(\#PBS -l select)=\d+/$1=$nNode/;
+		s/^PLOTDIR/\#PLOTDIR/;  # use separate plot directories
+		s/^AMR/\#AMR/;          # switch on AMR
 		print;
 	    }
-	}elsif($IsHera){
-	    my $nNode = int($nCore/16+0.99);
-	    while(<>){
-                s/(\#MSUB -l nodes)=\d+/$1=$nNode/;
-		s/(run_n|srun \-n)\d+/$1$nCore/;
-                print;
-            }
+	}
+
+	foreach $nCore (@nCore){
+	    $nNode = $nCore/8  if $IsPfe;
+	    $nNode = $nCore/16 if $IsHera;
+	    &shell("cp $JobScript $Rundir/job_$nCore");
+	    &edit_jobscript("$Rundir/job_$nCore");
+	    &shell("mkdir $Rundir/plot_$nCore/");
+	}
+	print "Creating $Rundir done\n";
+    }
+    if($CompileCode){
+	&shell("Config.pl -e=HdCrash -u=Crash -nMaterial=3");
+	foreach $nCore (@nCore){
+	    my $nBlock = int(2*196608/$nCore + 0.99);
+	    print "Compiling $Rundir/CRASH_$nCore.exe for nBlock=$nBlock\n";
+	    &shell("Config.pl -g=4,4,4,$nBlock,1");
+	    &shell("make CRASH");
+	    &shell("cp src/CRASH.exe $Rundir/CRASH_$nCore.exe");
+	}
+	print "Compilations done\n";
+    }
+
+    if($SubmitRun){
+	foreach $nCore (@nCore){
+	    &submit_run($Rundir,"job_$nCore");
+        }
+    }
+
+}else{
+    # Weak scaling uses many run directories and a single executable
+    if($CompileCode){
+	&shell("Config.pl -e=HdCrash -u=Crash -g=4,4,4,700,1");
+	&shell("make CRASH");
+    }
+
+    if($CreateRundir){
+	foreach $nCore (@nCore){
+	    $Rundir = "$Dir/run_n$nCore";
+	    &make_rundir;
+	    &shell("cp $JobScript $Rundir/job");
+
+	    my $res = $nCore**(1/3);
+	    my $nRootX   = $res*40;
+	    my $nRootYZ  = $res*4;
+	    my $rundir = "$Dir/run_n$nCore";
+
+	    if($Dryrun){
+		print "edit $Rundir/PARAM.in\n";
+	    }else{
+		# Set grid size in PARAM.in
+		@ARGV = ("$Rundir/PARAM.in");
+		while(<>){
+		    s/\d+(\s+nRootBlockX)/$nRootX$1/;
+		    s/\d+(\s+nRootBlock[YZ])/$nRootYZ$1/;
+		    print;
+		}
+	    }
+	    # Set number of nodes/cores in job
+	    &edit_jobscript("$rundir/job");
+	}
+    }
+
+    if($SubmitRun){
+	foreach $nCore (@nCore){
+	    &submit_run("$Dir/run_n$nCore","job");
 	}
     }
 }
 
-if($SubmitRun){
-    foreach $nCore (@nCore){
-	my $rundir = "$Dir/run_n$nCore";
-	`cd $rundir; qsub job`                   if $IsPfe;
-	`cd $rundir; msub job | tail -1 > jobid` if $IsHera;
+exit;
+#################################################################################
+sub make_rundir{
+    die "$Rundir already exists\n" if -d $Rundir and not $Dryrun;
+    &shell("make rundir RUNDIR=$Rundir DEFAULT_EXE=CRASH.exe");
+    &shell("gunzip -c dataCRASH/input/$HyadesFile.gz > $Rundir/$HyadesFile");
+    &shell("cp Param/CRASH/PARAM.in.hydro_scaling $Rundir/PARAM.in");
+}
+#################################################################################
+sub submit_run{
+    my $rundir = shift;
+    my $job    = shift;
+    &shell("cd $rundir; qsub $job") if $IsPfe;
+    &shell("cd $rundir; msub $job | tail -1 > ${job}id") if $IsHera;
+}
+#################################################################################
+sub edit_jobscript{
+    my $jobscript = shift;
+
+    if($Dryrun){
+	print "edit $jobscript\n";
+	return;
     }
+
+    @ARGV = ($jobscript);
+    while(<>){
+	if($IsPfe){
+	    $nNode = int($nCore/8+0.99);
+	    # Change number of nodes
+	    s/(^\#PBS -l select)=\d+/$1=$nNode/;
+	    s/(^\#PBS -q) normal/$1 wide/ if $nCore >= 1024;
+	    s/(^\#PBS -q) wide/$1 normal/ if $nCore <  1024;
+	}elsif($IsHera){
+	    my $nNode = int($nCore/16+0.99);
+	    s/(\#MSUB -l nodes)=\d+/$1=$nNode/;
+	    s/(run_n|srun \-n)\d+/$1$nCore/;
+	}
+	if(not $WeakScaling){
+	    # Change plot directory
+	    s/plot_\d+/plot_$nCore/;
+	    # Change executable and runlog filenames
+	    s/CRASH\w*\.exe > runlog\w*/CRASH_$nCore.exe > runlog_$nCore/;
+	}
+	print;
+    }
+}
+#################################################################################
+sub shell{
+    my $command = shift;
+    print "$command\n" if $Verbose;
+    `$command` unless $Dryrun;
+}
+#################################################################################
+sub print_help{
+    print "
+This script can setup weak and strong scaling runs on hera and Pleiades.
+It maybe necessary to edit Scripts/Scaling.pl and Scripts/Scaling.job* 
+files to customize the code. 
+
+Usage:
+
+Scripts/Scaling.pl [-v] [-n] [-weak] [-compile] [-rundir] [-setup] [-submit]
+
+-v        Switch on verbose mode.
+-n        Switch on dry mode (do not execute commands).
+-weak     Do weak scaling. Default is strong scaling.
+-compile  Compile the executables.
+-rundir   Create the run directory/directories.
+-submit   Submit the jobs.
+
+You can do these steps separately too.
+The machine name is determined automatically.
+The script stops if it is not recognized.
+";
+    exit;
 }
