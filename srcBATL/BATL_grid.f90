ebc706e089e92a67a1283dfc513a60bc1b2cb01e
gtoth <>
2011-01-14 16:20:42 +0000

    Generalized the interpolation algorithm from get_point_data in BATSRUS. Verified second order accuracy except at double resolution changes. More optimal code can be written by finishing interpolate_tree in BATL_tree.
    Former-commit-id: 5fd288b2df7fdd54c86cff90c3151cc0dd9baed6

--- srcBATL/BATL_grid.f90
+++ srcBATL/BATL_grid.f90
@@ -4,7 +4,7 @@ module BATL_grid
   use BATL_size
   use BATL_tree
   use BATL_geometry, ONLY: &
-       IsCartesian, IsRzGeometry, TypeGeometry, xyz_to_coord
+       IsCartesian, IsRzGeometry, TypeGeometry, xyz_to_coord, IsPeriodic_D
 
   implicit none
 
@@ -15,6 +15,7 @@ module BATL_grid
   public :: create_grid
   public :: create_grid_block
   public :: find_grid_block
+  public :: interpolate_grid
   public :: test_grid
 
   real, public:: &
@@ -109,7 +110,7 @@ contains
     CoordMax_DB(:,iBlock)= CoordMin_D + (CoordMax_D - CoordMin_D)*PositionMax_D
 
     CellSize_DB(:,iBlock) = (CoordMax_DB(:,iBlock) - CoordMin_DB(:,iBlock)) &
-         / nIJK_D
+         / nIjk_D
 
     if(IsCartesian .or. IsRzGeometry)then
        ! For RZ geometry this is true in generalized coordinate sense only
@@ -222,20 +223,21 @@ contains
 
   !===========================================================================
 
-  subroutine find_grid_block(XyzIn_D, iProcOut, iBlockOut, IjkOut_D, DistOut_D)
+  subroutine find_grid_block(XyzIn_D, &
+       iProcOut, iBlockOut, iCellOut_D, DistOut_D)
 
     ! Find the processor and block containing location XyzIn_D. 
-    ! If present, IjkOut_D returns the indexes of the closest cell center:
-    ! 1 <= IjkOut_D <= nIjk_D
+    ! If present, iCellOut_D returns the indexes of the closest cell center:
+    ! 1 <= iCellOut_D <= nIjk_D
     ! If present, DistOut_D returns the signed distance to the closest 
     ! cell center divided by the cell size.
-    ! DistOut_D can only be present if IjkOut_D is also present.
+    ! DistOut_D can only be present if iCellOut_D is also present.
 
     real,    intent(in) :: XyzIn_D(MaxDim)        ! Cartesian coords of point
     integer, intent(out):: iBlockOut, iProcOut    ! Block and proc indexes
 
-    integer, intent(out), optional:: IjkOut_D(MaxDim) ! Closest cell indexes
-    real,    intent(out), optional:: DistOut_D(MaxDim) ! Normalized distance
+    integer, intent(out), optional:: iCellOut_D(MaxDim) ! Closest cell indexes
+    real,    intent(out), optional:: DistOut_D(MaxDim)  ! Normalized distance
 
     real:: CoordTree_D(MaxDim), Coord_D(MaxDim)
     integer:: iNode
@@ -252,8 +254,8 @@ contains
     CoordTree_D = (Coord_D - CoordMin_D)/(CoordMax_D - CoordMin_D)
 
     ! Find node containing the point
-    if(present(IjkOut_D))then
-       call find_tree_cell(CoordTree_D, iNode, IjkOut_D, DistOut_D)
+    if(present(iCellOut_D))then
+       call find_tree_cell(CoordTree_D, iNode, iCellOut_D, DistOut_D)
     else
        call find_tree_node(CoordTree_D, iNode)
     end if
@@ -271,12 +273,200 @@ contains
   end subroutine find_grid_block
 
   !===========================================================================
+  subroutine interpolate_grid(Xyz_D, nCell, iCell_II, Weight_I)
+
+    ! Find the grid cells surrounding the point Xyz_D.
+    ! nCell returns the number of cells found on the processor.
+    ! iCell_II returns the block+cell indexes for each cell.
+    ! Weight_I returns the interpolation weights
+
+    real,    intent(in) :: Xyz_D(MaxDim)
+    integer, intent(out):: nCell  
+    integer, intent(out):: iCell_II(0:nDim,2**nDim)
+    real,    intent(out):: Weight_I(2**nDim)
+
+    real:: Coord_D(MaxDim), CoordOrig_D(MaxDim), CoordCell_D(nDim)
+    real:: CoordMin, CoordMax, Shift
+    real:: CellSize_D(nDim), BufferLo_D(nDim), BufferHi_D(nDim)
+    real:: InvSize_D(nDim), Weight_D(nDim), Weight
+    integer:: iBlock, iDim, DiLevel, iCell_D(MaxDim)
+    integer:: i, j, k, iLo, jLo, kLo, iHi, jHi, kHi
+
+    logical, parameter:: DoTest = .false.
+    !------------------------------------------------------------------------
+    ! Convert to generalized coordinates if necessary
+    if(IsCartesian .or. IsRzGeometry)then
+       Coord_D = Xyz_D
+    else
+       call xyz_to_coord(Xyz_D, Coord_D)
+    end if
+
+    ! Initialize values in case no cells are found on this processor
+    nCell    = 0
+    iCell_II = 0
+    Weight_I = 0
+
+    ! For periodic boundaries we may have to shift the coordinates
+    ! Since this shift varies from block to block, store the original
+    if(any(IsPeriodic_D)) CoordOrig_D = Coord_D
+
+
+    LOOPBLOCK: do iBlock = 1, nBlock
+
+       if(Unused_B(iBlock)) CYCLE
+
+       CellSize_D = CellSize_DB(1:nDim,iBlock)
+
+       ! Initialize with unshifted coordinates
+       if(any(IsPeriodic_D)) Coord_D = CoordOrig_D
+
+       ! Set buffer zone according to relative size of neighboring block
+       do iDim = 1, nDim
+
+          ! Block at the lower index side
+          select case(iDim)
+          case(1)
+             DiLevel = DiLevelNei_IIIB(-1,0,0,iBlock)
+          case(2)
+             DiLevel = DiLevelNei_IIIB(0,-1,0,iBlock)
+          case(3)
+             DiLevel = DiLevelNei_IIIB(0,0,-1,iBlock)
+          end select
+
+          select case(DiLevel)
+          case(1)
+             BufferLo_D(iDim) = 0.5*CellSize_D(iDim)*iRatio_D(iDim)
+          case(-1)
+             BufferLo_D(iDim) = 0.5*CellSize_D(iDim)/iRatio_D(iDim)
+          case default
+             BufferLo_D(iDim) = 0.5*CellSize_D(iDim)
+          end select
+
+          ! Lower limit on point coordinate
+          CoordMin = CoordMin_DB(iDim,iBlock) - BufferLo_D(iDim)
+
+          ! Check if point is inside the buffer zone on the lower side
+          if(Coord_D(iDim) < CoordMin .and. .not.IsPeriodic_D(iDim)) &
+               CYCLE LOOPBLOCK
+
+          ! Block at the upper index side
+          select case(iDim)
+          case(1)
+             DiLevel = DiLevelNei_IIIB(+1,0,0,iBlock)
+          case(2)
+             DiLevel = DiLevelNei_IIIB(0,+1,0,iBlock)
+          case(3)
+             DiLevel = DiLevelNei_IIIB(0,0,+1,iBlock)
+          end select
+
+          select case(DiLevel)
+          case(1)
+             BufferHi_D(iDim) = 0.5*CellSize_D(iDim)*iRatio_D(iDim)
+          case(-1)
+             BufferHi_D(iDim) = 0.5*CellSize_D(iDim)/iRatio_D(iDim)
+          case default
+             BufferHi_D(iDim) = 0.5*CellSize_D(iDim)
+          end select
+
+          ! Upper limit on point coordinate
+          CoordMax = CoordMax_DB(iDim,iBlock) + BufferHi_D(iDim)
+
+          ! Check if point is inside the buffer zone on the lower side
+          if(Coord_D(iDim) > CoordMax .and. .not.IsPeriodic_D(iDim)) &
+               CYCLE LOOPBLOCK
+
+          ! Done unless periodic
+          if(.not.IsPeriodic_D(iDim)) CYCLE
+
+          ! Shift coordinate and try check again
+          Shift = CoordMax_D(iDim) - CoordMin_D(iDim)
+          if(Coord_D(iDim) < CoordMin) Coord_D(iDim) = Coord_D(iDim) + Shift
+          if(Coord_D(iDim) > CoordMax) Coord_D(iDim) = Coord_D(iDim) - Shift
+          if(Coord_D(iDim) < CoordMin .or. Coord_D(iDim) > CoordMax) &
+               CYCLE LOOPBLOCK
+
+       end do
+
+       ! Find closest cell center indexes towards the lower index direction
+       iCell_D = 1 ! for ignored dimensions
+       iCell_D(1:nDim) = floor(0.5 + &
+            (Coord_D(1:nDim) - CoordMin_DB(1:nDim,iBlock))/CellSize_D )
+
+       ! Set inverse distance between interpolation cells
+       do iDim = 1, nDim
+          if(iCell_D(iDim) < 1)then
+             iCell_D(iDim) = 0
+             InvSize_D(iDim) = 1/(0.5*CellSize_D(iDim) + BufferLo_D(iDim))
+          elseif(iCell_D(iDim) >= nIjk_D(iDim))then
+             iCell_D(iDim) = nIjk_D(iDim)
+             InvSize_D(iDim) = 1/(0.5*CellSize_D(iDim) + BufferHi_D(iDim))
+          else
+             InvSize_D(iDim) = 1/CellSize_D(iDim)
+          end if
+       end do
+
+       ! Index range of surrounding cell centers
+       iLo = iCell_D(1); iHi = iLo + 1
+       jLo = iCell_D(2); jHi = jLo + 1
+       kLo = iCell_D(3); kHi = kLo + 1
+
+       ! Index range limited to physical and outer boundary ghost cells
+       if(            DiLevelNei_IIIB(-1,0,0,iBlock)/=Unset_) iLo = max(1,iLo)
+       if(nDim<2 .or. DiLevelNei_IIIB(0,-1,0,iBlock)/=Unset_) jLo = max(1,jLo)
+       if(nDim<3 .or. DiLevelNei_IIIB(0,0,-1,iBlock)/=Unset_) kLo = max(1,kLo)
+
+       if(            DiLevelNei_IIIB(+1,0,0,iBlock)/=Unset_) iHi = min(nI,iHi)
+       if(nDim<2 .or. DiLevelNei_IIIB(0,+1,0,iBlock)/=Unset_) jHi = min(nJ,jHi)
+       if(nDim<3 .or. DiLevelNei_IIIB(0,0,+1,iBlock)/=Unset_) kHi = min(nK,kHi)
+
+       if(DoTest)then
+          write(*,*)'!!! iProc, iBlock, iCell_D, Coord_D=', &
+               iProc, iBlock, iCell_D, Coord_D(1:nDim)
+          write(*,*)'!!! iLo, iHi, jLo, jHi=',iLo, iHi, jLo, jHi
+       end if
+
+       ! Calculate the weights and store it together with index information
+       do k = kLo, kHi; do j = jLo, jHi; do i = iLo, iHi
+          iCell_D = (/ i, j, k /)
+
+          CoordCell_D = CoordMin_DB(1:nDim,iBlock) &
+               + ( iCell_D(1:nDim) - 0.5 )*CellSize_D
+
+          Weight_D = 1 - InvSize_D*abs((Coord_D(1:nDim) - CoordCell_D))
+          
+          Weight = product(Weight_D)
+
+          ! Ignore cells with 0 weight
+          if(Weight <= 0.0) CYCLE
+
+          nCell = nCell + 1
+
+          if(nCell > 2**nDim)then
+             write(*,*)'ERROR in interpolate_grid: too many cells!'
+             write(*,*)'iProc,iBlock,nDim=',iProc,iBlock,nDim
+             write(*,*)'iLo,iHi,jLo,jHi,kLo,kHi=',iLo,iHi,jLo,jHi,kLo,kHi
+             write(*,*)'Coord_D=',Coord_D(1:nDim)
+          end if
+
+          Weight_I(nCell)   = Weight
+          iCell_II(0,nCell) = iBlock
+          iCell_II(1:nDim,nCell) = iCell_D(1:nDim)
+
+          if(DoTest)write(*,*)'!!! nCell, CoordCell, Weight=', &
+               nCell, CoordCell_D(1:nDim), Weight
+
+       end do; end do; end do
+    end do LOOPBLOCK
+
+  end subroutine interpolate_grid
+  !===========================================================================
 
   subroutine test_grid
 
-    use BATL_mpi, ONLY: iProc
+    use BATL_mpi, ONLY: iProc, nProc, iComm
     use BATL_geometry, ONLY: init_geometry
     use ModNumConst, ONLY: i_DD
+    use ModMpi, ONLY: MPI_allreduce, MPI_real, MPI_sum
 
     integer :: iBlock
 
@@ -286,9 +476,23 @@ contains
     real:: DomainMin_D(MaxDim) = (/ 3.0, 2.0, 1.0 /)
     real:: DomainMax_D(MaxDim) = (/ 9.0, 6.0, 4.0 /)
 
-    real, parameter:: Tolerance = 1e-6
+    ! number of points in each dimension to test interpolation
+    integer, parameter:: &
+         nPointI = 51, &
+         nPointJ = 1 + 50*min(1,nDim-1), &
+         nPointK = 1 + 50*max(0,nDim-2)
 
-    integer:: i, j, k, Di, Dj, iDim, iBlockOut, iProcOut, IjkOut_D(MaxDim)
+    integer, parameter:: nPoint_D(MaxDim) = (/ nPointI, nPointJ, nPointK /)
+    real, allocatable:: Point_VIII(:,:,:,:), PointAll_VIII(:,:,:,:)
+    integer, parameter:: nVarPoint = nDim
+    real:: XyzPoint_D(MaxDim), Point_V(nVarPoint), Weight
+    integer:: iPoint, jPoint, kPoint, iPoint_D(MaxDim), iCell, nCell, iError
+    integer:: iCell_II(0:nDim,2**nDim)
+    real   :: Weight_I(2**nDim)
+
+    real:: Tolerance
+
+    integer:: i, j, k, Di, Dj, iDim, iBlockOut, iProcOut, iCell_D(MaxDim)
     real:: Radius, Xyz_D(MaxDim), Distance_D(MaxDim)
     real, allocatable:: CellVolumeCart_B(:), CellFaceCart_DB(:,:)
 
@@ -317,45 +521,45 @@ contains
 
     if(DoTestMe) write(*,*)'Testing find_grid_block'
     call find_grid_block(DomainMin_D, iProcOut, iBlockOut, &
-         IjkOut_D, Distance_D)
+         iCell_D, Distance_D)
     if(iProc == iProcOut) then
-       Xyz_D = Xyz_DGB(:,IjkOut_D(1),IjkOut_D(2),IjkOut_D(3),iBlockOut) &
+       Xyz_D = Xyz_DGB(:,iCell_D(1),iCell_D(2),iCell_D(3),iBlockOut) &
             - 0.5*CellSize_DB(:,iBlockOut)
        if(any(abs(DomainMin_D(1:nDim) - Xyz_D(1:nDim)) > 1e-6)) then
           write(*,*) 'Error: DomainMin_D, Xyz_D=', &
                DomainMin_D, Xyz_D
-          write(*,*) 'iProcOut, iBlockOut, IjkOut_D = ',&
-               iProcOut, iBlockOut, IjkOut_D
+          write(*,*) 'iProcOut, iBlockOut, iCell_D = ',&
+               iProcOut, iBlockOut, iCell_D
        end if
     end if
 
-    if(any(IjkOut_D(1:nDim) /= 1)) then
-       write(*,*) 'Error: IjkOut_D=', IjkOut_D(1:nDim),' should be 1'
+    if(any(iCell_D(1:nDim) /= 1)) then
+       write(*,*) 'Error: iCell_D=', iCell_D(1:nDim),' should be 1'
        write(*,*) 'iProcOut, iBlockOut, Distance_D = ',&
             iProcOut, iBlockOut, Distance_D
     end if
 
     if(any(abs(Distance_D(1:nDim) + 0.5) > 1e-6)) then
        write(*,*) 'Error: Distance_D=', Distance_D(1:nDim),' should be -0.5'
-       write(*,*) 'iProcOut, iBlockOut, IjkOut_D = ',&
-            iProcOut, iBlockOut, IjkOut_D
+       write(*,*) 'iProcOut, iBlockOut, iCell_D = ',&
+            iProcOut, iBlockOut, iCell_D
     end if
 
     call find_grid_block(DomainMax_D, iProcOut, iBlockOut, &
-         IjkOut_D, Distance_D)
+         iCell_D, Distance_D)
     if(iProc == iProcOut) then
-       Xyz_D = Xyz_DGB(:,IjkOut_D(1),IjkOut_D(2),IjkOut_D(3),iBlockOut) &
+       Xyz_D = Xyz_DGB(:,iCell_D(1),iCell_D(2),iCell_D(3),iBlockOut) &
             + 0.5*CellSize_DB(:,iBlockOut)
        if(any(abs(DomainMax_D(1:nDim) - Xyz_D(1:nDim)) > 1e-6)) then
           write(*,*) 'Error: DomainMax_D, Xyz_D=', &
                DomainMax_D, Xyz_D
-          write(*,*) 'iProcOut, iBlockOut, IjkOut_D = ',&
-                 iProcOut, iBlockOut, IjkOut_D
+          write(*,*) 'iProcOut, iBlockOut, iCell_D = ',&
+                 iProcOut, iBlockOut, iCell_D
        end if
     end if
 
-    if(any(IjkOut_D(1:nDim) /= nIJK_D(1:nDim))) then
-       write(*,*) 'Error: IjkOut_D=', IjkOut_D(1:nDim), &
+    if(any(iCell_D(1:nDim) /= nIJK_D(1:nDim))) then
+       write(*,*) 'Error: iCell_D=', iCell_D(1:nDim), &
             ' should be ', nIJK_D(1:nDim)
        write(*,*) 'iProcOut, iBlockOut, Distance_D = ',&
             iProcOut, iBlockOut, Distance_D
@@ -363,8 +567,90 @@ contains
 
     if(any(abs(Distance_D(1:nDim) - 0.5) > 1e-6)) then
        write(*,*) 'Error: Distance_D=', Distance_D(1:nDim),' should be +0.5'
-       write(*,*) 'iProcOut, iBlockOut, IjkOut_D = ',&
-            iProcOut, iBlockOut, IjkOut_D
+       write(*,*) 'iProcOut, iBlockOut, iCell_D = ',&
+            iProcOut, iBlockOut, iCell_D
+    end if
+
+    if(DoTestMe) write(*,*)'Testing interpolate_grid'
+    Xyz_D = 0.0
+    if(.not.allocated(Point_VIII)) &
+         allocate(Point_VIII(0:nVarPoint,nPointI,nPointJ,nPointK))
+    Point_VIII = 0.0
+    do kPoint = 1, nPointK; do jPoint = 1, nPointJ; do iPoint = 1, nPointI
+       iPoint_D = (/ iPoint, jPoint, kPoint /)
+       XyzPoint_D(1:nDim) = CoordMin_D(1:nDim) + (iPoint_D(1:nDim)-0.5) &
+            *(CoordMax_D(1:nDim) - CoordMin_D(1:nDim))/nPoint_D(1:nDim)
+
+       call interpolate_grid(XyzPoint_D, nCell, iCell_II, Weight_I)
+       do iCell = 1, nCell
+          Point_VIII(0,iPoint,jPoint,kPoint) = &
+               Point_VIII(0,iPoint,jPoint,kPoint) + Weight_I(iCell)
+          iBlock = iCell_II(0,iCell)
+          iCell_D = 1
+          iCell_D(1:nDim) = iCell_II(1:nDim,iCell)
+          
+          ! Interpolate the coordinates to check order of accuracy
+          Xyz_D(1:nDim) = &
+               Xyz_DGB(1:nDim,iCell_D(1),iCell_D(2),iCell_D(3),iBlock)
+
+          ! Take care of periodic dimensions: shift coordinates as necessary
+          do iDim = 1, nDim
+             if(.not.IsPeriodicTest_D(iDim)) CYCLE
+             if(XyzPoint_D(iDim) < Xyz_D(iDim) - 2*CellSize_DB(iDim,iBlock)) &
+                  Xyz_D(iDim) = Xyz_D(iDim) - &
+                  (CoordMax_D(iDim) - CoordMin_D(iDim))
+
+             if(XyzPoint_D(iDim) > Xyz_D(iDim) + 2*CellSize_DB(iDim,iBlock)) &
+                  Xyz_D(iDim) = Xyz_D(iDim) + &
+                  (CoordMax_D(iDim) - CoordMin_D(iDim))
+          end do
+
+          Point_VIII(1:nDim,iPoint,jPoint,kPoint) = &
+               Point_VIII(1:nDim,iPoint,jPoint,kPoint) &
+               + Weight_I(iCell)*Xyz_D(1:nDim)
+
+          if(.false..and.iPoint==7.and.jPoint==1) write(*,*) &
+               '!!!iProc,ijkPoint,iCell,iCell_D,iBlock,Weight,Xyz,Point=',&
+               iProc, iPoint_D(1:nDim), iCell, iCell_D(1:nDim), &
+               iBlock, Weight_I(iCell), &
+               Xyz_DGB(1:nDim,iCell_D(1),iCell_D(2),iCell_D(3),iBlock), &
+               Point_VIII(1:nDim,iPoint,jPoint,kPoint)
+       end do
+    end do; end do; end do
+
+    ! Collect contributions from all processors to proc 0
+    if(nProc > 1)then
+       allocate(PointAll_VIII(0:nVarPoint,nPointI,nPointJ,nPointK))
+       call MPI_reduce(Point_VIII, PointAll_VIII, size(PointAll_VIII),&
+            MPI_REAL, MPI_SUM, 0, iComm, iError)
+       Point_VIII = PointAll_VIII
+       deallocate(PointAll_VIII)
+    end if
+
+    if(iProc == 0)then
+       ! Check interpolated coordinate values against point coordinates
+       do kPoint = 1, nPointK; do jPoint = 1, nPointJ; do iPoint = 1, nPointI
+          iPoint_D = (/ iPoint, jPoint, kPoint /)
+          Xyz_D(1:nDim) = CoordMin_D(1:nDim) + (iPoint_D(1:nDim)-0.5) &
+               *(CoordMax_D(1:nDim) - CoordMin_D(1:nDim))/nPoint_D(1:nDim)
+
+          Weight  = Point_VIII(0,iPoint,jPoint,kPoint)
+          Point_V = Point_VIII(1:nDim,iPoint,jPoint,kPoint)/Weight
+
+          if(abs(Weight - 1.0) < 1e-6)then
+             Tolerance = 1e-6
+          else
+             Tolerance = 3e-2
+          end if
+
+          if(any(abs(Xyz_D(1:nDim) - Point_V) > Tolerance))then
+             write(*,*) 'ERROR: Point_V=',Point_V(1:nDim),&
+                  ' should be ',Xyz_D(1:nDim)
+             write(*,*) 'Total weight=',Weight
+             write(*,*) 'i,j,kPoint=', iPoint_D(1:nDim)
+             write(*,*) 'CoordMin,Max=',CoordMin_D(1:nDim),CoordMax_D(1:nDim)
+          end if
+       end do; end do; end do
     end if
 
     if(nDim == 2)then
@@ -385,6 +671,7 @@ contains
        call show_grid
 
        ! Check relative to Cartesian
+       Tolerance = 1e-6
        do iBlock = 1, nBlock
           do k = MinK, MaxK; do j = MinJ, MaxJ; do i = MinI, MaxI
              if(abs( CellVolume_GB(i,j,k,iBlock) &
