e06c234cab78649fb86f98c9dec7ed148875c193
daldorff <>
2011-02-24 17:19:53 +0000

    Added wrapper for passing scalars and included min and max operator for do_restrict
    Former-commit-id: d71ce6d08c365d60f3c755d1f66e32b63d106496

--- srcBATL/BATL_pass_cell.f90
+++ srcBATL/BATL_pass_cell.f90
@@ -31,9 +31,57 @@ module BATL_pass_cell
 
 contains
 
+  subroutine message_pass_scalar_cell(Scalar_GB, &
+       nWidthIn, nProlongOrderIn, nCoarseLayerIn, DoSendCornerIn, &
+       DoRestrictFaceIn, TimeOld_B, Time_B, DoTestIn, NameOperatorIn)
+    ! Wrapper function for making it easy to pass scalar data to
+    ! message_pass_cell
+
+    use BATL_size, ONLY: MaxBlock, MinI, MaxI, MinJ, MaxJ, MinK, MaxK
+
+    ! Arguments
+    real, intent(inout) :: &
+         Scalar_GB(MinI:MaxI,MinJ:MaxJ,MinK:MaxK,MaxBlock)
+
+    ! Optional arguments
+    integer, optional, intent(in) :: nWidthIn
+    integer, optional, intent(in) :: nProlongOrderIn
+    integer, optional, intent(in) :: nCoarseLayerIn
+    logical, optional, intent(in) :: DoSendCornerIn
+    logical, optional, intent(in) :: DoRestrictFaceIn
+    logical, optional, intent(in) :: DoTestIn
+    real,    optional, intent(in) :: TimeOld_B(MaxBlock)
+    real,    optional, intent(in) :: Time_B(MaxBlock)
+    character(len=*), optional,intent(in) :: NameOperatorIn 
+
+    character(len=*), parameter:: NameSub = &
+         'BATL_pass_cell::message_pass_scalar_cell'
+
+    ! help array for converting between Scalar_GB and State_VGB
+    ! used by message_pass_cell
+    real, allocatable, save:: Scalar_VGB(:,:,:,:,:)
+
+    !--------------------------------------------------------------------------
+
+    if(.not.allocated(Scalar_VGB)) &
+         allocate(Scalar_VGB(1,MinI:MaxI,MinJ:MaxJ,MinK:MaxK,MaxBlock))
+
+    Scalar_VGB(1,:,:,:,:) = Scalar_GB
+
+    call message_pass_cell(1,Scalar_VGB, nWidthIn=nWidthIn, &
+         nProlongOrderIn=nProlongOrderIn, nCoarseLayerIn=nCoarseLayerIn, &
+         DoSendCornerIn=DoSendCornerIn, DoRestrictFaceIn=DoRestrictFaceIn, &
+         TimeOld_B=TimeOld_B, Time_B=Time_B, DoTestIn=DoTestIn, &
+         NameOperatorIn=NameOperatorIn)
+
+    Scalar_GB = Scalar_VGB(1,:,:,:,:)
+  end subroutine message_pass_scalar_cell
+
+  !==========================================================================
+
   subroutine message_pass_cell(nVar, State_VGB, &
        nWidthIn, nProlongOrderIn, nCoarseLayerIn, DoSendCornerIn, &
-       DoRestrictFaceIn, TimeOld_B, Time_B, DoTestIn)
+       DoRestrictFaceIn, TimeOld_B, Time_B, DoTestIn, NameOperatorIn)
 
     use BATL_size, ONLY: MaxBlock, &
          nBlock, nIjk_D, nG, MinI, MaxI, MinJ, MaxJ, MinK, MaxK, &
@@ -46,6 +94,9 @@ contains
          iTree_IA, Proc_, Block_, Coord1_, Coord2_, Coord3_
 
     use ModMpi
+
+
+    use ModUtilities, ONLY: lower_case
 
     ! Arguments
     integer, intent(in) :: nVar
@@ -61,6 +112,7 @@ contains
     logical, optional, intent(in) :: DoTestIn
     real,    optional, intent(in) :: TimeOld_B(MaxBlock)
     real,    optional, intent(in) :: Time_B(MaxBlock)
+    character(len=*), optional,intent(in) :: NameOperatorIn
 
     ! Fill in the nVar variables in the ghost cells of State_VGB.
     !
@@ -78,6 +130,8 @@ contains
     !    ghost cells and physical cells of State_VGB, respectively. 
     !    If these arguments are present, the ghost cells are interpolated 
     !    in time. Default is a simple update with no temporal interpolation.
+    ! NameOperatorIn is used for the minimum or the maximum at the fine
+    !    Grid to the course grid cell. If not given the average will be used
 
     ! Local variables
 
@@ -127,6 +181,8 @@ contains
 
     logical:: DoTest
     character(len=*), parameter:: NameSub = 'BATL_pass_cell::message_pass_cell'
+    logical:: UseMin, UseMax
+    character(len=4) :: NameOperator
     !--------------------------------------------------------------------------
     DoTest = .false.; if(present(DoTestIn)) DoTest = DoTestIn
     if(DoTest)write(*,*)NameSub,' starting with nVar=',nVar
@@ -158,6 +214,24 @@ contains
     if(nProlongOrder == 2 .and. nCoarseLayer>1) call CON_stop(NameSub, &
          ' cannot use 2nd order prolongation nCoarseLayer > 1')
 
+    UseMin =.false.
+    UseMax =.false.
+
+    if(present(NameOperatorIn)) then
+       NameOperator = adjustl(NameOperatorIn)
+       call lower_case(NameOperator)
+       select case(NameOperator)
+       case("min")
+          UseMin=.true.
+       case("max")
+          UseMax=.true.
+       end select
+    end if
+
+    if(present(Time_B) .and. present(NameOperatorIn)) then
+       call CON_stop(NameSub, &
+            ' Time_B can not be used with '//trim(NameOperator))
+    end if
 
     ! Initialize logical for time interpolation/extrapolation
     UseTime = .false.
@@ -274,7 +348,7 @@ contains
        if(nProc == 1) CYCLE
 
        call timing_start('recv_pass')
-       
+
        !write(*,*)'!!! iProc, total recv, send buffer=', &
        !     iProc,sum(nBufferR_P), sum(nBufferS_P)
 
@@ -287,7 +361,7 @@ contains
 
           !write(*,*)'!!! MPI_irecv:iProcRecv,iProcSend,nBufferR=',&
           !     iProc,iProcSend,nBufferR_P(iProcSend)
-          
+
           call MPI_irecv(BufferR_I(iBufferR), nBufferR_P(iProcSend), &
                MPI_REAL, iProcSend, 1, iComm, iRequestR_I(iRequestR), &
                iError)
@@ -327,7 +401,7 @@ contains
           iBufferS  = iBufferS  + nBufferS_P(iProcRecv)
        end do
        call timing_stop('send_pass')
-      
+
        call timing_start('wait_pass')
        ! wait for all requests to be completed
        if(iRequestR > 0) &
@@ -397,7 +471,7 @@ contains
                do k = kRMin, kRmax; do j = jRMin, jRMax; do i = iRMin, iRmax
                   State_VGB(:,i,j,k,iBlockRecv) = &
                        BufferR_I(iBufferR+1:iBufferR+nVar)
-                  
+
                   iBufferR = iBufferR + nVar
                end do; end do; end do
             end if
@@ -626,13 +700,27 @@ contains
                   do iR = iRMin, iRMax
                      iS1 = iSMin + iRatioRestr*(iR-iRMin)
                      iS2 = iS1 + iRatioRestr - 1
-                     do iVar = 1, nVar
-                        State_VGB(iVar,iR,jR,kR,iBlockRecv) = &
-                             WeightOld * State_VGB(iVar,iR,jR,kR,iBlockRecv)+ &
-                             WeightNew * InvIjkRatioRestr * &
-                             sum(State_VGB(iVar,iS1:iS2,jS1:jS2,kS1:kS2,&
-                             iBlockSend))
-                     end do
+                     if(UseMin) then
+                        do iVar = 1, nVar
+                           State_VGB(iVar,iR,jR,kR,iBlockRecv) = &
+                                minval(State_VGB(iVar,iS1:iS2,jS1:jS2,kS1:kS2,&
+                                iBlockSend))
+                        end do
+                     else if(UseMax) then
+                        do iVar = 1, nVar
+                           State_VGB(iVar,iR,jR,kR,iBlockRecv) = &
+                                maxval(State_VGB(iVar,iS1:iS2,jS1:jS2,kS1:kS2,&
+                                iBlockSend))
+                        end do
+                     else
+                        do iVar = 1, nVar
+                           State_VGB(iVar,iR,jR,kR,iBlockRecv) = &
+                                WeightOld*State_VGB(iVar,iR,jR,kR,iBlockRecv)+&
+                                WeightNew*InvIjkRatioRestr * &
+                                sum(State_VGB(iVar,iS1:iS2,jS1:jS2,kS1:kS2,&
+                                iBlockSend))
+                        end do
+                     end if
                   end do
                end do
             end do
@@ -648,12 +736,26 @@ contains
                   do iR = iRMin, iRMax
                      iS1 = iSMin + iRatioRestr*(iR-iRMin)
                      iS2 = iS1 + iRatioRestr - 1
-                     do iVar = 1, nVar
-                        State_VGB(iVar,iR,jR,kR,iBlockRecv) = &
-                             InvIjkRatioRestr * &
-                             sum(State_VGB(iVar,iS1:iS2,jS1:jS2,kS1:kS2, &
-                             iBlockSend))
-                     end do
+                     if(UseMin)then
+                        do iVar = 1, nVar
+                           State_VGB(iVar,iR,jR,kR,iBlockRecv) = &
+                                minval(State_VGB(iVar,iS1:iS2,jS1:jS2,kS1:kS2,&
+                                iBlockSend))
+                        end do
+                     else if(UseMax) then
+                        do iVar = 1, nVar
+                           State_VGB(iVar,iR,jR,kR,iBlockRecv) = &
+                                maxval(State_VGB(iVar,iS1:iS2,jS1:jS2,kS1:kS2,&
+                                iBlockSend))
+                        end do
+                     else
+                        do iVar = 1, nVar
+                           State_VGB(iVar,iR,jR,kR,iBlockRecv) = &
+                                InvIjkRatioRestr * &
+                                sum(State_VGB(iVar,iS1:iS2,jS1:jS2,kS1:kS2, &
+                                iBlockSend))
+                        end do
+                     end if
                   end do
                end do
             end do
@@ -686,12 +788,26 @@ contains
                do iR=iRMin,iRMax
                   iS1 = iSMin + iRatioRestr*(iR-iRMin)
                   iS2 = iS1 + iRatioRestr - 1
-                  do iVar = 1, nVar
-                     BufferS_I(iBufferS+iVar) = &
-                          InvIjkRatioRestr * &
-                          sum(State_VGB(iVar,iS1:iS2,jS1:jS2,kS1:kS2,&
-                          iBlockSend))
-                  end do
+                  if(UseMin) then
+                     do iVar = 1, nVar
+                        BufferS_I(iBufferS+iVar) = &
+                             minval(State_VGB(iVar,iS1:iS2,jS1:jS2,kS1:kS2,&
+                             iBlockSend))
+                     end do
+                  else if(UseMax) then
+                     do iVar = 1, nVar
+                        BufferS_I(iBufferS+iVar) = &
+                             maxval(State_VGB(iVar,iS1:iS2,jS1:jS2,kS1:kS2,&
+                             iBlockSend))
+                     end do
+                  else
+                     do iVar = 1, nVar
+                        BufferS_I(iBufferS+iVar) = &
+                             InvIjkRatioRestr * &
+                             sum(State_VGB(iVar,iS1:iS2,jS1:jS2,kS1:kS2,&
+                             iBlockSend))
+                     end do
+                  end if
                   iBufferS = iBufferS + nVar
                end do
             end do
@@ -844,7 +960,7 @@ contains
                end if
 
                if(iProc == iProcRecv)then
-                  
+
                   if(present(Time_B)) UseTime = &
                        abs(Time_B(iBlockSend) - Time_B(iBlockRecv)) > 1e-30
                   if(UseTime)then
@@ -1037,27 +1153,35 @@ contains
 
   subroutine test_pass_cell
 
-    use BATL_mpi,  ONLY: iProc
+    use BATL_mpi,  ONLY: iProc, iComm
     use BATL_size, ONLY: MaxDim, nDim, iRatio, jRatio, kRatio, &
-         MinI, MaxI, MinJ, MaxJ, MinK, MaxK, nG, nI, nJ, nK, nBlock
+         MinI, MaxI, MinJ, MaxJ, MinK, MaxK, nG, nI, nJ, nK, nBlock,&
+         nIJK_D, iRatio_D
     use BATL_tree, ONLY: init_tree, set_tree_root, find_tree_node, &
          refine_tree_node, distribute_tree, show_tree, clean_tree, &
-         Unused_B, DiLevelNei_IIIB
+         Unused_B, DiLevelNei_IIIB, iNode_B
     use BATL_grid, ONLY: init_grid, create_grid, clean_grid, &
-         Xyz_DGB, CellSize_DB
+         Xyz_DGB, CellSize_DB, CoordMin_DB
     use BATL_geometry, ONLY: init_geometry
+
+    use ModMpi, ONLY: MPI_allreduce, MPI_REAL, MPI_MIN, MPI_MAX
 
     integer, parameter:: MaxBlockTest            = 50
     integer, parameter:: nRootTest_D(MaxDim)     = (/3,3,3/)
     logical, parameter:: IsPeriodicTest_D(MaxDim)= .true.
-    real, parameter:: DomainMin_D(MaxDim) = (/ 1.0, 10.0, 100.0 /)
-    real, parameter:: DomainMax_D(MaxDim) = (/ 4.0, 40.0, 400.0 /)
-    real, parameter:: DomainSize_D(MaxDim) = DomainMax_D - DomainMin_D
+    real :: DomainMin_D(MaxDim) = (/ 1.0, 10.0, 100.0 /)
+    real :: DomainMax_D(MaxDim) = (/ 4.0, 40.0, 400.0 /)
+    real :: DomainSize_D(MaxDim)
 
     real, parameter:: Tolerance = 1e-6
 
     integer, parameter:: nVar = nDim
     real, allocatable:: State_VGB(:,:,:,:,:)
+    real, allocatable:: Scalar_GB(:,:,:,:)
+    real, allocatable:: FineGridLocal_III(:,:,:)
+    real, allocatable:: FineGridGlobal_III(:,:,:)
+    real, allocatable:: XyzCorn_DGB(:,:,:,:,:)
+    real :: CourseGridCell_III(iRatio,jRatio,kRatio)
 
     integer:: nWidth
     integer:: nProlongOrder
@@ -1069,13 +1193,23 @@ contains
     integer:: iNode, iBlock, i, j, k, iMin, iMax, jMin, jMax, kMin, kMax, iDim
     integer:: iDir, jDir, kDir, Di, Dj, Dk
 
+    integer ::iOp
+    integer, parameter :: nOp=2
+    character(len=4) :: NameOperator_I(nOp) = (/ "min", "max" /)
+    character(len=4) :: NameOperator = "Min"
+    real :: FineGridStep_D(MaxDim)
+    integer :: iFG, jFG, kFG
+    integer :: nFineCell
+    integer :: iMpiOperator
+    integer :: iError
+
     logical:: DoTestMe
     character(len=*), parameter :: NameSub = 'test_pass_cell'
     !-----------------------------------------------------------------------
     DoTestMe = iProc == 0
 
     if(DoTestMe) write(*,*) 'Starting ',NameSub
-
+    DomainSize_D = DomainMax_D - DomainMin_D
     call init_tree(MaxBlockTest)
     call init_grid( DomainMin_D(1:nDim), DomainMax_D(1:nDim) )
     call init_geometry( IsPeriodicIn_D = IsPeriodicTest_D(1:nDim) )
@@ -1263,6 +1397,161 @@ contains
     end do; end do ! test parameters
     deallocate(State_VGB)
 
+    call clean_grid
+    call clean_tree
+    !------------------------ Test Scalar -----------------------------
+
+    ! To test the message pass for the cell with min max operators we 
+    ! generate a fine uniform grid 
+    ! for the whole domain and transfer the cell values from the
+    ! block cells to the cells on the fine grid. Then we gather all the
+    ! data on the fine grid with the proper operator.
+    ! We can then compare the values on the coresponding node after
+    ! message_pass_scalar_cell is called with the fine grid values.
+
+    ! rescale the domain to make indexing easyer
+    DomainSize_D = iRatio_D*nRootTest_D*nIJK_D
+    DomainMin_D = (/ 0.0, 0.0, 0.0 /)
+    DomainMax_D = DomainSize_D
+
+    call init_tree(MaxBlockTest)
+    call init_grid( DomainMin_D(1:nDim), DomainMax_D(1:nDim) )
+    call init_geometry( IsPeriodicIn_D = IsPeriodicTest_D(1:nDim) )
+    call set_tree_root( nRootTest_D(1:nDim))
+
+    call find_tree_node( (/0.5,0.5,0.5/), iNode)
+    call refine_tree_node(iNode)
+    call distribute_tree(.true.)
+    call create_grid
+
+    ! Position of Cell coners, for solving problems with round of 
+    ! when geting fine grid positions
+    allocate(XyzCorn_DGB(MaxDim,MinI:MaxI,MinJ:MaxJ,MinK:MaxK,MaxBlockTest))
+    do iBlock = 1, nBlock
+       if(Unused_B(iBlock)) CYCLE
+       do k = MinK, MaxK; do j = MinJ, MaxJ; do i = MinI, MaxI 
+          XyzCorn_DGB(:,i,j,k,iBlock) = &
+               Xyz_DGB(:,i,j,k,iBlock) - &
+               0.5*CellSize_DB(:,iBlock)*&
+               (/ min(1,nI-1),min(1,nJ-1),min(1,nK-1) /)
+       end do; end do; end do
+    end do
+
+    allocate(Scalar_GB(MinI:MaxI,MinJ:MaxJ,MinK:MaxK,MaxBlockTest))
+    Scalar_GB = -7777
+
+    allocate(FineGridLocal_III( &
+         nI*iRatio*nRootTest_D(1),&
+         nJ*jRatio*nRootTest_D(2),&
+         nK*kRatio*nRootTest_D(3)))
+    allocate(FineGridGlobal_III( &
+         (nI)*iRatio*nRootTest_D(1),&
+         (nJ)*jRatio*nRootTest_D(2),&
+         (nK)*kRatio*nRootTest_D(3)))
+
+
+    nFineCell = ((nI)*iRatio*nRootTest_D(1))*&
+         ((nJ)*jRatio*nRootTest_D(2))*&
+         ((nK)*kRatio*nRootTest_D(3))
+
+    FineGridStep_D = DomainSize_D &
+         / (DomainMax_D - DomainMin_D)
+
+    do iOp = 1, nOp
+
+       NameOperator = NameOperator_I(iOp)
+       select case(NameOperator)
+       case("min")
+          FineGridLocal_III(:,:,:)  =  1.0e8
+          FineGridGlobal_III(:,:,:) =  1.0e8
+          iMpiOperator = MPI_MIN
+       case("max")
+          FineGridLocal_III(:,:,:)  = -1.0e8
+          FineGridGlobal_III(:,:,:) = -1.0e8 
+          iMpiOperator=MPI_MAX
+       case default
+          call CON_stop(NameSub,' incorrect operator name')
+       end select
+
+       if(DoTestMe) write(*,*) NameSub,' testing for Operator: ',NameOperator
+
+       do iBlock = 1, nBlock
+          if(Unused_B(iBlock)) CYCLE
+          do k = 1, nK; do j = 1, nJ; do i = 1, nI
+             Scalar_GB(i,j,k,iBlock)= iNode_B(iBlock) + &
+                  sum(CoordMin_DB(:,iBlock) + &
+                  ( (/i, j, k/) ) * CellSize_DB(:,iBlock))
+          end do; end do; end do
+       end do
+
+       do iBlock = 1, nBlock
+          if(Unused_B(iBlock)) CYCLE
+          do k = 1, nK; do j = 1, nJ; do i = 1, nI
+
+             iFG = nint(XyzCorn_DGB(1,i,j,k,iBlock)*FineGridStep_D(1)) + 1 
+             jFG = nint(XyzCorn_DGB(2,i,j,k,iBlock)*FineGridStep_D(2)) + 1
+             kFG = nint(XyzCorn_DGB(3,i,j,k,iBlock)*FineGridStep_D(3)) + 1
+    
+             FineGridLocal_III(iFG,jFG,kFG) = Scalar_GB(i,j,k,iBlock)
+          end do; end do; end do
+       end do
+
+       call message_pass_scalar_cell(Scalar_GB, nWidthIn=2, &
+            nProlongOrderIn=1, nCoarseLayerIn=2, &
+            DoSendCornerIn=.true., DoRestrictFaceIn=.false., &
+            DoTestIn=DoTestMe, NameOperatorIn=NameOperator_I(iOp))
+
+       call MPI_ALLREDUCE(FineGridLocal_III(1,1,1), &
+            FineGridGlobal_III(1,1,1),              &
+            nFineCell, MPI_REAL, iMpiOperator, iComm, iError)
+
+       ! making sure that we have the center cell along the x=0 side
+       ! so the boundary are not tested.
+       call find_tree_node( (/0.0,0.5,0.5/), iNode)
+
+       do iBlock = 1, nBlock
+          if(Unused_B(iBlock)) CYCLE
+          if(iNode_B(iBlock) == iNode) then
+             do k = MinK, MaxK; do j = MinJ, MaxJ; do i = 1, MaxI
+
+                iFG = nint(XyzCorn_DGB(1,i,j,k,iBlock)*FineGridStep_D(1)) + 1 
+                jFG = nint(XyzCorn_DGB(2,i,j,k,iBlock)*FineGridStep_D(2)) + 1
+                kFG = nint(XyzCorn_DGB(3,i,j,k,iBlock)*FineGridStep_D(3)) + 1
+
+                !copy cells that are inside the course grid cell
+                CourseGridCell_III = FineGridGlobal_III(&
+                     iFG:iFG+min(1,iRatio-1),&
+                     jFG:jFG+min(1,jRAtio-1),&
+                     kFG:kFG+min(1,kRatio-1))
+
+                select case(NameOperator)
+                case("min") 
+                   if(Scalar_GB(i,j,k,iBlock) /= &
+                        minval(CourseGridCell_III))then
+                      write (*,*) "Error for operator, iNode,  iBlock= ",&
+                           NameOperator, iNode_B(iBlock), iBlock, ", value=",&
+                           minval(CourseGridCell_III),&
+                           " should be ", Scalar_GB(i,j,k,iBlock), "index : " &
+                           ,i,j,k, " ::", iFG, jFG,kFG
+
+                   end if
+                case("max") 
+                   if(Scalar_GB(i,j,k,iBlock) /= &
+                        maxval(CourseGridCell_III))then
+                      write (*,*) "Error for operator, iNode,  iBlock= ",&
+                           NameOperator, iNode_B(iBlock), iBlock, ", value=",&
+                           maxval(CourseGridCell_III),&
+                           " should be ", Scalar_GB(i,j,k,iBlock), "index : " &
+                           ,i,j,k, " ::", iFG, jFG,kFG
+                   end if
+                end select
+
+             end do; end do; end do
+          end if
+       end do
+
+    end do
+    deallocate(Scalar_GB, FineGridLocal_III, FineGridGlobal_III,XyzCorn_DGB)
     call clean_grid
     call clean_tree
 
