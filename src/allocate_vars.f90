ebeea9b6f26539de83e569164451529d51c5c6c1
gtoth <>
2011-05-08 01:23:59 +0000

    Do not allocate various large arrays when BATL is used.
    Former-commit-id: 4bef8f60b7c7cad7aad5a76bbc36365143f8efb9

--- src/allocate_vars.f90
+++ src/allocate_vars.f90
@@ -1,7 +1,7 @@
 !^CFG COPYRIGHT UM
 subroutine allocate_vars
   use ModProcMH
-  use ModMain, ONLY : unusedBLK
+  use ModMain, ONLY : unusedBLK, UseBatl
   use ModAMR
   use ModParallel
   use ModMpi
@@ -10,78 +10,80 @@ subroutine allocate_vars
   integer :: iPE, iBLK, ierror
   integer :: MaxBlockALL
 
-  !\  
-  ! Allocate and initialize list of available solution blocks.
-  !/
-  allocate( availableBLKs(0:nBLK,0:nProc-1), stat=ierror )
-  if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
-       " allocation error: availableBLKs"
-  availableBLKs(0,:)=1
-  do iBLK=1,nBLK; do iPE=0,nProc-1
-     availableBLKs(iBLK,iPE)=iBLK
-  end do; end do
+  if(.not. UseBatl)then
+     !\  
+     ! Allocate and initialize list of available solution blocks.
+     !/
+     allocate( availableBLKs(0:nBLK,0:nProc-1), stat=ierror )
+     if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
+          " allocation error: availableBLKs"
+     availableBLKs(0,:)=1
+     do iBLK=1,nBLK; do iPE=0,nProc-1
+        availableBLKs(iBLK,iPE)=iBLK
+     end do; end do
 
-  !\
-  ! Allocate and initialize global refinement/coarsening criteria and
-  ! flagging variables.
-  !/
-  allocate( refine_criteria_list(4,nBLK,nProc), stat=ierror )
-  if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
-       " allocation error: refine_criteria_list"
-  refine_criteria_list = 0.00
+     !\
+     ! Allocate and initialize global refinement/coarsening criteria and
+     ! flagging variables.
+     !/
+     allocate( refine_criteria_list(4,nBLK,nProc), stat=ierror )
+     if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
+          " allocation error: refine_criteria_list"
+     refine_criteria_list = 0.00
 
-  allocate( refine_list(nBLK,nProc), stat=ierror )
-  if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
-       " allocation error: refine_list"
-  refine_list = .false.
+     allocate( refine_list(nBLK,nProc), stat=ierror )
+     if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
+          " allocation error: refine_list"
+     refine_list = .false.
 
-  allocate( coarsen_list(nBLK,nProc), stat=ierror )
-  if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
-       " allocation error: coarsen_list"
-  coarsen_list = .false.
+     allocate( coarsen_list(nBLK,nProc), stat=ierror )
+     if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
+          " allocation error: coarsen_list"
+     coarsen_list = .false.
 
-  allocate( ListToCoarsen(2,nBLK*nProc), stat=ierror )
-  if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
-       " allocation error: ListToCoarsen"
-  ListToCoarsen = -1
+     allocate( ListToCoarsen(2,nBLK*nProc), stat=ierror )
+     if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
+          " allocation error: ListToCoarsen"
+     ListToCoarsen = -1
 
-  allocate( ListToRefine(2,nBLK*nProc), stat=ierror )
-  if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
-       " allocation error: ListToRefine"
-  ListToRefine = -1
+     allocate( ListToRefine(2,nBLK*nProc), stat=ierror )
+     if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
+          " allocation error: ListToRefine"
+     ListToRefine = -1
 
-  allocate( SortB(3,nBLK*nProc), stat=ierror )
-  if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
-       " allocation error: SortB"
-  SortB = -1
+     allocate( SortB(3,nBLK*nProc), stat=ierror )
+     if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
+          " allocation error: SortB"
+     SortB = -1
 
-  allocate( SortP(3,nBLK*nProc), stat=ierror )
-  if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
-       " allocation error: SortP"
-  SortP = -1
+     allocate( SortP(3,nBLK*nProc), stat=ierror )
+     if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
+          " allocation error: SortP"
+     SortP = -1
 
-  allocate( SortC(3,nBLK*nProc), stat=ierror )
-  if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
-       " allocation error: SortC"
-  SortC = 0.00
+     allocate( SortC(3,nBLK*nProc), stat=ierror )
+     if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
+          " allocation error: SortC"
+     SortC = 0.00
 
-  !\
-  ! Allocate and initialize other global variables.
-  !/
-  allocate( PE_time_list(nProc), stat=ierror )
-  if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
-       " allocation error: PE_time_list"
-  PE_time_list = 0.00
+     !\
+     ! Allocate and initialize other global variables.
+     !/
+     allocate( PE_time_list(nProc), stat=ierror )
+     if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
+          " allocation error: PE_time_list"
+     PE_time_list = 0.00
 
-  allocate( PE_float_list(nProc), stat=ierror )
-  if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
-       " allocation error: PE_float_list"
-  PE_float_list = 0.00
+     allocate( PE_float_list(nProc), stat=ierror )
+     if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
+          " allocation error: PE_float_list"
+     PE_float_list = 0.00
 
-  allocate( PE_mflop_list(nProc), stat=ierror )
-  if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
-       " allocation error: PE_mflop_list"
-  PE_mflop_list = 0.00
+     allocate( PE_mflop_list(nProc), stat=ierror )
+     if (ierror > 0) write(*,*) "allocate_vars: PE = ",iProc, &
+          " allocation error: PE_mflop_list"
+     PE_mflop_list = 0.00
+  end if
 
   !\
   ! allocate index arrays
