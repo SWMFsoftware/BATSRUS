d99c83d46f100c80d730009ecc765166bb47ad80
gtoth <>
2011-06-22 19:33:33 +0000

    Commented out part that keeps changing the util/HDF5/src/Flash.h file.
    Former-commit-id: 3daa7dfda357ab512f4ba9061d930966828f6948

--- Config.pl
+++ Config.pl
@@ -204,20 +204,22 @@ sub set_grid_size{
     }
     
     
-    if(-f $NameHdf5File){
-	# Determine the number of dimensions specified and write
-	# that value to the necessary HDF5 header files
-    
-	@ARGV = ($NameHdf5File);
-	my $nDim = 0 + ($nI>1) + ($nJ>1) + ($nK>1);
-	while(<>){
-	    s/\b(NDIM\s*[^0-9]*)(\d+)/$1$nDim/i;
-	    print;
-	}
-    
-	# Recompile the HDF5 library if it is currently enabled.
-	&shell_command("make HDF5") if $Hdf5 eq "yes";
-    }
+    # The HDF5 support is not yet functional, and changing
+    # a CVS controlled file is not a good idea. So it is commented out.
+    #if(-f $NameHdf5File){
+    #	# Determine the number of dimensions specified and write
+    #	# that value to the necessary HDF5 header files
+    #
+    #	@ARGV = ($NameHdf5File);
+    #	my $nDim = 0 + ($nI>1) + ($nJ>1) + ($nK>1);
+    #	while(<>){
+    #	    s/\b(NDIM\s*[^0-9]*)(\d+)/$1$nDim/i;
+    #	    print;
+    #	}
+    #
+    #	# Recompile the HDF5 library if it is currently enabled.
+    #	&shell_command("make HDF5") if $Hdf5 eq "yes";
+    #}
 }
 
 ##############################################################################
