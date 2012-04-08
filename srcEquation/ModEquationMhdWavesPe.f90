aea1fef709e8c5bcae49dc4c33359b4143919783
bartvand <>
2012-04-08 18:42:57 +0000

    waves are not guaranteed to be initialized resulting in a division by zero.
    Former-commit-id: c609f034fd1df745a0a33a11cee627e9e5859b1b

--- srcEquation/ModEquationMhdWavesPe.f90
+++ srcEquation/ModEquationMhdWavesPe.f90
@@ -65,7 +65,7 @@ module ModVarIndexes
        0.0, & ! Bx_
        0.0, & ! By_
        0.0, & ! Bz_
-       (0.0, iWave=WaveFirst_,WaveLast_), &
+       (1.0, iWave=WaveFirst_,WaveLast_), &
        1.0, & ! Pe_
        1.0, & ! p_
        1.0 /) ! Energy_
