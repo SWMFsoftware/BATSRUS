a16de2e1d72543d280f5be601c2829246137a883
pauldast <>
2012-04-12 20:57:42 +0000

    so non hdf runs dont crash
    Former-commit-id: 546883ac953becc4e565132cab6e250d29aa3757

--- src/ModHdf5_empty.f90
+++ src/ModHdf5_empty.f90
@@ -8,17 +8,17 @@ module ModHdf5
 
 contains
   !=========================================================================
-  subroutine write_plot_hdf5(NameFile, NameVar_V, NameUnit_V, &
-       nPlotVar, xMin, xMax, yMin, yMax, zMin, zMax, nCell)
+  subroutine write_plot_hdf5(filename, plotVarNames, plotVarUnits,&
+    nPlotVar, isCutFile, isnonCartesian,plot_dimensional)
 
     use ModProcMH, ONLY: iProc
 
-    character(len=80),       intent(in):: NameFile
     integer,                 intent(in):: nPlotVar
-    character(len=lNameVar), intent(in):: NameVar_V(nPlotVar)
-    character(len=lNameVar), intent(in):: NameUnit_V(nPlotVar)
-    real,                    intent(in):: xMin, xMax, yMin, yMax, zMin, zMax
-    integer,                 intent(in):: nCell
+    character(len=80),       intent(in):: filename
+    character(len=lNameVar), intent(in):: plotVarNames(nPlotVar)
+    character(len=lNameVar),  intent(in):: plotVarUnits(nPlotVar)
+    logical, intent(in) :: isCutFile, isnonCartesian,plot_dimensional
+
     !----------------------------------------------------------------------
     if(iProc==0) write (*,*) "ERROR: HDF5 plotting is not enabled!"
 
@@ -33,5 +33,22 @@ contains
     !----------------------------------------------------------------------
 
   end subroutine write_var_hdf5
+
+  subroutine write_cut_var_hdf5(iFile, iBlock, H5Index,nPlotVar,PlotVar, &
+     xMin, xMax, yMin, yMax, zMin, zMax, DxBlock, DyBlock, DzBlock,&
+     isNonCartesian,nCell,H5Advance)
+
+      use ModMain, ONLY : nI, nJ, nK
+      integer, intent(in)   :: iFile, iBlock, H5Index
+      integer, intent(in)   :: nPlotVar
+      real,    intent(in)   :: PlotVar(-1:nI+2,-1:nJ+2,-1:nK+2,nPlotVar)
+      real,    intent(in)   :: xMin,xMax,yMin,yMax,zMin,zMax
+      logical, intent(in) :: isNonCartesian
+      real,    intent(inout):: DxBlock,DyBlock,DzBlock
+      integer, intent(out)  :: nCell 
+      logical, intent(out) :: H5advance
+
+  end subroutine write_cut_var_hdf5
+
 
 end module ModHdf5
