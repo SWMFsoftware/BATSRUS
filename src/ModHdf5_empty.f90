1700f724368bacaa0975ac02a925c343d296d27f
bartvand <>
2012-04-13 01:46:34 +0000

    Fixed empty hdf5 interface
    Former-commit-id: e45ddd12fd20349c272b14875ca0b276d1c70cdb

--- src/ModHdf5_empty.f90
+++ src/ModHdf5_empty.f90
@@ -24,12 +24,14 @@ contains
 
   end subroutine write_plot_hdf5
   !=========================================================================
-  subroutine write_var_hdf5(PlotVar_GI, nPlotVar, iBlockH5, iBlock)
+  subroutine write_var_hdf5(PlotVar_GI, nPlotVar, iBlockH5, iBlock, &
+       IsNonCartesian)
 
     use ModMain, ONLY : nI, nJ, nK
 
     integer, intent(in) :: nPlotVar, iBlockH5, iBlock
     real, intent(in) :: PlotVar_GI(-1:nI+2,-1:nJ+2,-1:nK+2,nPlotVar)
+    logical, intent(in) :: IsNonCartesian
     !----------------------------------------------------------------------
 
   end subroutine write_var_hdf5
