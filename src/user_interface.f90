12de79c9428d18df6b6dbaa6c8b121f2b73b0f81
gtoth <>
2012-06-15 16:17:06 +0000

    Added external subroutine interfaces for most user routines in ModUser so that we can avoid circular dependencies as the code is developed. Modified Files: 	Makefile Added Files: 	user_interface.f90
    Former-commit-id: 53720934afc8e0092bb093140eee744cb6cee055

---
+++ src/user_interface.f90
@@ -0,0 +1,246 @@
+!^CFG COPYRIGHT UM
+! We provide external subroutine interfaces to ModUser routines 
+! to avoid circular dependencies
+
+!=====================================================================
+subroutine user_set_boundary_cells(iBlock)
+
+  use ModUser, ONLY: user_sub => user_set_boundary_cells
+  implicit none
+
+  integer,intent(in)::iBlock
+  !-------------------------------------------------------------------
+  call user_sub(iBlock)
+
+end subroutine user_set_boundary_cells
+
+!=====================================================================
+subroutine user_face_bcs(VarsGhostFace_V)
+
+  use ModAdvance, ONLY: nVar
+  use ModUser, ONLY: user_sub => user_face_bcs
+  implicit none
+
+  real, intent(out):: VarsGhostFace_V(nVar)
+  !-------------------------------------------------------------------
+  call user_sub(VarsGhostFace_V)
+
+end subroutine user_face_bcs
+
+!=====================================================================
+subroutine user_set_outerbcs(iBlock, iSide, TypeBc, IsFound)
+
+  use ModUser, ONLY: user_sub => user_set_outerbcs
+  implicit none
+
+  integer,          intent(in)  :: iBlock, iSide
+  character(len=20),intent(in)  :: TypeBc
+  logical,          intent(out) :: IsFound
+  !-------------------------------------------------------------------
+  call user_sub(iBlock,iSide, TypeBc, IsFound)
+
+end subroutine user_set_outerbcs
+
+!=====================================================================
+subroutine user_initial_perturbation
+
+  use ModUser, ONLY: user_sub => user_initial_perturbation
+  implicit none
+  !-------------------------------------------------------------------
+  call user_sub
+
+end subroutine user_initial_perturbation
+
+!=====================================================================
+subroutine user_set_ics
+
+  use ModUser, ONLY: user_sub => user_set_ics
+  implicit none
+  !-------------------------------------------------------------------
+  call user_sub
+
+end subroutine user_set_ics
+
+!=====================================================================
+subroutine user_init_session
+
+  use ModUser, ONLY: user_sub => user_init_session
+  implicit none
+  !-------------------------------------------------------------------
+  call user_sub
+
+end subroutine user_init_session
+
+!=====================================================================
+subroutine user_action(NameAction)
+
+  use ModUser, ONLY: user_sub => user_action
+  implicit none
+
+  character(len=*), intent(in):: NameAction
+  !-------------------------------------------------------------------
+  call user_sub(NameAction)
+
+end subroutine user_action
+!=====================================================================
+subroutine user_specify_refinement(iBlock, iArea, DoRefine)
+
+  use ModUser, ONLY: user_sub => user_specify_refinement
+  implicit none
+
+  integer, intent(in) :: iBlock, iArea
+  logical,intent(out) :: DoRefine
+
+  !-------------------------------------------------------------------
+  call user_sub(iBlock, iArea, DoRefine)
+
+end subroutine user_specify_refinement
+
+!=====================================================================
+subroutine user_amr_criteria(iBlock, UserCriteria, TypeCriteria, IsFound)
+
+  use ModUser, ONLY: user_sub => user_amr_criteria
+  implicit none
+
+  integer, intent(in)          :: iBlock
+  real, intent(out)            :: UserCriteria
+  character(len=*), intent(in) :: TypeCriteria
+  logical, intent(inout)       :: IsFound
+  !-------------------------------------------------------------------
+  call user_sub(iBlock, UserCriteria, TypeCriteria, IsFound)
+
+end subroutine user_amr_criteria
+
+!=====================================================================
+subroutine user_read_inputs
+
+  use ModUser, ONLY: user_sub => user_read_inputs
+  implicit none
+  !-------------------------------------------------------------------
+  call user_sub
+
+end subroutine user_read_inputs
+
+!=====================================================================
+subroutine user_get_log_var(VarValue, TypeVar, Radius)
+
+  use ModUser, ONLY: user_sub => user_get_log_var
+  implicit none
+
+  real, intent(out)           :: VarValue
+  character(len=*), intent(in):: TypeVar
+  real, intent(in)            :: Radius
+  !-------------------------------------------------------------------
+  call user_sub(VarValue, TypeVar, Radius)
+  
+end subroutine user_get_log_var
+
+!====================================================================
+
+subroutine user_set_plot_var(iBlock, NameVar, IsDimensional, &
+     PlotVar_G, PlotVarBody, UsePlotVarBody, &
+     NameTecVar, NameTecUnit, NameIdlUnit, IsFound)
+
+  use ModSize, ONLY: nI, nJ, nK
+  use ModUser, ONLY: user_sub => user_set_plot_var
+  implicit none
+
+  integer,          intent(in)   :: iBlock
+  character(len=*), intent(in)   :: NameVar
+  logical,          intent(in)   :: IsDimensional
+  real,             intent(out)  :: PlotVar_G(-1:nI+2, -1:nJ+2, -1:nK+2)
+  real,             intent(out)  :: PlotVarBody
+  logical,          intent(out)  :: UsePlotVarBody
+  character(len=*), intent(inout):: NameTecVar
+  character(len=*), intent(inout):: NameTecUnit
+  character(len=*), intent(inout):: NameIdlUnit
+  logical,          intent(out)  :: IsFound
+
+  !-------------------------------------------------------------------
+  call user_sub(iBlock, NameVar, IsDimensional, &
+       PlotVar_G, PlotVarBody, UsePlotVarBody, &
+       NameTecVar, NameTecUnit, NameIdlUnit, IsFound)
+
+end subroutine user_set_plot_var
+
+!====================================================================
+
+subroutine user_calc_sources
+
+  use ModUser, ONLY: user_sub => user_calc_sources
+  implicit none
+  !-------------------------------------------------------------------
+  call user_sub
+
+end subroutine user_calc_sources
+
+!=====================================================================
+
+subroutine user_init_point_implicit
+
+  use ModUser, ONLY: user_sub => user_init_point_implicit
+  implicit none
+  !-------------------------------------------------------------------
+  call user_sub
+
+end subroutine user_init_point_implicit
+
+!=====================================================================
+
+subroutine user_get_b0(x, y, z, B0_D)
+
+  use ModUser, ONLY: user_sub => user_get_b0
+  implicit none
+
+  real, intent(in) :: x, y, z
+  real, intent(out):: B0_D(3)
+  !-------------------------------------------------------------------
+  call user_sub(x, y, z, B0_D)
+
+end subroutine user_get_b0
+
+!=====================================================================
+subroutine user_update_states(iStage, iBlock)
+
+  use ModUser, ONLY: user_sub => user_update_states
+  implicit none
+ 
+  integer,intent(in):: iStage, iBlock
+  !-------------------------------------------------------------------
+  call user_sub(iStage, iBlock)
+
+end subroutine user_update_states
+
+!=====================================================================
+subroutine user_normalization
+
+  use ModUser, ONLY: user_sub => user_normalization
+  implicit none
+  !-------------------------------------------------------------------
+  call user_sub
+
+end subroutine user_normalization
+
+!=====================================================================
+subroutine user_io_units
+
+  use ModUser, ONLY: user_sub => user_io_units
+  implicit none
+  !-------------------------------------------------------------------
+  call user_sub
+  
+end subroutine user_io_units
+
+!=====================================================================
+subroutine user_set_resistivity(iBlock, Eta_G)
+
+  use ModSize, ONLY: MinI, MaxI, MinJ, MaxJ, MinK, MaxK
+  use ModUser, ONLY: user_sub => user_set_resistivity
+  implicit none
+
+  integer, intent(in) :: iBlock
+  real,    intent(out):: Eta_G(MinI:MaxI,MinJ:MaxJ,MinK:MaxK) 
+  !-------------------------------------------------------------------
+  call user_sub(iBlock, Eta_G)
+
+end subroutine user_set_resistivity
