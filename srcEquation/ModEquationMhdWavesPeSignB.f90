54b7d47b6367f44fc1cdf4b0cc50dacdd4c2c436
igorsok <>
2014-01-31 20:04:05 +0000

    Explicitly pdovide the wave variable names (I01 I02) in the list of primitive variables, to provide a correct output to satellite files.
    Former-commit-id: 61b25947815b3dea64670d051ea58b16f19d43e6

--- srcEquation/ModEquationMhdWavesPeSignB.f90
+++ srcEquation/ModEquationMhdWavesPeSignB.f90
@@ -100,7 +100,7 @@ module ModVarIndexes
 
   ! The space separated list of nVar primitive variables for plotting
   character(len=*), parameter :: NamePrimitiveVar = &
-       'rho ux uy uz bx by bz sign ehot I(2) pe p'
+       'rho ux uy uz bx by bz sign ehot I01 I02 pe p'
 
   ! The space separated list of nVar primitive variables for TECplot output
   character(len=*), parameter :: NamePrimitiveVarTec = &
