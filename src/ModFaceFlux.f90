eea0cf034962a805f235d4853058f299f9212d78
yuxichen <>
2014-02-28 03:48:16 +0000

    Moved subroutine correct_face_value to ModHighOrder.f90
    Former-commit-id: b2fe66234f5674c6265a7e33461b646358495d4d

--- src/ModFaceFlux.f90
+++ src/ModFaceFlux.f90
@@ -3419,12 +3419,11 @@ subroutine calc_simple_cell_flux(iBlock)
 
     use ModMultiFluid, ONLY: iRho_I, iRhoUx_I, iRhoUy_I, iRhoUz_I
     use ModAdvance,    ONLY: State_VGB
-
+    use ModHighOrder,  ONLY: correct_face_value
     integer, intent(in):: iDim
 
     integer:: iFluid, iRho, iRhoUx, iRhoUy, iRhoUz
     real :: Ucell_I(4), Unormal 
-    real, external :: correct_face_value
     !--------------------------------------------------------------------------
 
     do iFluid = iFluidMin, iFluidMax
@@ -4049,22 +4048,3 @@ subroutine calc_electric_field(iBlock)
        + Flux_VY(Bx_,1:nI  ,2:nJ+1,1:nK)) / CellFace_DB(2,iBlock))
 
 end subroutine calc_electric_field
-
-!==============================================================================
-
-real function correct_face_value(FaceValue, CellValue_I)
-
-  ! FaceValue is at cell face. CellValue_I are cell centered.
-  ! Return 6th order approximation
-
-  real, intent(in):: CellValue_I(4), FaceValue
-  real:: Der2, Der4
-  real, parameter:: c1over6 = 1./6, c1over180 = 1./180
-  !----------------------------------------------------------------------------
-  Der2 = c1over6*(CellValue_I(2) - 2*FaceValue + CellValue_I(3))
-  Der4 = c1over180*(16*FaceValue - &
-       9*(CellValue_I(2) + CellValue_I(3)) + &
-       CellValue_I(1) + CellValue_I(4))
-  correct_face_value = FaceValue - Der2 + Der4
-
-end function correct_face_value
