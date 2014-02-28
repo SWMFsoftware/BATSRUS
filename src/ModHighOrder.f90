dc04adaff268dffc71ad8d79517ef4df118804e3
yuxichen <>
2014-02-28 03:44:38 +0000

    Including subroutines for high-order schemes.
    Former-commit-id: a1cc2d238b9c5539d2df2219534bcbac4e4e0bd4

---
+++ src/ModHighOrder.f90
@@ -0,0 +1,28 @@
+!  Copyright (C) 2002 Regents of the University of Michigan, 
+!  portions used with permission 
+!  For more information, see http://csem.engin.umich.edu/tools/swmf
+module ModHighOrder
+
+  implicit none
+
+contains
+
+  !======================================================================
+  real function correct_face_value(FaceValue, CellValue_I)
+
+    ! FaceValue is at cell face. CellValue_I are cell centered.
+    ! Return 6th order approximation
+
+    real, intent(in):: CellValue_I(4), FaceValue
+    real:: Der2, Der4
+    real, parameter:: c1over6 = 1./6, c1over180 = 1./180
+    !----------------------------------------------------------------------------
+    Der2 = c1over6*(CellValue_I(2) - 2*FaceValue + CellValue_I(3))
+    Der4 = c1over180*(16*FaceValue - &
+         9*(CellValue_I(2) + CellValue_I(3)) + &
+         CellValue_I(1) + CellValue_I(4))
+    correct_face_value = FaceValue - Der2 + Der4
+
+  end function correct_face_value
+
+end module ModHighOrder
