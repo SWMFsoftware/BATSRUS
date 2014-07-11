2b2cc064b7e9e0314e4954de10b4b1d3330cc7ca
yuxichen <>
2014-07-11 16:57:03 +0000

    Added some indexes for ghost cells.
    Former-commit-id: f678b02151760b7401ce83444c4a86e81b0b0123

--- srcBATL/BATL_size_orig.f90
+++ srcBATL/BATL_size_orig.f90
@@ -15,6 +15,9 @@ module BATL_size
   ! Set 1 for ignored directions! 
   integer, parameter :: nI = 8, nJ = 8, nK = 8
 
+  ! Maximum number of ghost cells
+  integer, parameter :: nG = 2
+
   ! 0 or 1 if a given dimension is ignnored or used
   integer, parameter:: iDim_ = min(nI - 1, 1)
   integer, parameter:: jDim_ = min(nJ - 1, 1)
@@ -26,14 +29,37 @@ module BATL_size
   integer, parameter:: nKNode = nK + kDim_
 
   ! Index values for ghost cells that may or may not exist
-  integer, parameter:: j0_   =  1 - jDim_ ! j=0
-  integer, parameter:: j2_   =  1 + jDim_ ! j=2
-  integer, parameter:: nJp1_ = nJ + jDim_ ! j=nJ+1
-  integer, parameter:: nJm1_ = nJ - jDim_ ! j=nJ-1
-  integer, parameter:: k0_   =  1 - kDim_ ! k=0
-  integer, parameter:: k2_   =  1 + kDim_ ! k=2
-  integer, parameter:: nKp1_ = nK + kDim_ ! k=nK+1
-  integer, parameter:: nKm1_ = nK - kDim_ ! k=nK-1
+  integer, parameter:: im2_  =  1 - min(3*iDim_,nG) ! i=-2
+  integer, parameter:: im1_  =  1 - 2*iDim_         ! i=-1
+  integer, parameter:: i0_   =  1 - iDim_           ! i=0
+  integer, parameter:: i2_   =  1 + iDim_           ! i=2
+  integer, parameter:: i3_   =  1 + 2*iDim_         ! i=3
+  integer, parameter:: nIm2_ = nI - 2*iDim_         ! i=nI-2
+  integer, parameter:: nIm1_ = nI - iDim_           ! i=nI-1
+  integer, parameter:: nIp1_ = nI + iDim_           ! i=nI+1
+  integer, parameter:: nIp2_ = nI + 2*iDim_         ! i=nI+2
+  integer, parameter:: nIp3_ = nI + min(3*iDim_,nG) ! i=nI+3
+  integer, parameter:: jm2_  =  1 - min(3*jDim_,nG) ! j=-2
+  integer, parameter:: jm1_  =  1 - 2*jDim_         ! j=-1
+  integer, parameter:: j0_   =  1 - jDim_           ! j=0
+  integer, parameter:: j2_   =  1 + jDim_           ! j=2
+  integer, parameter:: j3_   =  1 + 2*jDim_         ! j=3
+  integer, parameter:: nJm2_ = nJ - 2*jDim_         ! j=nJ-2
+  integer, parameter:: nJm1_ = nJ - jDim_           ! j=nJ-1
+  integer, parameter:: nJp1_ = nJ + jDim_           ! j=nJ+1
+  integer, parameter:: nJp2_ = nJ + 2*jDim_         ! j=nJ+2
+  integer, parameter:: nJp3_ = nJ + min(3*jDim_,nG) ! j=nJ+3
+  integer, parameter:: km2_  =  1 - min(3*kDim_,nG) ! k=-2
+  integer, parameter:: km1_  =  1 - 2*kDim_         ! k=-1
+  integer, parameter:: k0_   =  1 - kDim_           ! k=0
+  integer, parameter:: k2_   =  1 + kDim_           ! k=2
+  integer, parameter:: k3_   =  1 + 2*kDim_         ! k=3
+  integer, parameter:: nKm2_ = nK - 2*kDim_         ! k=nK-2
+  integer, parameter:: nKm1_ = nK - kDim_           ! k=nK-1
+  integer, parameter:: nKp1_ = nK + kDim_           ! k=nK+1
+  integer, parameter:: nKp2_ = nK + 2*kDim_         ! k=nK+2
+  integer, parameter:: nKp3_ = nK + min(3*kDim_,nG) ! k=nK+3
+
 
   ! Number of not ignored dimensions
   integer, parameter:: nDim = iDim_ + jDim_ + kDim_
@@ -52,9 +78,6 @@ module BATL_size
        (/ 1 + (2-iRatio)*(3-jRatio), 6-iRatio-jRatio, 3 /)
 
   integer, parameter :: iDimAmr_D(nDimAmr) = iDimAmrTmp_D(1:nDimAmr)
-
-  ! Maximum number of ghost cells
-  integer, parameter :: nG = 2
 
   ! Number of ghost cells in each direction
   integer, parameter:: nGI = nG*iDim_
