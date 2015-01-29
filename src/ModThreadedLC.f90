05e221d392848aee44c4c538498c414f0fd7eccd
gtoth <>
2015-01-29 22:32:31 +0000

    Added missing copyright lines.
    Former-commit-id: b5eb53b3e80112cf8836493d89404da408c6d41a

--- src/ModThreadedLC.f90
+++ src/ModThreadedLC.f90
@@ -1,3 +1,6 @@
+!  Copyright (C) 2002 Regents of the University of Michigan, 
+!  portions used with permission 
+!  For more information, see http://csem.engin.umich.edu/tools/swmf
 module ModThreadedLC
   use ModFieldLineThread, ONLY: &
        BoundaryThreads, BoundaryThreads_B
