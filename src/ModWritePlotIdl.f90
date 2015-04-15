fae74bc2fed1cafa86aaabb6596cb33ad4f0e5fd
gtoth <>
2015-04-15 17:38:42 +0000

    Save the generalized coordinates for cuts out of non-Cartesian grids. Removed unnecessary unit conversions. Improved some formatting.
    Former-commit-id: a37760e7f8472a00a04b4435f97b7a55d3a7de2b

--- src/ModWritePlotIdl.f90
+++ src/ModWritePlotIdl.f90
@@ -1,6 +1,6 @@
-!  Copyright (C) 2002 Regents of the University of Michigan, portions used with permission 
+!  Copyright (C) 2002 Regents of the University of Michigan, 
+!  portions used with permission 
 !  For more information, see http://csem.engin.umich.edu/tools/swmf
-!This code is a copyright protected software (c) 2002- University of Michigan
 !=============================================================================
 subroutine write_plot_idl(iFile, iBlock, nPlotVar, PlotVar, &
      xMin, xMax, yMin, yMax, zMin, zMax, DxBlock, DyBlock, DzBlock, nCell)
@@ -8,15 +8,18 @@ subroutine write_plot_idl(iFile, iBlock, nPlotVar, PlotVar, &
   ! Save all cells within plotting range, for each processor
 
   use ModProcMH
-  use ModMain, ONLY: nI, nJ, nK, PROCtest, BLKtest, test_string, &
-       x_, y_, z_, Phi_
-  use ModGeometry, ONLY: Xyz_DGB, CellSize_DB,&
-       x1, x2, y1, y2, z1, z2, XyzStart_BLK, XyzMin_D, XyzMax_D
-  use ModPhysics, ONLY : No2Io_V, UnitX_
-  use ModIO
+  use ModMain,     ONLY: PROCtest, BLKtest, test_string
+  use ModGeometry, ONLY: x1, x2, y1, y2, z1, z2, XyzStart_BLK
+  use ModIO,       ONLY: save_binary, plot_type1, plot_dx, plot_range, &
+       nPlotVarMax
   use ModNumConst, ONLY: cPi, cTwoPi
-  use ModKind, ONLY: nByteReal
-  use BATL_lib, ONLY: IsRLonLat, IsCylindrical
+  use ModKind,     ONLY: nByteReal
+  use ModIoUnit,   ONLY: UnitTmp_
+  use BATL_size,   ONLY: nGI, nGJ, nGK
+  use BATL_lib,    ONLY: IsRLonLat, IsCylindrical, IsCartesianGrid, &
+       CoordMin_D, CoordMax_D, CoordMin_DB, CellSize_DB, &
+       nI, nJ, nK, MinI, MaxI, MinJ, MaxJ, MinK, MaxK, &
+       Xyz_DGB, x_, y_, z_, Phi_
 
   implicit none
 
@@ -33,12 +36,10 @@ subroutine write_plot_idl(iFile, iBlock, nPlotVar, PlotVar, &
   ! Indices and coordinates
   integer :: iVar, i, j, k, i2, j2, k2, iMin, iMax, jMin, jMax, kMin, kMax
   integer :: nRestrict, nRestrictX, nRestrictY, nRestrictZ
-  real :: x,y,z,Dx,Restrict
+  real :: Coord_D(3), x, y, z, ySqueezed, Dx, Restrict
   real :: xMin1,xMax1,yMin1,yMax1,zMin1,zMax1
-  real :: DxBlockOut
   real :: Plot_V(nPlotVarMax)
-  logical:: IsBinary
-  real :: ySqueezed
+  logical:: IsBinary, DoSaveGenCoord
 
   real:: cHalfMinusTiny
 
@@ -60,44 +61,43 @@ subroutine write_plot_idl(iFile, iBlock, nPlotVar, PlotVar, &
 
   IsBinary = save_binary .and. plot_type1 /= 'cut_pic'
 
+  ! Save generalized coordinates for cuts out of non-Cartesian grids
+  DoSaveGenCoord = plot_type1(1:3) == 'cut' .and. .not. IsCartesianGrid
+
   if(index(test_string,'SAVEPLOTALL')>0)then
+
      ! Save all cells of block including ghost cells
+     DxBlock = CellSize_DB(x_,iBlock)
+     DyBlock = CellSize_DB(y_,iBlock) 
+     DzBlock = CellSize_DB(z_,iBlock)
+
+     plot_range(1,iFile) = CoordMin_D(1) - nGI*DxBlock
+     plot_range(2,iFile) = CoordMax_D(1) + nGI*DxBlock
+     plot_range(3,iFile) = CoordMin_D(2) - nGJ*DyBlock
+     plot_range(4,iFile) = CoordMax_D(2) + nGJ*DyBlock
+     plot_range(5,iFile) = CoordMin_D(3) - nGK*DzBlock
+     plot_range(6,iFile) = CoordMax_D(3) + nGK*DzBlock
+     plot_Dx(1,iFile) = DxBlock
+     plot_Dx(2,iFile) = DyBlock
+     plot_Dx(3,iFile) = DzBlock
 
      nCell=0
-     DxBlock=CellSize_DB(x_,iBlock); DyBlock=CellSize_DB(y_,iBlock); DzBlock=CellSize_DB(z_,iBlock)
-     DxBlockOut = DxBlock
-     if (plot_dimensional(iFile)) DxBlockOut = DxBlock*No2Io_V(UnitX_)
-
-     plot_range(1,iFile)=XyzMin_D(1)-2*DxBlock
-     plot_range(2,iFile)=XyzMax_D(1)+2*DxBlock
-     plot_range(3,iFile)=XyzMin_D(2)-2*DyBlock
-     plot_range(4,iFile)=XyzMax_D(2)+2*DyBlock
-     plot_range(5,iFile)=XyzMin_D(3)-2*DzBlock
-     plot_range(6,iFile)=XyzMax_D(3)+2*DzBlock
-     plot_Dx(1,iFile)=DxBlock
-     plot_Dx(2,iFile)=DyBlock
-     plot_Dx(3,iFile)=DzBlock
-
-     do k=MinK,MaxK; do j=MinJ,MaxJ; do i=MinI,MaxI
+     do k = MinK, MaxK; do j = MinJ, MaxJ; do i = MinI, MaxI
         nCell=nCell+1
-        if (plot_dimensional(iFile)) then
-           x = Xyz_DGB(x_,i,j,k,iBlock)*No2Io_V(UnitX_)
-           y = Xyz_DGB(y_,i,j,k,iBlock)*No2Io_V(UnitX_)
-           z = Xyz_DGB(z_,i,j,k,iBlock)*No2Io_V(UnitX_)
+        if(DoSaveGenCoord)then
+           Coord_D = CoordMin_DB(:,iBlock) &
+                + ((/i,j,k/)-0.5)*CellSize_DB(:,iBlock)
         else
-           x = Xyz_DGB(x_,i,j,k,iBlock)
-           y = Xyz_DGB(y_,i,j,k,iBlock)
-           z = Xyz_DGB(z_,i,j,k,iBlock)
+           Coord_D = Xyz_DGB(:,i,j,k,iBlock)
         end if
         if(IsBinary)then
-           write(unit_tmp)DxBlockOut,x,y,z,PlotVar(i,j,k,1:nPlotVar)
+           write(UnitTmp_) DxBlock, Coord_D, PlotVar(i,j,k,1:nPlotVar)
         else
            do iVar=1,nPlotVar
               Plot_V(iVar) = PlotVar(i,j,k,iVar)
               if(abs(Plot_V(iVar))<1.0d-99)Plot_V(iVar)=0.0
            end do
-           write(unit_tmp,'(50(1pe13.5))') &
-                DxBlockOut, x, y, z, Plot_V(1:nPlotVar)
+           write(UnitTmp_,'(50(1pe13.5))') DxBlock, Coord_D, Plot_V(1:nPlotVar)
         endif
      end do; end do; end do
 
@@ -113,8 +113,7 @@ subroutine write_plot_idl(iFile, iBlock, nPlotVar, PlotVar, &
   zMin1 = zMin - cHalfMinusTiny*CellSize_DB(z_,iBlock)
   zMax1 = zMax + cHalfMinusTiny*CellSize_DB(z_,iBlock)
 
-  nCell = 0
-  if(IsRLonLat .or. IsCylindrical)then                 
+  if(IsRLonLat .or. IsCylindrical .and. .not.DoSaveGenCoord)then
      ! Make sure that angles around 3Pi/2 are moved to Pi/2 for x=0 cut
      ySqueezed = mod(xyzStart_BLK(Phi_,iBlock),cPi)
      ! Make sure that small angles are moved to Pi degrees for y=0 cut
@@ -130,8 +129,9 @@ subroutine write_plot_idl(iFile, iBlock, nPlotVar, PlotVar, &
           xMin1,xMax1,yMin1,yMax1,zMin1,zMax1
      write(*,*) NameSub, 'xyzStart_BLK=',iBlock,xyzStart_BLK(:,iBlock)
      write(*,*) NameSub, 'ySqueezed =',ySqueezed
-     write(*,*) NameSub, 'xyzEnd=',xyzStart_BLK(x_,iBlock)+(nI-1)*CellSize_DB(x_,iBlock),&
-          ySqueezed+(nJ-1)*CellSize_DB(y_,iBlock),&
+     write(*,*) NameSub, 'xyzEnd=', &
+          xyzStart_BLK(x_,iBlock)+(nI-1)*CellSize_DB(x_,iBlock),&
+          ySqueezed + (nJ-1)*CellSize_DB(y_,iBlock),&
           xyzStart_BLK(z_,iBlock)+(nK-1)*CellSize_DB(z_,iBlock)
   end if
 
@@ -145,7 +145,9 @@ subroutine write_plot_idl(iFile, iBlock, nPlotVar, PlotVar, &
        RETURN
 
   Dx = plot_Dx(1,iFile)
-  DxBlock=CellSize_DB(x_,iBlock); DyBlock=CellSize_DB(y_,iBlock); DzBlock=CellSize_DB(z_,iBlock)
+  DxBlock = CellSize_DB(x_,iBlock)
+  DyBlock = CellSize_DB(y_,iBlock)
+  DzBlock = CellSize_DB(z_,iBlock)
 
   ! Calculate index limits of cells inside cut
   iMin = max(1 ,floor((xMin1-xyzStart_BLK(x_,iBlock))/DxBlock)+2)
@@ -167,45 +169,45 @@ subroutine write_plot_idl(iFile, iBlock, nPlotVar, PlotVar, &
      write(*,*) NameSub, 'zMin1,zMax1=',zMin1,zMax1
   end if
 
-  if(DxBlock>=Dx)then
+  ! Count number of cells saved into the plot
+  nCell = 0
+  if(DxBlock >= Dx)then
      ! Cell is equal or coarser than Dx, save all cells in cut
-     DxBlockOut = DxBlock
-     if (plot_dimensional(iFile))DxBlockOut = DxBlockOut*No2Io_V(UnitX_)
      do k=kMin,kMax; do j=jMin,jMax; do i=iMin,iMax
         x = Xyz_DGB(x_,i,j,k,iBlock)
         y = Xyz_DGB(y_,i,j,k,iBlock)
         z = Xyz_DGB(z_,i,j,k,iBlock)
 
+        ! Check if we are inside the Cartesian box
         if(x<x1 .or. x>x2 .or. y<y1 .or. y>y2 .or. z<z1 .or. z>z2) CYCLE
 
-        if (plot_dimensional(iFile)) then
-           x = x*No2Io_V(UnitX_)
-           y = y*No2Io_V(UnitX_)
-           z = z*No2Io_V(UnitX_)
-        end if
-        if(IsBinary)then
-           write(unit_tmp) DxBlockOut, x, y, z, PlotVar(i,j,k,1:nPlotVar)
+        if(DoSaveGenCoord)then
+           Coord_D = CoordMin_DB(:,iBlock) &
+                + ((/i,j,k/)-0.5)*CellSize_DB(:,iBlock)
         else
-           do iVar=1,nPlotVar
+           Coord_D = Xyz_DGB(:,i,j,k,iBlock)
+        end if
+
+        if(IsBinary)then
+           write(UnitTmp_) DxBlock, Coord_D, PlotVar(i,j,k,1:nPlotVar)
+        else
+           do iVar=1, nPlotVar
               Plot_V(iVar) = PlotVar(i,j,k,iVar)
               if(abs(Plot_V(iVar)) < 1.0d-99) Plot_V(iVar) = 0.0
            end do
-           write(unit_tmp,'(50(1pe13.5))') &
-                DxBlockOut, x, y, z, Plot_V(1:nPlotVar)
+           write(UnitTmp_,'(50es13.5)') DxBlock, Coord_D, Plot_V(1:nPlotVar)
         endif
         nCell = nCell+1
      end do; end do; end do
   else
      ! Block is finer then required resolution
      ! Calculate restriction factor
-     nRestrict = min(nI,nint(Dx/DxBlock))
+     nRestrict = min(nI, nint(Dx/DxBlock))
 
      ! Calclulate restricted cell size
      DxBlock    = nRestrict*DxBlock
      DyBlock    = nRestrict*DyBlock
      DzBlock    = nRestrict*DzBlock
-     DxBlockOut = DxBlock
-     if (plot_dimensional(iFile))DxBlockOut = DxBlockOut*No2Io_V(UnitX_)
 
      ! Restriction is limited by the width of the plotting region
      nRestrictX = min(iMax-iMin+1, nRestrict)
@@ -213,40 +215,42 @@ subroutine write_plot_idl(iFile, iBlock, nPlotVar, PlotVar, &
      nRestrictZ = min(kMax-kMin+1, nRestrict)
 
      ! Factor for taking the average
-     Restrict=1./(nRestrictX*nRestrictY*nRestrictZ)
+     Restrict = 1./(nRestrictX*nRestrictY*nRestrictZ)
 
      if(DoTestMe) write(*,*) NameSub, 'nRestrict, X, Y, Z,Restrict=',&
           nRestrict, nRestrictX, nRestrictY, nRestrictZ, Restrict
 
      ! Loop for the nRestrictX*nRestrictY*nRestrictZ bricks inside the cut
-     do k=kMin,kMax,nRestrictZ
-        k2=k+nRestrictZ-1
-        do j=jMin,jMax,nRestrictY
-           j2=j+nRestrictY-1
-           do i=iMin,iMax,nRestrictX
-              i2=i+nRestrictX-1
-              x = 0.5*(Xyz_DGB(x_,i,j,k,iBlock)+Xyz_DGB(x_,i2,j2,k2,iBlock))
-              y = 0.5*(Xyz_DGB(y_,i,j,k,iBlock)+Xyz_DGB(y_,i2,j2,k2,iBlock))
-              z = 0.5*(Xyz_DGB(z_,i,j,k,iBlock)+Xyz_DGB(z_,i2,j2,k2,iBlock))
+     do k = kMin, kMax, nRestrictZ
+        k2 = k + nRestrictZ - 1
+        do j = jMin, jMax, nRestrictY
+           j2 = j + nRestrictY - 1
+           do i = iMin, iMax, nRestrictX
+              i2 = i + nRestrictX - 1
+              x = 0.5*(Xyz_DGB(x_,i,j,k,iBlock) + Xyz_DGB(x_,i2,j2,k2,iBlock))
+              y = 0.5*(Xyz_DGB(y_,i,j,k,iBlock) + Xyz_DGB(y_,i2,j2,k2,iBlock))
+              z = 0.5*(Xyz_DGB(z_,i,j,k,iBlock) + Xyz_DGB(z_,i2,j2,k2,iBlock))
 
               if(x<x1 .or. x>x2 .or. y<y1 .or. y>y2 .or. z<z1 .or. z>z2) &
                    CYCLE
 
-              if(plot_dimensional(iFile))then
-                 x = x*No2Io_V(UnitX_)
-                 y = y*No2Io_V(UnitX_)
-                 z = z*No2Io_V(UnitX_)
+              if(DoSaveGenCoord)then
+                 Coord_D = CoordMin_DB(:,iBlock) &
+                      + (0.5*(/i+i2,j+j2,k+k2/) - 0.5)*CellSize_DB(:,iBlock)
+              else
+                 Coord_D = (/x, y, z/)
               end if
+
               do iVar=1,nPlotVar
                  Plot_V(iVar) = Restrict*sum(PlotVar(i:i2,j:j2,k:k2,iVar))
               end do
               if(IsBinary)then
-                 write(unit_tmp)DxBlockOut,x,y,z,Plot_V(1:nPlotVar)
+                 write(UnitTmp_)DxBlock, Coord_D, Plot_V(1:nPlotVar)
               else
-                 do iVar=1,nPlotVar
+                 do iVar = 1, nPlotVar
                     if(abs(Plot_V(iVar)) < 1.0d-99)Plot_V(iVar)=0.0
                  end do
-                 write(unit_tmp,'(50es13.5)')DxBlockOut,x,y,z,&
+                 write(UnitTmp_,'(50es13.5)')DxBlock, Coord_D,&
                       Plot_V(1:nPlotVar)
               endif
               nCell=nCell+1
