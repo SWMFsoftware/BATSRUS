4d1e42b25efe4f019c34e79d1138823b39d1ae35
gtoth <>
2015-06-26 22:18:05 +0000

    Slight change in the logic for avoiding very small time step. This might be more robust.
    Former-commit-id: b761b6b82eeb0899aaa68fb49a1f450c641fc94b

--- src/ModBatsrusMethods.f90
+++ src/ModBatsrusMethods.f90
@@ -446,7 +446,7 @@ subroutine BATS_advance(TimeSimulationLimit)
   ! Adjust Time_Simulation to match TimeSimulationLimit if it is very close
   if(  time_accurate .and. &
        Time_Simulation < TimeSimulationLimit .and. &
-       Time_Simulation + 1e-6*Dt*No2Si_V(UnitT_) >= TimeSimulationLimit)then
+       TimeSimulationLimit - Time_Simulation <= 1e-6*Dt*No2Si_V(UnitT_))then
 
      if(iProc == 0 .and. lVerbose > 0)then
         call write_prefix; write(iUnitOut,*) NameSub, &
