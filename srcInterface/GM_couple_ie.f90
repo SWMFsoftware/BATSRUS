c078036edbd4944ef00725a952e7e8e7778bc9af
gtoth <>
2015-10-07 16:29:11 +0000

    get_info_for_ie now sets the number and names of variables to pass from IE. Use variable name array in GM_put_from_ie. Remvoed GM_put_mag_from_ie. Removed obsolete print_iono_potential.
    Former-commit-id: 80b23af4fffe00499867fc396bb5e03d6f3190b4

--- srcInterface/GM_couple_ie.f90
+++ srcInterface/GM_couple_ie.f90
@@ -22,63 +22,47 @@ module GM_couple_ie
   public:: GM_get_for_ie
   public:: GM_get_info_for_ie
   public:: GM_put_from_ie
-  public:: GM_put_mag_from_ie
-  public:: print_iono_potential
 
 contains
 
-  !========================================================================== 
-  subroutine print_iono_potential
-
-    use ModIoUnit, ONLY: UnitTmp_
-
-    integer:: i, j
-
-    do j=1,nPhiIono
-       do i=1,nThetaIono
-          write(UNITTMP_,'(2i4,3G14.6)')i,j,&
-               ThetaIono_I(i),PhiIono_I(j),IonoPotential_II(i,j)
-       end do
-    end do
-
-  end subroutine print_iono_potential
-
   !============================================================================
-  subroutine GM_get_info_for_ie(nMagOut, NameMagsOut_I, CoordMagsOut_DI)
-    
-    use ModGroundMagPerturb, ONLY: nMagTotal, nMagnetometer, nGridMag, &
-         TypeCoordMag, TypeCoordGrid, PosMagnetometer_II
+  subroutine GM_get_info_for_ie(nVar, NameVar_I)
 
-    integer, intent(out) :: nMagOut
-    character(len=3), intent(out),optional :: NameMagsOut_I(     nMagTotal)
-    real,             intent(out),optional :: CoordMagsOut_DI(2, nMagTotal)
+    use ModMain,             ONLY: TypeBc_I, body1_
+    use ModGroundMagPerturb, ONLY: nMagTotal
 
-    integer :: iMag
+    ! Pass number and names of variables requested from IE
+
+    integer, intent(out) :: nVar
+    character(len=*), intent(out), optional:: NameVar_I(:)
+
     logical :: DoTest, DoTestMe
     character(len=*), parameter :: NameSub='GM_get_info_for_ie'
     !-------------------------------------------------------------------------
     call CON_set_do_test(NameSub, DoTest, DoTestMe)
 
-    ! Collect magnetometer info to share with IE.
-    nMagOut=nMagTotal
-    
-    if(nMagOut==0) return
+    ! Always pass electric potential
+    nVar = 1
+    if(present(NameVar_I)) NameVar_I(nVar) = 'potential'
 
-    if (present(CoordMagsOut_DI)) &
-         CoordMagsOut_DI = PosMagnetometer_II
+    ! Joule heating is needed for empirical inner boundary
+    if(TypeBc_I(body1_) == 'ionosphereoutflow')then
+       nVar = nVar+1
+       if(present(NameVar_I)) NameVar_I(nVar) = 'jouleheat'
+    end if
 
-    if (present(NameMagsOut_I)) then
-       NameMagsOut_I(1:nMagnetometer)                         = TypeCoordMag
-       NameMagsOut_I(nMagnetometer+1:nMagnetometer+nGridMag)  = TypeCoordGrid
-       NameMagsOut_I(nMagnetometer+nGridMag+1:nMagTotal)      = 'SMG'
-    endif
+    ! Conductances are needed for magnetometer calculations
+    if(nMagTotal > 0)then
+       nVar = nVar + 1
+       if(present(NameVar_I)) NameVar_I(nVar) = 'sigmahall'
+       nVar = nVar + 1
+       if(present(NameVar_I)) NameVar_I(nVar) = 'sigmapedersen'
+    end if
 
-    if(DoTestMe .and. present(NameMagsOut_I))then
-       write(*,*) NameSub//' is sending the following to IE:'
-       do iMag=1, nMagTotal
-          write(*,*)'  iMag, Lat, Lon, Coord = ', &
-               iMag, CoordMagsOut_DI(:,iMag), NameMagsOut_I(iMag)
-       end do
+    if(DoTestMe)then
+       write(*,*) NameSub//' set nVar=', nVar
+       if(present(NameVar_I)) write(*,*) NameSub//' set NameVar_I=', &
+            NameVar_I
     end if
 
   end subroutine GM_get_info_for_ie
@@ -224,13 +208,10 @@ contains
   end subroutine GM_get_for_ie
 
   !============================================================================
-  subroutine GM_put_from_ie(Buffer_IIV, iSize, jSize, nVar)
+  subroutine GM_put_from_ie(Buffer_IIV, iSize, jSize, nVar, NameVar_I)
 
-    ! Receive nVar variables from IE on the IE grid:
-    !   1. Electric potential   (always)
-    !   2. Joule heating        (if nVar = 2 or 4)
-    !   3. Hall conductance     (if nVar > 2)
-    !   4. Pedersen conductance (if nVar > 2)
+    ! Receive nVar variables listed in NameVar_I from IE on the IE grid
+    ! and store them into the allocatable arrays in ModIeCoupling
 
     use ModPhysics, ONLY: &
          Si2No_V, UnitX_, UnitElectric_, UnitPoynting_, UnitJ_
@@ -238,6 +219,9 @@ contains
 
     integer, intent(in) :: iSize, jSize, nVar
     real,    intent(in) :: Buffer_IIV(iSize,jSize,nVar)
+    character(len=*), intent(in):: NameVar_I(nVar)
+
+    integer:: iVar
 
     logical :: DoTest, DoTestMe
     character(len=*), parameter :: NameSub='GM_put_from_ie'
@@ -250,63 +234,52 @@ contains
          Grid_C(IE_) % Coord2_I, &
          Grid_C(IE_) % Coord3_I(1))
 
-    if(.not. allocated(IonoPotential_II)) allocate( &
-         IonoPotential_II(nThetaIono, nPhiIono))
+    do iVar = 1, nVAr
+       select case(NameVar_I(iVar))
+       case('potential')
+          if(.not. allocated(IonoPotential_II)) &
+               allocate(IonoPotential_II(nThetaIono, nPhiIono))
+          ! Electric potential has units of [E]*[x] (in SI: V/m*m=V)
+          IonoPotential_II = Buffer_IIV(:,:,iVar) &
+               *Si2No_V(UnitElectric_)*Si2No_V(UnitX_)
+          call calc_grad_ie_potential
+       case('jouleheat')
+          if (.not. allocated(IonoJouleHeating_II)) &
+               allocate(IonoJouleHeating_II(nThetaIono, nPhiIono))
+          ! Joule heating variable has units of Poynting flux
+          IonoJouleHeating_II = Buffer_IIV(:,:,2) &
+               *Si2No_V(UnitPoynting_)
 
-    ! Electric potential has units of [E]*[x] (in SI: V/m*m=V)
-    IonoPotential_II = Buffer_IIV(:,:,1)*Si2No_V(UnitElectric_)*Si2No_V(UnitX_)
-    call calc_grad_ie_potential
+          ! scale Joule heating from ionosphere to GM inner BC
+          call map_jouleheating_to_inner_bc
+       case('sigmahall')
+          if(.not. allocated(SigmaHall_II)) &
+               allocate(SigmaHall_II(nThetaIono,nPhiIono))
 
-    if(nVar == 2 .or. nVar == 4)then
-       if (.not. allocated(IonoJouleHeating_II)) &
-            allocate(IonoJouleHeating_II(nThetaIono, nPhiIono))
+          ! Height integrated conductivty has units of [j]*[x]/[E] (in SI:A/V)
+          SigmaHall_II = Buffer_IIV(:,:,iVar) &
+               *Si2No_V(UnitJ_)*Si2No_V(UnitX_)/Si2No_V(UnitElectric_)
 
-       ! Add the iono. jouleheating - Yiqun Sep 2008
-       IonoJouleHeating_II = Buffer_IIV(:,:,2) * Si2No_V(UnitPoynting_)
-       call map_jouleheating_to_inner_bc
-    end if
+          ! The ionosphere currents will need recalculation, so deallocate them
+          if(allocated(jHall_DII)) deallocate(jHall_DII)
 
-    if(nVar > 2)then
-       ! Hall and Pedersen conductivities
-       if(.not. allocated(SigmaHall_II)) &
-            allocate(SigmaHall_II(iSize,jSize), SigmaPedersen_II(iSize,jSize))
+       case('sigmapedersen')
+          if(.not. allocated(SigmaPedersen_II)) &
+               allocate(SigmaPedersen_II(nThetaIono,nPhiIono))
 
-       ! The ionosphere currents will need recalculation, so deallocate them
-       if(allocated(jHall_DII)) deallocate(jHall_DII, jPedersen_DII)
+          ! Height integrated conductivty has units of [j]*[x]/[E] (in SI:A/V)
+          SigmaPedersen_II = Buffer_IIV(:,:,iVar) &
+               *Si2No_V(UnitJ_)*Si2No_V(UnitX_)/Si2No_V(UnitElectric_)
 
-       ! Height integrated conductivty has units of [j]*[x]/[E] (in SI:A/V)
-       SigmaHall_II     = Buffer_IIV(:,:,nVar-1) &
-            *Si2No_V(UnitJ_)*Si2No_V(UnitX_)/Si2No_V(UnitElectric_)
-       SigmaPedersen_II = Buffer_IIV(:,:,nVar) &
-            *Si2No_V(UnitJ_)*Si2No_V(UnitX_)/Si2No_V(UnitElectric_)
-    endif
+          ! The ionosphere currents will need recalculation, so deallocate them
+          if(allocated(jPedersen_DII)) deallocate(jPedersen_DII)
+       case default
+          call stop_mpi(NameSub//' unknown variable='//NameVar_I(iVar))
+       end select
+    end do
 
     if(DoTest)write(*,*)NameSub,': done'
 
   end subroutine GM_put_from_ie
-
-  !==========================================================================
-  subroutine GM_put_mag_from_ie(Buffer_DII, iSize)
-    ! Get magnetometer "measurements" from IE.  They are received via input
-    ! array Buffer_DII, which has dimensions (3x2xiSize) where iSize should
-    ! equal the total number of shared magnetometers.
-
-    use ModGroundMagPerturb, ONLY: nMagTotal, IeMagPerturb_DII
-
-    integer, intent(in) :: iSize
-    real, intent(in)    :: Buffer_DII(3,2,iSize)
-
-    character(len=*), parameter :: NameSub='GM_put_mag_from_ie'
-    logical :: DoTest, DoTestMe
-    !--------------------------------------------------------------------------
-    call set_oktest(NameSub, DoTest, DoTestMe)
-
-    if(DoTestMe)write(*,*)NameSub, ' nMagTotal, iSize = ',  nMagTotal, iSize
-
-    ! Place all mag data into right place (keep hall/pederson separate):
-    if (nMagTotal>0) &
-         IeMagPerturb_DII = Buffer_DII
-
-  end subroutine GM_put_mag_from_ie
 
 end module GM_couple_ie
