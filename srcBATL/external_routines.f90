97416852f28e31cb393d48ce10971134348f6cd7
gtoth <>
2015-10-27 21:40:55 +0000

    user_specify_refinement --> user_block_inside_region
    Former-commit-id: e94ab2c2fb45c54ddde97891111eec0ea9544e16

--- srcBATL/external_routines.f90
+++ srcBATL/external_routines.f90
@@ -14,16 +14,23 @@ subroutine CON_stop(String)
   stop
 end subroutine CON_stop
 !=============================================================================
-subroutine user_specify_refinement(iBlock, iArea, DoRefine)
-  
+subroutine user_block_inside_region(iArea, iBlock, nValue, NameLocation, &
+     IsInside, IsInside_I, Value_I)
+
   implicit none
 
-  integer, intent(in) :: iBlock, iArea
-  logical, intent(out):: DoRefine
+  integer,   intent(in):: iArea        ! area index in BATL_region
+  integer,   intent(in):: iBlock       ! block index
+  integer,   intent(in):: nValue       ! number of output values
+  character, intent(in):: NameLocation ! c, g, x, y, z, or n
 
-  character(len=*), parameter :: NameSub = 'user_specify_refinement'
+  logical, optional, intent(out) :: IsInside
+  logical, optional, intent(out) :: IsInside_I(nValue)
+  real,    optional, intent(out) :: Value_I(nValue)
+
+  character(len=*), parameter :: NameSub = 'user_block_inside_region'
   !-------------------------------------------------------------------
   call CON_stop(NameSub//' is not implemented')
 
-end subroutine user_specify_refinement
-!=============================================================================
+end subroutine user_block_inside_region
+!=====================================================================
