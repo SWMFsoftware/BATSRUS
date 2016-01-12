73ceed975c4e03c36447b64c87bf1d6520245faa
gtoth <>
2016-01-12 19:59:49 +0000

    Avoid warnings about -i.
    Former-commit-id: d6308502f5c011b5ef56790db74118627d1cfe89

--- Config.pl
+++ Config.pl
@@ -1,6 +1,11 @@
-#!/usr/bin/perl -i
-#  Copyright (C) 2002 Regents of the University of Michigan, portions used with permission 
+#!/usr/bin/perl
+#  Copyright (C) 2002 Regents of the University of Michigan, 
+#  portions used with permission 
 #  For more information, see http://csem.engin.umich.edu/tools/swmf
+
+# Allow in-place editing
+$^I = "";
+
 use strict;
 
 our $Component = "GM";
