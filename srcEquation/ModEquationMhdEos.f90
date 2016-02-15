a756b8189fca6a2bc053f311a694e4676f3990f3
bartvand <>
2016-02-15 19:45:01 +0000

    test5 is now a coupled EE-SC test Modified Files: 	Makefile.test Added Files: 	GM/BATSRUS/srcEquation/ModEquationMhdEos.f90 	Param/PARAM.in.test.EESC Param/PARAM.in.test.SC 	output/test5/EE.log output/test5/SC.log Removed Files: 	output/test5/EE_log_n000011.log
    Former-commit-id: f8c5a938b89f5101f511c294fdc8e50d8a395930

---
+++ srcEquation/ModEquationMhdEos.f90
@@ -0,0 +1,112 @@
+!  Copyright (C) 2002 Regents of the University of Michigan,
+!  portions used with permission 
+!  For more information, see http://csem.engin.umich.edu/tools/swmf
+module ModVarIndexes
+
+  use ModSingleFluid
+  use ModExtraVariables, &
+       Redefine1 => ExtraEint_
+
+  implicit none
+
+  save
+
+  ! This equation module contains the standard MHD equations with
+  ! extra internal energy
+  character (len=*), parameter :: NameEquation='MHD+eos'
+
+  ! Number of variables without energy:
+  integer, parameter :: nVar = 9
+
+  ! Named indexes for State_VGB and other variables
+  ! These indexes should go subsequently, from 1 to nVar+1.
+  ! The energy is handled as an extra variable, so that we can use
+  ! both conservative and non-conservative scheme and switch between them.
+  integer, parameter :: &
+       Rho_       = 1,          &
+       RhoUx_     = 2, Ux_ = 2, &
+       RhoUy_     = 3, Uy_ = 3, &
+       RhoUz_     = 4, Uz_ = 4, &
+       Bx_        = 5,          &
+       By_        = 6,          &
+       Bz_        = 7,          &
+       ExtraEint_ = 8,          &
+       p_         = nVar,       &
+       Energy_    = nVar+1
+
+  ! This allows to calculate RhoUx_ as RhoU_+x_ and so on.
+  integer, parameter :: U_ = Ux_ - 1, RhoU_ = RhoUx_ - 1, B_ = Bx_ - 1
+
+  ! These arrays are useful for multifluid
+  integer, parameter :: iRho_I(nFluid)   = (/Rho_/)
+  integer, parameter :: iRhoUx_I(nFluid) = (/RhoUx_/)
+  integer, parameter :: iRhoUy_I(nFluid) = (/RhoUy_/)
+  integer, parameter :: iRhoUz_I(nFluid) = (/RhoUz_/)
+  integer, parameter :: iP_I(nFluid)     = (/p_/)
+
+  ! The default values for the state variables:
+  ! Variables which are physically positive should be set to 1,
+  ! variables that can be positive or negative should be set to 0:
+  real, parameter :: DefaultState_V(nVar+nFluid) = (/ & 
+       1.0, & ! Rho_
+       0.0, & ! RhoUx_
+       0.0, & ! RhoUy_
+       0.0, & ! RhoUz_
+       0.0, & ! Bx_
+       0.0, & ! By_
+       0.0, & ! Bz_
+       0.0, & ! ExtraEint_
+       1.0, & ! p_
+       1.0 /) ! Energy_
+
+  ! The names of the variables used in i/o
+  character(len=4) :: NameVar_V(nVar+nFluid) = (/ &
+       'Rho ', & ! Rho_
+       'Mx  ', & ! RhoUx_
+       'My  ', & ! RhoUy_
+       'Mz  ', & ! RhoUz_
+       'Bx  ', & ! Bx_
+       'By  ', & ! By_
+       'Bz  ', & ! Bz_
+       'EInt', & ! ExtraEint_
+       'P   ', & ! p_
+       'E   '/)  ! Energy_
+
+  ! The space separated list of nVar conservative variables for plotting
+  character(len=*), parameter :: NameConservativeVar = &
+       'rho mx my mz bx by bz EInt e'
+
+  ! The space separated list of nVar primitive variables for plotting
+  character(len=*), parameter :: NamePrimitiveVar = &
+       'Rho Ux Uy Uz bx by bz EInt p'
+
+  ! The space separated list of nVar primitive variables for TECplot output
+  character(len=*), parameter :: NamePrimitiveVarTec = &
+       '"`r", "U_x", "U_y", "U_z", "B_x", "B_y", "B_z", "EInt", "p"'
+
+  ! Names of the user units for IDL and TECPlot output
+  character(len=20) :: &
+       NameUnitUserIdl_V(nVar+nFluid) = '', NameUnitUserTec_V(nVar+nFluid) = ''
+
+  ! The user defined units for the variables
+  real :: UnitUser_V(nVar+nFluid) = 1.0
+
+  ! The only scalar to be advected is the radiation energy density
+  integer, parameter :: ScalarFirst_ = ExtraEint_, ScalarLast_ = ExtraEint_
+
+  ! There are no multi-species
+  logical, parameter :: UseMultiSpecies = .false.
+
+  ! Declare the following variables to satisfy the compiler
+  integer, parameter :: SpeciesFirst_ = 1, SpeciesLast_ = 1
+  real               :: MassSpecies_V(SpeciesFirst_:SpeciesLast_)
+
+contains
+
+  subroutine init_mod_equation
+
+    call init_mhd_variables
+
+  end subroutine init_mod_equation
+
+end module ModVarIndexes
