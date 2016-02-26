0e6571620d5f5870424a6ee6eddd0a3aa656bd3f
gtoth <>
2016-02-26 16:00:37 +0000

    Use TimeSimulationLimit=-1.0 to indicate no limiting.
    Former-commit-id: 3cb48f5bc91a5687c060dc8ac283c0bb0e5c71e4

--- srcInterface/GM_couple_pc.f90
+++ srcInterface/GM_couple_pc.f90
@@ -17,15 +17,16 @@ contains
 
   subroutine GM_get_for_pc_dt(DtSi)
 
+    ! Calculate the global time step for PC
+    
     use ModMain,            ONLY: Dt
     use ModPhysics,         ONLY: No2Si_V, UnitT_
     use ModTimeStepControl, ONLY: set_global_timestep
 
     real, intent(out) ::  DtSi
     !--------------------------------------------------------------------------
-
-    ! use 1.0e12 >> dt  for limiting time 
-    call set_global_timestep(1.0e12)
+    ! use -1.0 so that no limit is applied on Dt
+    call set_global_timestep(TimeSimulationLimit=-1.0)
 
     DtSi = Dt*No2Si_V(UnitT_)
 
