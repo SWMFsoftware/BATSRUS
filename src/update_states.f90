344261e4d65b596c8e97d10ca306e4f6c1198b5b
gtoth <>
2016-04-29 21:27:39 +0000

    Fixed copyright message
    Former-commit-id: 506b8300b9a9cd5703bbcbdc9d2c85db70fd0728

--- src/update_states.f90
+++ src/update_states.f90
@@ -1,7 +1,7 @@
 !  Copyright (C) 2002 Regents of the University of Michigan,
 !  portions used with permission
 !  For more information, see http://csem.engin.umich.edu/tools/swmf
-!This code is a copyright protected software (c) 2002- University of Michigan
+
 subroutine update_states(iStage,iBlock)
   use ModProcMH
   use ModMain
