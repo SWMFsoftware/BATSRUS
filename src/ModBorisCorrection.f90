268e2e256eefd88255d4f49235bdfc6c3e8ae972
gtoth <>
2016-12-27 21:48:45 +0000

    Modified update_explicit so that Boris correction is compatible with any time stepping scheme, including RK3 and RK4. The transformations to and from semi-relativistiv variables are moved into ModBorisCorrection. Tested on 1D problem, and seems to work fine. Modified Files: 	Makefile update_states_MHD.f90 Added Files: 	ModBorisCorrection.f90
    Former-commit-id: dcb03c09510bb772a51d00bba07ef983b00b388e

---
+++ src/ModBorisCorrection.f90
@@ -0,0 +1,82 @@
+module ModBorisCorrection
+
+  use ModVarIndexes, ONLY: nVar, Rho_, RhoUx_, RhoUz_, Bx_, Bz_
+  use ModPhysics,    ONLY: c2Light, inv_c2light
+  use ModCoordTransform, ONLY: cross_product
+
+  implicit none
+  private ! except
+
+  public:: mhd_to_boris ! from classical to semi-relativistic variables
+  public:: boris_to_mhd ! from semi-relativistic to classical variables
+
+contains
+  !==========================================================================
+  subroutine mhd_to_boris(State_V, Energy, B0_D)
+
+    real, intent(inout)          :: State_V(nVar)
+    real, intent(inout), optional:: Energy
+    real, intent(in),    optional:: B0_D(3)
+
+    ! Replace MHD momentum with semi-relativistic momentum in State_V.
+    ! Replace MHD energy density Energy with semi-relativistic value.
+    ! Use B0=B0_D in the total magnetic field if present.
+
+    real:: Rho, b_D(3), u_D(3)
+    !-------------------------------------------------------------------------
+
+    b_D = State_V(Bx_:Bz_)
+    if(present(b0_D)) b_D = b_D + B0_D
+
+    Rho = State_V(Rho_)
+    u_D = State_V(RhoUx_:RhoUz_)/Rho
+
+    ! Gombosi et al. 2001, eq(12) with vA^2 = B^2/Rho
+    !
+    ! RhoUBoris = RhoU + (RhoU B^2 - B RhoU.B)/(Rho c^2)
+    !           = U*(Rho + B^2/c^2 - B U.B/c^2
+    State_V(RhoUx_:RhoUz_) = u_D*(Rho + sum(b_D**2)*inv_c2LIGHT) &
+         - b_D*sum(u_D*b_D)*inv_c2LIGHT
+
+    ! e_Boris = e + (UxB)^2/(2 c^2)   eq 92
+    if(present(Energy)) &
+         Energy = Energy + 0.5*sum(cross_product(u_D, b_D)**2)*inv_c2LIGHT
+
+  end subroutine mhd_to_boris
+  !==========================================================================
+  subroutine boris_to_mhd(State_V, Energy, B0_D)
+
+    real, intent(inout)          :: State_V(nVar)
+    real, intent(inout), optional:: Energy
+    real, intent(in),    optional:: B0_D(3)
+
+    ! Replace semi-relativistic momentum with classical momentum in State_V.
+    ! Replace semi-relativistic energy density Energy with classical value.
+    ! Use B0=B0_D in the total magnetic field if present.
+
+    real:: RhoC2, b_D(3), RhoUBoris_D(3), u_D(3)
+    !-------------------------------------------------------------------------
+
+    b_D = State_V(Bx_:Bz_)
+    if(present(b0_D)) b_D = b_D + B0_D
+
+    RhoC2       = State_V(Rho_)*c2Light
+    RhoUBoris_D = State_V(RhoUx_:RhoUz_)
+
+    ! Gombosi et al. 2001, eq(16) with vA^2 = B^2/Rho, gA^2=1/(1+vA^2/c^2)
+    !
+    ! RhoU = [RhoUBoris + B*B.RhoUBoris/(Rho c^2)]/(1+B^2/Rho c^2)
+    !      = (RhoUBoris*Rho*c^2 + B*B.RhoUBoris)/(Rho*c^2 + B^2)
+
+    State_V(RhoUx_:RhoUz_) =(RhoC2*RhoUBoris_D + b_D*sum(b_D*RhoUBoris_D)) &
+         /(RhoC2 + sum(b_D**2))
+
+    if(present(Energy))then
+       ! e = e_boris - (U x B)^2/(2 c^2)   eq 92
+       u_D = State_V(RhoUx_:RhoUz_)/State_V(Rho_)
+       Energy = Energy - 0.5*sum(cross_product(u_D, b_D)**2)*inv_c2LIGHT
+    end if
+
+  end subroutine boris_to_mhd
+
+end module ModBorisCorrection
--- src/ModUpdateState.f90
+++ src/ModUpdateState.f90
@@ -36,11 +36,7 @@ subroutine update_states_MHD(iBlock)
   integer :: i, j, k
 
   ! These variables have to be double precision for accurate Boris scheme
-  real(Real8_) :: FullBx, FullBy, FullBz, fullBB, rhoc2, UdotBc2, gA2_Boris,&
-       FullBxOld, FullByOld, FullBzOld, Ux, Uy, Uz, UxOld, UyOld, UzOld,&
-       Bx, By, Bz, BxOld, ByOld, BzOld, B0x, B0y, B0z, RhoUx, RhoUy, RhoUz,&
-       mBorisMinusRhoUxOld, mBorisMinusRhoUyOld, mBorisMinusRhoUzOld,&
-       Rho, RhoInv, eCorr
+  real(Real8_) :: FullBx, FullBy, FullBz, fullBB, rhoc2, gA2_Boris
   real:: DtLocal, DtFactor, Coeff
   real:: B0_DC(3,nI,nJ,nK)
   logical :: DoTest, DoTestMe
@@ -245,6 +241,8 @@ contains
 
   subroutine update_explicit
 
+    use ModBorisCorrection, ONLY: mhd_to_boris, boris_to_mhd
+
     ! Allocatable storage for classical 4th order Runge-Kutta scheme
     real, allocatable, save:: Rk4_VCB(:,:,:,:,:), Rk4_CBI(:,:,:,:,:)
 
@@ -252,6 +250,31 @@ contains
     real:: Coeff1, Coeff2
     integer:: iFluid, iRho
     !--------------------------------------------------------------------------
+
+    if(boris_correction) then
+       ! Convert StateOld_VCB and State_VGB from classical MHD variables
+       ! to semi-relativistic MHD variable
+       do k=1,nK; do j=1,nJ; do i=1,nI
+          if(.not.true_cell(i,j,k,iBlock)) CYCLE
+
+          if(UseB0)then
+             call mhd_to_boris(StateOld_VCB(:,i,j,k,iBlock), &
+                  EnergyOld_CBI(i,j,k,iBlock,1), B0_DGB(:,i,j,k,iBlock))
+             call mhd_to_boris(State_VGB(:,i,j,k,iBlock), &
+                  Energy_GBI(i,j,k,iBlock,1), B0_DGB(:,i,j,k,iBlock))
+          else
+             call mhd_to_boris(StateOld_VCB(:,i,j,k,iBlock), &
+                  EnergyOld_CBI(i,j,k,iBlock,1))
+             call mhd_to_boris(State_VGB(:,i,j,k,iBlock), &
+                  Energy_GBI(i,j,k,iBlock,1))
+          end if
+       end do; end do; end do
+          
+
+       if(DoTestMe)write(*,*) NameSub, ' after mhd_to_boris=', &
+            State_VGB(VarTest,iTest,jTest,kTest,iBlock), &
+            Energy_GBI(iTest,jTest,kTest,iBlock,:)
+    endif
 
     if(UseHalfStep .or. nStage == 1 .or. nStage == 4)then
        ! Update state variables starting from level n (=old) state
@@ -368,6 +391,29 @@ contains
          State_VGB(VarTest,iTest,jTest,kTest,iBlock), &
          Energy_GBI(iTest,jTest,kTest,iBlock,:)
 
+    if(boris_correction) then
+       ! Convert StateOld_VCB and State_VGB back from 
+       ! semi-relativistic to classical MHD variables
+       do k=1,nK; do j=1,nJ; do i=1,nI
+          if(.not.true_cell(i,j,k,iBlock)) CYCLE
+
+          if(UseB0)then
+             call boris_to_mhd(StateOld_VCB(:,i,j,k,iBlock), &
+                  EnergyOld_CBI(i,j,k,iBlock,1), B0_DGB(:,i,j,k,iBlock))
+             call boris_to_mhd(State_VGB(:,i,j,k,iBlock), &
+                  Energy_GBI(i,j,k,iBlock,1), B0_DGB(:,i,j,k,iBlock))
+          else
+             call boris_to_mhd(StateOld_VCB(:,i,j,k,iBlock), &
+                  EnergyOld_CBI(i,j,k,iBlock,1))
+             call boris_to_mhd(State_VGB(:,i,j,k,iBlock), &
+                  Energy_GBI(i,j,k,iBlock,1))
+          end if
+       end do; end do; end do
+          
+       if(DoTestMe)write(*,*) NameSub, ' after boris_to_mhd=', &
+            State_VGB(VarTest,iTest,jTest,kTest,iBlock), &
+            Energy_GBI(iTest,jTest,kTest,iBlock,:)
+    endif
 
     if(UseMultiSpecies)then
        ! Fix negative species densities
@@ -456,121 +502,6 @@ contains
                + Source_VC(Pe_,i,j,k))**GammaElectron
        end do; end do; end do
     end if
-
-    if(boris_correction) then
-       if(UseB0)then
-          B0_DC=B0_DGB(:,1:nI,1:nJ,1:nK,iBlock)
-       else
-          B0_DC=0.00
-       end if
-       do k=1,nK; do j=1,nJ; do i=1,nI
-          if(.not.true_cell(i,j,k,iBlock)) CYCLE
-          B0x= B0_DC(x_,i,j,k)
-          B0y= B0_DC(y_,i,j,k)
-          B0z= B0_DC(z_,i,j,k)
-
-          BxOld= StateOld_VCB(Bx_,i,j,k,iBlock)
-          ByOld= StateOld_VCB(By_,i,j,k,iBlock)
-          BzOld= StateOld_VCB(Bz_,i,j,k,iBlock)
-          fullBxOld = B0x + BxOld
-          fullByOld = B0y + ByOld
-          fullBzOld = B0z + BzOld
-
-          Rho = StateOld_VCB(rho_,i,j,k,iBlock)
-          rhoc2  = Rho*c2LIGHT
-
-          RhoUx = StateOld_VCB(rhoUx_,i,j,k,iBlock)
-          RhoUy = StateOld_VCB(rhoUy_,i,j,k,iBlock)
-          RhoUz = StateOld_VCB(rhoUz_,i,j,k,iBlock)
-
-          RhoInv=1/Rho
-
-          UxOld=RhoUx*RhoInv
-          UyOld=RhoUy*RhoInv
-          UzOld=RhoUz*RhoInv
-
-          UdotBc2= (RhoUx*fullBxOld + &
-               RhoUy*fullByOld + &
-               RhoUz*fullBzOld)/rhoc2
-
-          gA2_Boris= (fullBxOld**2 + fullByOld**2 + fullBzOld**2)/rhoc2
-
-          ! rhoU_Boris_old - rhoU_Old= (U B^2 - B U.B)/c^2
-          !    
-
-          MBorisMinusRhoUxOld = RhoUx*ga2_Boris - fullBxOld*UdotBc2
-          MBorisMinusRhoUyOld = RhoUy*ga2_Boris - fullByOld*UdotBc2
-          MBorisMinusRhoUzOld = RhoUz*ga2_Boris - fullBzOld*UdotBc2
-
-          Bx= State_VGB(Bx_,i,j,k,iBlock)
-          By= State_VGB(By_,i,j,k,iBlock)
-          Bz= State_VGB(Bz_,i,j,k,iBlock)
-
-          fullBx = B0x + Bx
-          fullBy = B0y + By
-          fullBz = B0z + Bz
-          fullBB = fullBx**2 + fullBy**2 + fullBz**2
-
-          Rho  = State_VGB(rho_,i,j,k,iBlock)
-          rhoc2  = Rho*c2LIGHT
-          RhoUx = State_VGB(rhoUx_,i,j,k,iBlock)
-          RhoUy = State_VGB(rhoUy_,i,j,k,iBlock)
-          RhoUz = State_VGB(rhoUz_,i,j,k,iBlock)
-
-          UdotBc2= (RhoUx*fullBx + &
-               MBorisMinusRhoUxOld*Source_VC(Bx_,i,j,k)+ &
-               RhoUy*fullBy +      &
-               MBorisMinusRhoUyOld*Source_VC(By_,i,j,k)+ &
-               RhoUz*fullBz+       &
-               MBorisMinusRhoUzOld*Source_VC(Bz_,i,j,k))/rhoc2
-          gA2_Boris=rhoc2/(fullBB+rhoc2)
-
-          ! rhoU = 1/[1+BB/(rho c^2)]* (rhoU_Boris 
-          !                             + (rhoUBorisdotB/(rho c^2) * B)
-          !  ((M_Boris_old-RhoU) /cdot FullB_old)=0! 
-
-          RhoUx = gA2_Boris * &
-               (RhoUx+MBorisMinusRhoUxOld+UdotBc2*fullBx)
-          RhoUy = gA2_Boris * &
-               (RhoUy+MBorisMinusRhoUyOld+UdotBc2*fullBy)
-          RhoUz = gA2_Boris * &
-               (RhoUz+MBorisMinusRhoUzOld+UdotBc2*fullBz)
-
-
-          State_VGB(rhoUx_,i,j,k,iBlock) = RhoUx
-          State_VGB(rhoUy_,i,j,k,iBlock) = RhoUy
-          State_VGB(rhoUz_,i,j,k,iBlock) = RhoUz
-
-          RhoInv=1/Rho
-
-          Ux=RhoUx*RhoInv
-          Uy=RhoUy*RhoInv
-          Uz=RhoUz*RhoInv
-
-          ! E = E_Boris - (UxB)^2/(2 c^2)
-          ECorr = (UyOld*BzOld-Uy*Bz+(UyOld-Uy)*B0z  &
-               -(UzOld*ByOld-Uz*By+(UzOld-Uz)*B0y))* &
-               (UyOld*fullBzOld+Uy*fullBz            &
-               -UzOld*fullByOld-Uz*fullBy)           &
-               +(UxOld*BzOld-Ux*Bz+(UxOld-Ux)*B0z    &
-               -(UzOld*BxOld-Uz*Bx+(UzOld-Uz)*B0x))* &
-               (UxOld*FullBzOld+Ux*FullBz            &
-               -UzOld*FullBxOld-Uz*FullBx)           &
-               +(UxOld*ByOld-Ux*By+(UxOld-Ux)*B0y    &
-               -(UyOld*BxOld-Uy*Bx+(UyOld-Uy)*B0x))* &
-               (UxOld*fullByOld+Ux*FullBy            &
-               -UyOld*fullBxOld-Uy*FullBx)
-
-          Energy_GBI(i,j,k,iBlock,1) = Energy_GBI(i,j,k,iBlock,1)  &
-               + cHalf*inv_c2LIGHT*ECorr
-
-       end do; end do; end do
-
-       if(DoTestMe)write(*,*) NameSub, ' after Boris update=', &
-            State_VGB(VarTest,iTest,jTest,kTest,iBlock), &
-            Energy_GBI(iTest,jTest,kTest,iBlock,:)
-
-    endif
 
     if(UseBorisSimple .and. IsMhd) then
        ! Update using simplified Boris correction, i.e. update
