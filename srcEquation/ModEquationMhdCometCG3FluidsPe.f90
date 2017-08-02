0dd1254177f2fcb48f089741f34523143a2e73dd
zghuang <>
2017-08-02 20:49:01 +0000

    First attempt to remove the total fluid. May need to verify the results.
    Former-commit-id: 7b49acc0ac15fc36c928bd8928cf8572363ad9e7

--- srcEquation/ModEquationMhdCometCG3FluidsPe.f90
+++ srcEquation/ModEquationMhdCometCG3FluidsPe.f90
@@ -17,17 +17,17 @@ module ModVarIndexes
   character (len=*), parameter :: NameEquation= &
        '2-fluid + Pe MHD + 1-Neutral for Comet CG'
 
-  integer, parameter :: nVar = 24
+  integer, parameter :: nVar = 19
 
-  integer, parameter :: nFluid    = 4
-  integer, parameter :: IonFirst_ = 2        ! First individual ion fluid
-  integer, parameter :: IonLast_  = 3        ! Last individual ion fluid
-  logical, parameter :: IsMhd     = .true.   ! First total ion fluid obeys MHD
-  real               :: MassFluid_I(2:nFluid) = (/1.0, 17.0, 17.0/)
+  integer, parameter :: nFluid    = 3
+  integer, parameter :: IonFirst_ = 1        ! First individual ion fluid
+  integer, parameter :: IonLast_  = 2        ! Last individual ion fluid
+  logical, parameter :: IsMhd     = .false.   ! First total ion fluid obeys MHD
+  real               :: MassFluid_I(1:nFluid) = (/1.0, 17.0, 17.0/)
 
   ! Fluids: total fluid, solar wind protons, cometary water ions
   character (len=4), parameter :: NameFluid_I(nFluid) = &
-       (/ 'All ', 'Sw  ', 'H2Op', 'Neu1' /)
+       (/ 'Sw  ', 'H2Op', 'Neu1' /)
 
   ! Named indexes for State_VGB and other variables
   ! These indexes should go subsequently, from 1 to nVar+nFluid.
@@ -42,37 +42,31 @@ module ModVarIndexes
        By_         =  6, &
        Bz_         =  7, &
        Pe_         =  8, &
-       p_          =  9, &
-       SwRho_      = 10, &
-       SwRhoUx_    = 11, &
-       SwRhoUy_    = 12, &
-       SwRhoUz_    = 13, &
-       SwP_        = 14, &
-       H2OpRho_    = 15, &
-       H2OpRhoUx_  = 16, &
-       H2OpRhoUy_  = 17, &
-       H2OpRhoUz_  = 18, &
-       H2OpP_      = 19, &
-       Neu1Rho_    = 20, &
-       Neu1RhoUx_  = 21, Neu1Ux_ = 21, &
-       Neu1RhoUy_  = 22, Neu1Uy_ = 22, &
-       Neu1RhoUz_  = 23, Neu1Uz_ = 23, &
-       Neu1P_      = 24, &
+       P_          =  9, &
+       H2OpRho_    = 10, &
+       H2OpRhoUx_  = 11, H2OpUx_ = 11, &
+       H2OpRhoUy_  = 12, H2OpUy_ = 12, &
+       H2OpRhoUz_  = 13, H2OpUz_ = 13, &
+       H2OpP_      = 14, &
+       Neu1Rho_    = 15, &
+       Neu1RhoUx_  = 16, Neu1Ux_ = 16, &
+       Neu1RhoUy_  = 17, Neu1Uy_ = 17, &
+       Neu1RhoUz_  = 18, Neu1Uz_ = 18, &
+       Neu1P_      = 19, &
        Energy_     = nVar+1, &
-       SwEnergy_   = nVar+2, &
-       H2OpEnergy_ = nVar+3, &
-       Neu1Energy_ = nVar+4
+       H2OpEnergy_ = nVar+2, &
+       Neu1Energy_ = nVar+3
 
   ! This allows to calculate RhoUx_ as RhoU_+x_ and so on.
   integer, parameter :: U_ = Ux_ - 1, RhoU_ = RhoUx_-1, B_ = Bx_-1
 
   ! These arrays are useful for multifluid
   integer, parameter :: &
-       iRho_I(nFluid)  =(/Rho_,   SwRho_,   H2OpRho_,   Neu1Rho_ /) ,&
-       iRhoUx_I(nFluid)=(/RhoUx_, SwRhoUx_, H2OpRhoUx_, Neu1RhoUx_ /),&
-       iRhoUy_I(nFluid)=(/RhoUy_, SwRhoUy_, H2OpRhoUy_, Neu1RhoUy_ /),&
-       iRhoUz_I(nFluid)=(/RhoUz_, SwRhoUz_, H2OpRhoUz_, Neu1RhoUz_ /),&
-       iP_I(nFluid)    =(/p_,     SwP_,     H2OpP_,     Neu1P_ /)
+       iRho_I(nFluid)  =(/Rho_,   H2OpRho_,   Neu1Rho_ /) ,&
+       iRhoUx_I(nFluid)=(/RhoUx_, H2OpRhoUx_, Neu1RhoUx_ /),&
+       iRhoUy_I(nFluid)=(/RhoUy_, H2OpRhoUy_, Neu1RhoUy_ /),&
+       iRhoUz_I(nFluid)=(/RhoUz_, H2OpRhoUz_, Neu1RhoUz_ /),&
+       iP_I(nFluid)    =(/P_,     H2OpP_,     Neu1P_ /)
 
   integer, parameter :: iPparIon_I(IonFirst_:IonLast_) = (/1,2/)
 
@@ -88,12 +82,7 @@ module ModVarIndexes
        0.0, & ! By_
        0.0, & ! Bz_
        1.0, & ! Pe_
-       1.0, & ! p_
-       1.0, & ! SwRho_
-       0.0, & ! SwRhoUx_
-       0.0, & ! SwRhoUy_
-       0.0, & ! SwRhoUz_
-       1.0, & ! SwP_
+       1.0, & ! P_
        1.0, & ! H2OpRho_
        0.0, & ! H2OpRhoUx_
        0.0, & ! H2OpRhoUy_
@@ -105,7 +94,6 @@ module ModVarIndexes
        0.0, & ! Neu1RhoUz_
        1.0, & ! Neu1P_
        1.0, & ! Energy_
-       1.0, & ! SwEnergy_
        1.0, & ! H2OpEnergy_
        1.0/)  ! Neu1Energy_
 
@@ -119,12 +107,7 @@ module ModVarIndexes
        'By     ', & ! By_
        'Bz     ', & ! Bz_
        'Pe     ', & ! Pe_
-       'p      ', & ! p_
-       'SwRho  ', & ! SwRho_
-       'SwMx   ', & ! SwRhoUx_
-       'SwMy   ', & ! SwRhoUy_
-       'SwMz   ', & ! SwRhoUz_
-       'SwP    ', & ! SwP_
+       'P      ', & ! p_
        'H2OpRho', & ! H2OpRho_
        'H2OpMx ', & ! H2OpRhoUx_
        'H2OpMy ', & ! H2OpRhoUy_
@@ -136,29 +119,24 @@ module ModVarIndexes
        'Neu1Mz ', & ! Neu1RhoUz_
        'Neu1P  ', & ! Neu1P_
        'E      ', & ! Energy_
-       'SwE    ', & ! SwEnergy_
        'H2OpE  ', & ! H2OpEnergy_
        'Neu1E  '/)  ! Neu1Energy_
-
 
   ! The space separated list of nVar conservative variables for plotting
   character(len=*), parameter :: NameConservativeVar = &
        'Rho Mx My Mz Bx By Bz E '// &
-       'SwRho SwMx SwMy SwMz SwE '// &
        'H2OpRho H2OpMx H2OpMy H2OpMz H2OpE ' // &
        'Neu1Rho Neu1Mx Neu1My Neu1Mz Neu1E '
 
   ! The space separated list of nVar primitive variables for plotting
   character(len=*), parameter :: NamePrimitiveVar = &
        'Rho Ux Uy Uz Bx By Bz Pe P ' // &
-       'SwRho SwUx SwUy SwUz SwP '// &
        'H2OpRho H2OpUx H2OpUy H2OpUz H2OpP ' // &
        'Neu1Rho Neu1Ux Neu1Uy Neu1Uz Neu1P '
 
   ! The space separated list of nVar primitive variables for TECplot output
   character(len=*), parameter :: NamePrimitiveVarTec = &
        '"`r", "U_x", "U_y", "U_z", "B_x", "B_y", "B_z", "p_e", "p",' // &
-       '"`r^SW^+", "U_x^SW^+", "U_y^SW^+", "U_z^SW^+", "P^SW^+",'// &
        '"`r^H2O^+", "U_x^H2O^+", "U_y^H2O^+", "U_z^H2O^+", "P^H2O^+"' // &
        '"`r^Neu1", "U_x^Neu1", "U_y^Neu1", "U_z^Neu1", "p^Neu1"'
 
