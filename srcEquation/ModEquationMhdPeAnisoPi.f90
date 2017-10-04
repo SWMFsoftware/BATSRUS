74834f9958213dd05abe7c313cdaae41588c0aa1
zghuang <>
2017-10-04 20:10:52 +0000

    Removed pperp (and ppar, p) in NameConservativeVar, NamePrimitiveVar and NamePrimitiveVarTec.
    Former-commit-id: d8e00486ed092a0af8c505111005cb643e2422ca

--- srcEquation/ModEquationMhdPeAnisoPi.f90
+++ srcEquation/ModEquationMhdPeAnisoPi.f90
@@ -82,16 +82,16 @@ module ModVarIndexes
 
   ! The space separated list of nVar conservative variables for plotting
   character(len=*), parameter :: NameConservativeVar = &
-       'rho mx my mz bx by bz ppar p e'
+       'rho mx my mz bx by bz e'
 
   ! The space separated list of nVar primitive variables for plotting
   character(len=*), parameter :: NamePrimitiveVar = &
-       'rho ux uy uz bx by bz pe ppar p pperp'
+       'rho ux uy uz bx by bz pe ppar p'
 
   ! The space separated list of nVar primitive variables for TECplot output
   character(len=*), parameter :: NamePrimitiveVarTec = &
        '"`r", "U_x", "U_y", "U_z", "B_x", "B_y", "B_z"' // &
-       '"Pe", "p_par", "p", "p_perp"'
+       '"Pe", "p_par", "p"'
 
   ! Names of the user units for IDL and TECPlot output
   character(len=20) :: &
