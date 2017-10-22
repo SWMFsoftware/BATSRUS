bf5ab221da4b4ba599d53defed13384f6e9a8227
gtoth <>
2017-10-22 21:19:12 +0000

    Part of rotation.f90 merged into this routine
    Former-commit-id: ba2169aa69b43cd61a2a322b6ffd84993538f008

--- src/ModSetInitialCondition.f90
+++ src/ModSetInitialCondition.f90
@@ -163,3 +163,53 @@ subroutine set_ics(iBlock)
        NameSub, 'State(test)=',State_VGB(:,iTest,jTest,kTest,BlkTest)
 
 end subroutine set_ics
+
+!==========================================================================
+subroutine add_rotational_velocity(iSign, iBlock)
+
+  use ModSize,     ONLY: MinI, MaxI, MinJ, MaxJ, MinK, MaxK, nBlock, x_, y_
+  use ModMain,     ONLY: Unused_B
+  use ModAdvance,  ONLY: State_VGB
+  use ModGeometry, ONLY: true_cell
+  use ModPhysics,  ONLY: OmegaBody
+  use ModMultiFluid, ONLY: iRho_I, iRhoUx_I, iRhoUy_I
+  use BATL_lib,    ONLY: Xyz_DGB
+
+  implicit none
+
+  integer, intent(in):: iSign
+  integer, intent(in):: iBlock
+
+  ! Transform velocities between inertial and rotating frames
+  ! where Omega is the angular velocity of the rotating frame
+  ! Since Omega = (0,0,OmegaBody)
+  ! ux = ux - iSign*OmegaBody*y
+  ! uy = uy + iSign*OmegaBody*x
+  ! iSign=+1: from rotating to inertial frame
+  ! iSign=-1: from inertial to rotating frame
+  !
+  ! If iBlock is 0 or negative, do all blocks in State_VGB,
+  ! otherwise do only block iBlock
+
+  integer :: i, j, k, iBlk, iBlockFirst, iBlockLast
+  !---------------------------------------------------------------------------      
+  if(iBlock > 0)then
+     iBlockFirst = 1; iBlockLast = nBlock
+  else
+     iBlockFirst = iBlock; iBlockLast = iBlock
+  end if
+
+  do iBlk = iBlockFirst, iBlockLast
+     if(Unused_B(iBlk))CYCLE
+     do k=MinK,MaxK; do j=MinJ,MaxJ; do i=MinI,MaxI
+        if(.not.true_Cell(i,j,k,iBlk)) CYCLE
+        State_VGB(iRhoUx_I,i,j,k,iBlk) = State_VGB(iRhoUx_I,i,j,k,iBlk) - &
+             iSign*State_VGB(iRho_I,i,j,k,iBlk)*OmegaBody*Xyz_DGB(y_,i,j,k,iBlk)
+        
+        State_VGB(iRhoUy_I,i,j,k,iBlk) = State_VGB(iRhoUy_I,i,j,k,iBlk) + &
+             iSign*State_VGB(iRho_I,i,j,k,iBlk)*OmegaBody*Xyz_DGB(x_,i,j,k,iBlk)
+
+     end do; end do; end do
+  end do
+  
+end subroutine add_rotational_velocity
