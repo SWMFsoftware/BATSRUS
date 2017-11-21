883eaafed06abb02658e01a3e39b723ef2e14e33
gtoth <>
2017-11-21 22:03:10 +0000

    Now iProc, nProc, iComm are taken from BATL_lib so they are always the same.
    Former-commit-id: 4e632dd5baf445eb2bf8d13b09c34f8b86582de9

--- src/ModProcMH.f90
+++ src/ModProcMH.f90
@@ -2,13 +2,8 @@
 !  portions used with permission
 !  For more information, see http://csem.engin.umich.edu/tools/swmf
 module ModProcMH
-  !
-  ! MPI control variables for MH
-  !
-  implicit none
 
-  ! My processor number and number of processors
-  integer :: iProc = -1, nProc = -1, iComm = -1
+  use BATL_lib, ONLY: iProc, nProc, iComm
 
 end module ModProcMH
 !==============================================================================
