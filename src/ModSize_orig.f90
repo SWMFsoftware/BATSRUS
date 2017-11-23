132f72b58730daa39263a597ebf4af5dc1a45fbd
gtoth <>
2017-11-23 19:24:46 +0000

    Now MaxBlock is taken from BATL_size (no renaming) and it can be changed.
    Former-commit-id: 8f352e696a42dd7c0b2fc4b822de4a006dad53f4

--- src/ModSize_orig.f90
+++ src/ModSize_orig.f90
@@ -1,17 +1,14 @@
 module ModSize
 
-  use BATL_size, MaxBlockBatl => MaxBlock
+  use BATL_size
 
   ! Named indexes for coordinate directions
   use BATL_geometry, ONLY: x_, y_, z_, r_, Phi_, Theta_, Lon_, Lat_
 
   implicit none
 
-  ! Maximum number of blocks per processor (set by Config.pl)
-  integer, parameter :: MaxBlock = 400
-
   ! Maximum number of implicit blocks (set by Config.pl)
-  integer, parameter :: MaxImplBLK = min(MaxBlock, 100)
+  integer, parameter :: MaxImplBLK = 100
 
 end module ModSize
 !==============================================================================
