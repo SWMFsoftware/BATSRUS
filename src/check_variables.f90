d706549cdf2cf71942713f97aaad10d673f9669c
Toth <gtoth@mesh.engin.umich.edu>
2018-10-18 16:28:55 -0400

    Empty version so that it can be compiled
    Former-commit-id: 247599f066772554be568fe54b892994b2714a8c

---
+++ src/check_variables.f90
@@ -0,0 +1,8 @@
+
+subroutine init_check_variables
+
+end subroutine init_check_variables
+!===========================================================================
+subroutine do_check_variables
+
+end subroutine do_check_variables
