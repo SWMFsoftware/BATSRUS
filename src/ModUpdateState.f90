7be6332ef9b13959d8dc5998e488b603744d4eea
hyzhou <hyzhou@umich.edu>
2019-02-26 19:37:22 -0500

    Fixed the OpenMP issue with fix_update. Pass private variables as arguments to avoid threadprivate declaration.
    Former-commit-id: ca7bf062157d41c99bede8ef79ba80600737acd6 [formerly 947b61ecbdcb62a4129c5c453da531803781dfbf]
    Former-commit-id: 68ddcac30cb66a027ce9347e8bda0f919982acc7

--- src/ModUpdateState.f90
+++ src/ModUpdateState.f90
@@ -831,7 +831,7 @@ contains
              call MPI_allreduce(percent_chg_p, pChangeMax_I, 2, &
                   MPI_REAL, MPI_MAX, iComm, iError)
 
-             !$omp parallel do private(time_fraction) &
+             !$omp parallel do &
              !$omp reduction(max:percent_chg_rho) reduction(max:percent_chg_p)
              do iBlock = 1, nBlockMax
                 if(Unused_B(iBlock)) CYCLE
@@ -948,11 +948,11 @@ contains
        !///
        report_tf = 1.
        PercentChangePE = 0.
-       !!$omp parallel do private(i,j,k,num_checks,iVar,update_check_done) &
-       !!$omp private(time_fraction_rho,time_fraction_p,cell_time_fraction) &
-       !!$omp private(time_fraction) &
-       !!$omp reduction(max:percent_chg_rho) reduction(max:percent_chg_p) &
-       !!$omp reduction(min:report_tf)
+       !$omp parallel do private(i,j,k,num_checks,iVar,update_check_done) &
+       !$omp private(time_fraction_rho,time_fraction_p,cell_time_fraction) &
+       !$omp private(time_fraction, percent_chg_rho, percent_chg_p) &
+       !$omp reduction(max:PercentChangePE) &
+       !$omp reduction(min:report_tf)
        do iBlock = 1, nBlockMax
           if(Unused_B(iBlock)) CYCLE
           do k=1,nK; do j=1,nJ; do i=1,nI
@@ -1024,7 +1024,7 @@ contains
                            NameVar_V(1),'=',State_VGB(1,i,j,k,iBlock), &
                            '   ', NameVar_V(nVar),State_VGB(nVar,i,j,k,iBlock)
                    end if
-                   call fix_update
+                   call fix_update(i,j,k,iblock,time_fraction)
                    if(DoTest2) then
                       write(*,*) &
                            iProc,' ',iBlock,' ',i,' ',j,' ',k,' NEW: ', &
@@ -1041,7 +1041,7 @@ contains
              report_tf = min(report_tf, cell_time_fraction)
           end do; end do; end do
        end do
-       !!$omp end parallel do
+       !$omp end parallel do
 
        call MPI_allreduce(report_tf, report_tf_all, 1, &
             MPI_REAL, MPI_MIN, iComm, iError)
@@ -1064,7 +1064,7 @@ contains
        end if
 
        if(DoTest3)then
-          !!$omp parallel do private(i,j,k)
+          !$omp parallel do private(i,j,k)
           do iBlock = 1,nBlockMax
              if(Unused_B(iBlock))CYCLE
              do k=1,nK; do j=1,nJ; do i=1,nI
@@ -1112,7 +1112,7 @@ contains
                      'value_new=',State_VGB(p_,i,j,k,iBlock)
              end do; end do; end do
           end do
-          !!$omp end parallel do
+          !$omp end parallel do
        end if
     end if
 
@@ -1122,7 +1122,7 @@ contains
 
     ! Check for positivity of variables
     IsNegative = .false.
-    !!$omp parallel do private(Value,i_D,IsNegative,iVar,i,j,k)
+    !$omp parallel do private(Value,i_D,IsNegative,iVar,i,j,k)
     do iBlock = 1, nBlockMax
        if(Unused_B(iBlock)) CYCLE
        do iVar = 1, nVar
@@ -1150,7 +1150,7 @@ contains
           end if
        end do
     end do
-    !!$omp end parallel do
+    !$omp end parallel do
     if(IsNegative)then
        if(time_accurate)then
           write(*,'(a,i4,a,a,i6,a,f12.8,a,f12.8)') &
@@ -1172,7 +1172,10 @@ contains
   contains
     !==========================================================================
 
-    subroutine fix_update
+    subroutine fix_update(i,j,k,iBlock,time_fraction)
+
+      integer, intent(in), optional:: i,j,k,iBlock
+      real, intent(in), optional:: time_fraction
 
       logical :: IsConserv
       real :: fullBx, fullBy, fullBz, fullBB, UdotBc2, rhoc2, gA2_Boris
@@ -1279,7 +1282,7 @@ contains
          end if
 
          ! Interpolate
-         ! For possible extension to multifluid
+         ! For possible extension to multifluid:
          ! State_VGB(iRho_I,i,j,k,iBlock) = &
          !     (   time_fraction) *   State_VGB(iRho_I,i,j,k,iBlock) + &
          !     (1.0-time_fraction) * StateOld_VGB(iRho_I,i,j,k,iBlock)
@@ -1376,12 +1379,12 @@ contains
          end if
       else ! Non-Boris interpolation
 
-         State_VGB(1:nVar,i,j,k,iBlock) = &
-              (   time_fraction) *   State_VGB(1:nVar,i,j,k,iBlock) + &
-              (1.0-time_fraction) * StateOld_VGB(1:nVar,i,j,k,iBlock)
+         State_VGB(:,i,j,k,iBlock) = &
+              (    time_fraction) *    State_VGB(:,i,j,k,iBlock) + &
+              (1.0-time_fraction) * StateOld_VGB(:,i,j,k,iBlock)
          if(IsConserv)then
             Energy_GBI(i,j,k,iBlock,:) = &
-                 (   time_fraction) *   Energy_GBI(i,j,k,iBlock,:) + &
+                 (    time_fraction) *    Energy_GBI(i,j,k,iBlock,:) + &
                  (1.0-time_fraction) * EnergyOld_CBI(i,j,k,iBlock,:)
 
             if(IsMhd .and. (nStage==1.and..not.time_accurate).or.&
@@ -1398,6 +1401,7 @@ contains
             call calc_energy_point(i,j,k,iBlock)
          end if
       end if
+
       time_BLK(i,j,k,iBlock) = time_BLK(i,j,k,iBlock)*time_fraction
 
       if(DoTestCell)write(*,*)NameSub,' final state=',State_VGB(:,i,j,k,iBlock)
