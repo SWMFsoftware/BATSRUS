287b87ee0c94549f3c09a32852fd82a80af69d0d
igorsok <igorsok@umich.edu>
2019-03-12 18:37:45 -0400

    Some development
    Former-commit-id: 1dafc0be919830bce9d286a7c60f5b6143907ef3 [formerly 0e498193c952e064ba212b4e0de307a7c2a41796]
    Former-commit-id: f5dbab2e3612c7271111a94f7817e7ccd04c7529

--- src/ModParticleMover.f90
+++ src/ModParticleMover.f90
@@ -14,7 +14,33 @@ module ModParticleMover
   use ModMain,         ONLY: n_step
   use ModGeometry,     ONLY: far_field_bcs_blk, true_cell
   use ModCellBoundary, ONLY: set_cell_boundary
+  use ModParticles,    ONLY: allocate_particles, message_pass_particles
+  use BATL_particles,  ONLY: Particle_I, remove_undefined_particles, &
+       mark_undefined, check_particle_location, put_particles, trace_particles
+  !\
+  !Parameters
+  !/
+  ! number of particles with different charge-to-mass ratios
+  integer :: nKindChargedParticles = 0 
+  !\
+  ! The order number of this kind of particle in the whole 
+  ! BATL_particle repository
+  !/
+  integer, allocatable :: iKindParticle_I(:)
+  !\
+  !Charge to mass ratio, same for all particles of a given 
+  !kind
+  !/
+  real,    allocatable :: Charge2Mass_I(:)
+  !\
+  ! Named indexes
+  !/
+  integer, parameter :: nVar = 2*nDim +1, x_ = 1, y_ = 2, z_=nDim,&
+       Ux_ = nDim + x_, Uy_= nDim + y_, Uz_ = 2*nDim, Mass = Uz_ +1
 contains
+  subroutine allocate_charged_particles
+  end subroutine allocate_charged_particles
+  !==================
   subroutine trace_charged_particles(iSort)
     integer, intent(in) :: iSort
     !=======================PARTICLE MOVER=========================!
