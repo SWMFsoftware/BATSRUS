0ca5b65bcb62059df82dc81e38f1d8350bc148cb
igorsok <igorsok@umich.edu>
2019-06-29 02:09:57 -0400

    modified:   src/ModParticleMover.f90 Disabled particle generation based on the state vector for all cells.
    Former-commit-id: 41267a3c852e03404ae19236687b64d8e25d5fc6 [formerly a3d2090fa9f8317f9e095c96350a8d122179cec2]
    Former-commit-id: 3c4b724f4431ac8718e191dd8e31070a28972d6d

--- src/ModParticleMover.f90
+++ src/ModParticleMover.f90
@@ -387,23 +387,23 @@ contains
        !/
        QDtPerM = cHalf*Dt*Charge2Mass_I(iLoop)
        Index_II(Status_,1:nParticle) = DoAll_
-       if(iLoop.le.nHybridParticleSort)then
-         ! Loop over each Block
-         do iBlock = 1, nBlock
-            ! Loop over each Cell in each BLock
-            do k=1,nK; do j=1,nJ; do i=1,nI
-               ! Skip not tru cells
-               if(.not.true_cell(i,j,k,iBlock)) CYCLE
-               !\
-               ! For each Hybrid Sort
-               ! each Block and each Cell
-               ! Calculate the kinetic counterpart of the MHD solution
-               ! i.e. the particle positions and velocities
-               !/
-               call get_vdf_from_state(i,j,k,iBlock,iKind)
-            end do; end do; end do
-         end do
-       end if
+       !if(iLoop.le.nHybridParticleSort)then
+       !  ! Loop over each Block
+       !  do iBlock = 1, nBlock
+       !     ! Loop over each Cell in each BLock
+       !     do k=1,nK; do j=1,nJ; do i=1,nI
+       !        ! Skip not tru cells
+       !        if(.not.true_cell(i,j,k,iBlock)) CYCLE
+       !        !\
+       !        ! For each Hybrid Sort
+       !        ! each Block and each Cell
+       !        ! Calculate the kinetic counterpart of the MHD solution
+       !        ! i.e. the particle positions and velocities
+       !        !/
+       !        call get_vdf_from_state(i,j,k,iBlock,iKind)
+       !     end do; end do; end do
+       !  end do
+       !end if
        call batl_trace_particles(iKind, boris_scheme, check_done)
        !\
        ! For particles near the block boundary, contributions are
