b1d246115062b3f73d04edfec22f20cc890c5076
Igor V. Sokolov <igorsok@umich.edu>
2020-05-11 17:44:41 -0400

    modified:   src/ModFieldLineThread.f90 	modified:   src/ModThreadedLC.f90 	modified:   src/ModWritePlotLos.f90 Fix typos
    Former-commit-id: 6516ab69ee0fc56516041aa774d8ba7195567269

--- src/ModFieldLineThread.f90
+++ src/ModFieldLineThread.f90
@@ -2,7 +2,7 @@
 !  portions used with permission
 !  For more information, see http://csem.engin.umich.edu/tools/swmf
 module ModFieldLineThread
-! use ModUtilties, ONLY: norm2
+!  use ModUtilities, ONLY: norm2
   use BATL_lib,      ONLY: &
        test_start, test_stop, jTest, kTest, iBlockTest, &
        iProc, nProc, iComm, nJ, nK, jDim_, kDim_
