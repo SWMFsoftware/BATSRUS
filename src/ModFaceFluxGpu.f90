4eb67741eaa681114685a5aaeae8f63bd1988dbc
Toth <gtoth@mesh.engin.umich.edu>
2021-03-20 18:05:16 -0400

    Generalized to 3D. Seems to work OK (but not exact) for test_fast_wave

--- src/ModFaceFluxGpu.f90
+++ src/ModFaceFluxGpu.f90
@@ -5,10 +5,12 @@ module ModFaceFluxGpu
 
   use ModVarIndexes
   use ModAdvance, ONLY: nGang, nFlux, &
-       LeftState_VXI, RightState_VXI,Flux_VXI, VdtFace_XI, uDotArea_XII
+       LeftState_VXI, RightState_VXI, Flux_VXI, VdtFace_XI, uDotArea_XII, &
+       LeftState_VYI, RightState_VYI, Flux_VYI, VdtFace_YI, uDotArea_YII, &
+       LeftState_VZI, RightState_VZI, Flux_VZI, VdtFace_ZI, uDotArea_ZII
   use ModPhysics, ONLY: Gamma, InvGammaMinus1
   use ModMain, ONLY: SpeedHyp
-  use BATL_lib, ONLY: nI, nJ, nK, x_, CellFace_DB  
+  use BATL_lib, ONLY: nDim, nI, nJ, nK, x_, y_, z_, CellFace_DB
   implicit none
   
 contains
@@ -19,7 +21,7 @@ contains
     logical, intent(in) :: DoResChangeOnly
     integer, intent(in) :: iBlock
 
-    real :: Area, Cmax
+    real :: Area, NormalX, NormalY, NormalZ, Un, Cmax
     real :: StateLeft_V(nVar), StateRight_V(nVar), State_V(nVar)
     real :: StateLeftCons_V(nFlux), StateRightCons_V(nFlux)
     real :: FluxLeft_V(nFlux), FluxRight_V(nFlux)
@@ -31,22 +33,31 @@ contains
     !--------------------------------------------------------------------------
     if(DoResChangeOnly) RETURN
 
-    iGang = max(iBlock, nGang)
+    iGang = min(iBlock, nGang)
     
     !$acc loop vector collapse(3) &
-    !$acc private(Area, Cmax, StateLeft_V, StateRight_V, State_V, &
+    !$acc private(Area, NormalX, NormalY, NormalZ, Un, Cmax, &
+    !$acc         StateLeft_V, StateRight_V, State_V, &
     !$acc         StateLeftCons_V, StateRightCons_V, FluxLeft_V, FluxRight_V)
     do kFace = 1, nK; do jFace = 1, nJ; do iFace = 1, nI+1
+       !if(IsCartesianGrid)then
        Area = CellFace_DB(x_,iBlock)
+       NormalX = 1.0
+       NormalY = 0.0
+       NormalZ = 0.0
+       !end if
 
        ! Primitive variables
        StateLeft_V  = LeftState_VXI(:,iFace,jFace,kFace,iGang)
        StateRight_V = RightState_VXI(:,iFace,jFace,kFace,iGang)
        State_V      = 0.5*(StateLeft_V + StateRight_V)  ! average state
 
-       call get_speed_max(State_V, Cmax)
-       call get_physical_flux(StateLeft_V, StateLeftCons_V, FluxLeft_V)
-       call get_physical_flux(StateRight_V, StateRightCons_V, FluxRight_V)
+       call get_speed_max(State_V, NormalX, NormalY, NormalZ, &
+            Un, Cmax)
+       call get_physical_flux(StateLeft_V, NormalX, NormalY, NormalZ, &
+            StateLeftCons_V, FluxLeft_V)
+       call get_physical_flux(StateRight_V, NormalX, NormalY, NormalZ, &
+            StateRightCons_V, FluxRight_V)
 
        ! Rusanov flux
        Flux_VXI(:,iFace,jFace,kFace,iGang) = &
@@ -55,23 +66,104 @@ contains
 
        ! For time step
        VdtFace_XI(iFace,jFace,kFace,iGang) = Cmax*Area
+
        ! For div U source term
-       uDotArea_XII(iFace,jFace,kFace,:,iGang) = State_V(Ux_)*Area
+       uDotArea_XII(iFace,jFace,kFace,:,iGang) = Un*Area
 
     end do; end do; end do
 
+    if(nDim == 1) RETURN
+    
+    !$acc loop vector collapse(3) &
+    !$acc private(Area, NormalX, NormalY, NormalZ, Un, Cmax, &
+    !$acc         StateLeft_V, StateRight_V, State_V, &
+    !$acc         StateLeftCons_V, StateRightCons_V, FluxLeft_V, FluxRight_V)
+    do kFace = 1, nK; do jFace = 1, nJ+1; do iFace = 1, nI
+       !if(IsCartesianGrid)then
+       Area = CellFace_DB(y_,iBlock)
+       NormalX = 0.0
+       NormalY = 1.0
+       NormalZ = 0.0
+       !end if
+
+       ! Primitive variables
+       StateLeft_V  = LeftState_VYI(:,iFace,jFace,kFace,iGang)
+       StateRight_V = RightState_VYI(:,iFace,jFace,kFace,iGang)
+       State_V      = 0.5*(StateLeft_V + StateRight_V)  ! average state
+
+       call get_speed_max(State_V, NormalX, NormalY, NormalZ, &
+            Un, Cmax)
+       call get_physical_flux(StateLeft_V, NormalX, NormalY, NormalZ, &
+            StateLeftCons_V, FluxLeft_V)
+       call get_physical_flux(StateRight_V, NormalX, NormalY, NormalZ, &
+            StateRightCons_V, FluxRight_V)
+
+       ! Rusanov flux
+       Flux_VYI(:,iFace,jFace,kFace,iGang) = &
+            Area*(0.5*(FluxLeft_V + FluxRight_V) &
+            + 0.5*Cmax*(StateLeftCons_V - StateRightCons_V))
+
+       ! For time step
+       VdtFace_YI(iFace,jFace,kFace,iGang) = Cmax*Area
+
+       ! For div U source term
+       uDotArea_YII(iFace,jFace,kFace,:,iGang) = Un*Area
+
+    end do; end do; end do
+
+    if(nDim == 2) RETURN
+
+    !$acc loop vector collapse(3) &
+    !$acc private(Area, NormalX, NormalY, NormalZ, Un, Cmax, &
+    !$acc         StateLeft_V, StateRight_V, State_V, &
+    !$acc         StateLeftCons_V, StateRightCons_V, FluxLeft_V, FluxRight_V)
+    do kFace = 1, nK+1; do jFace = 1, nJ; do iFace = 1, nI
+       !if(IsCartesianGrid)then
+       Area = CellFace_DB(z_,iBlock)
+       NormalX = 0.0
+       NormalY = 0.0
+       NormalZ = 1.0
+       !end if
+
+       ! Primitive variables
+       StateLeft_V  = LeftState_VZI(:,iFace,jFace,kFace,iGang)
+       StateRight_V = RightState_VZI(:,iFace,jFace,kFace,iGang)
+       ! average state
+       State_V      = 0.5*(StateLeft_V + StateRight_V)
+
+       call get_speed_max(State_V, NormalX, NormalY, NormalZ, &
+            Un, Cmax)
+       call get_physical_flux(StateLeft_V, NormalX, NormalY, NormalZ, &
+            StateLeftCons_V, FluxLeft_V)
+       call get_physical_flux(StateRight_V, NormalX, NormalY, NormalZ, &
+            StateRightCons_V, FluxRight_V)
+
+       ! Rusanov flux
+       Flux_VZI(:,iFace,jFace,kFace,iGang) = &
+            Area*(0.5*(FluxLeft_V + FluxRight_V) &
+            + 0.5*Cmax*(StateLeftCons_V - StateRightCons_V))
+
+       ! For time step
+       VdtFace_ZI(iFace,jFace,kFace,iGang) = Cmax*Area
+
+       ! For div U source term
+       uDotArea_ZII(iFace,jFace,kFace,:,iGang) = Un*Area
+
+    end do; end do; end do
+    
   contains
     !==========================================================================
-    subroutine get_physical_flux(State_V, StateCons_V, Flux_V)
+    subroutine get_physical_flux(State_V, NormalX, NormalY, NormalZ, &
+         StateCons_V, Flux_V)
       !$acc routine seq
 
       real, intent(in) :: State_V(nVar)      ! primitive state vector
+      real, intent(in) :: NormalX, NormalY, NormalZ ! face normal
       real, intent(out):: StateCons_V(nFlux) ! conservative state vector
       real, intent(out):: Flux_V(nFlux)      ! conservative flux
 
-      real:: Rho, Ux, Uy, Uz, Bx, By, Bz, Hyp, p, e, Un, Bn, B2
+      real:: Rho, Ux, Uy, Uz, Bx, By, Bz, Hyp, p, e, Un, Bn, pB, pTot
       !------------------------------------------------------------------------
-
       Rho = State_V(Rho_)
       Ux  = State_V(Ux_)
       Uy  = State_V(Uy_)
@@ -83,9 +175,9 @@ contains
       p   = State_V(p_)
 
       ! Convenient variables
-      Un  = Ux
-      Bn  = Bx
-      B2  = Bx**2 + By**2 + Bz**2
+      Un  = Ux*NormalX + Uy*NormalY + Uz*NormalZ
+      Bn  = Bx*NormalX + By*NormalY + Bz*NormalZ
+      pB  = 0.5*(Bx**2 + By**2 + Bz**2)
 
       ! Hydro energy density
       e   = InvGammaMinus1*p + 0.5*Rho*(Ux**2 + Uy**2 + Uz**2)
@@ -100,18 +192,18 @@ contains
       StateCons_V(Bz_)     = Bz
       if(Hyp_ > 1) StateCons_V(Hyp_) = Hyp
       StateCons_V(p_)      = p
-      ! Add magnetic energy
-      StateCons_V(Energy_) = e + 0.5*B2
+      StateCons_V(Energy_) = e + pB  ! Add magnetic energy density
 
       ! Physical flux
-      Flux_V(Rho_)    =  Rho*Un
-      Flux_V(RhoUx_)  = Un*Rho*Ux + p - Bn*Bx + 0.5*B2
-      Flux_V(RhoUy_)  = Un*Rho*Uy     - Bn*By
-      Flux_V(RhoUz_)  = Un*Rho*Uz     - Bn*Bz
+      pTot = p + pB
+      Flux_V(Rho_)    = Rho*Un
+      Flux_V(RhoUx_)  = Un*Rho*Ux - Bn*Bx + NormalX*pTot
+      Flux_V(RhoUy_)  = Un*Rho*Uy - Bn*By + NormalY*pTot
+      Flux_V(RhoUz_)  = Un*Rho*Uz - Bn*Bz + NormalZ*pTot
       if(Hyp_ > 1)then
-         Flux_V(Bx_)  =  Un*Bx - Ux*Bn + SpeedHyp*Hyp
-         Flux_V(By_)  =  Un*By - Uy*Bn
-         Flux_V(Bz_)  =  Un*Bz - Uz*Bn
+         Flux_V(Bx_)  =  Un*Bx - Ux*Bn + NormalX*SpeedHyp*Hyp
+         Flux_V(By_)  =  Un*By - Uy*Bn + NormalY*SpeedHyp*Hyp
+         Flux_V(Bz_)  =  Un*Bz - Uz*Bn + NormalZ*SpeedHyp*Hyp
          Flux_V(Hyp_) =  SpeedHyp*Bn
       else
          Flux_V(Bx_)  =  Un*Bx - Ux*Bn
@@ -124,12 +216,12 @@ contains
 
     end subroutine get_physical_flux
     !==========================================================================
-    subroutine get_speed_max(State_V, Cmax)
+    subroutine get_speed_max(State_V, NormalX, NormalY, NormalZ, Un, Cmax)
       !$acc seq
-      real, intent(in):: State_V(nVar)
-      real, intent(out):: Cmax
+      real, intent(in):: State_V(nVar), NormalX, NormalY, NormalZ
+      real, intent(out):: Un, Cmax
 
-      real:: InvRho, p, Un, Bx, By, Bz, Bn, B2
+      real:: InvRho, p, Bx, By, Bz, Bn, B2
       real:: Sound2, Alfven2, Alfven2Normal, Fast2, Discr, Fast
       !------------------------------------------------------------------------
 
@@ -138,8 +230,7 @@ contains
       By  = State_V(By_)
       Bz  = State_V(Bz_)
       p   = State_V(p_)
-      Un  = State_V(Ux_)
-      Bn  = Bx
+      Bn  = State_V(Bx_)*NormalX + State_V(By_)*NormalY + State_V(Bz_)*NormalZ
       B2  = Bx**2 + By**2 + Bz**2
 
       Sound2        = InvRho*p*Gamma
@@ -149,9 +240,11 @@ contains
       Discr = sqrt(max(0.0, Fast2**2 - 4*Sound2*Alfven2Normal))
       Fast  = sqrt( 0.5*(Fast2 + Discr) )
       
+      Un   = State_V(Ux_)*NormalX + State_V(Uy_)*NormalY + State_V(Uz_)*NormalZ
       Cmax = Un + Fast
       
     end subroutine get_speed_max
+
   end subroutine calc_face_flux_gpu
   !============================================================================
 end module ModFaceFluxGpu
