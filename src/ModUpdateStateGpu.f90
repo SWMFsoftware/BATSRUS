baf6a8a309c29d97cc6a7e2a429ce655e8e4b65c
Gabor Toth <gtoth@umich.edu>
2021-03-24 20:50:27 -0400

    Single cell loop

--- src/ModUpdateStateGpu.f90
+++ src/ModUpdateStateGpu.f90
@@ -4,14 +4,15 @@
 
 module ModUpdateStateGpu
 
-  use ModMain, ONLY: iStage, Cfl
-  use ModAdvance
-  use BATL_lib, ONLY: nI, nJ, nK, CellVolume_GB
-  use ModEnergy, ONLY: calc_energy_or_pressure
-  use ModMultiFluid, ONLY: nFluid, IonLast_, &
-       iRho, iRhoUx, iRhoUy, iRhoUz, iP, select_fluid
-  use ModPhysics,    ONLY: GammaMinus1_I
-  
+  use ModVarIndexes
+  use ModFaceFlux, ONLY: print_face_values
+  use ModMain, ONLY: iStage, Cfl, Dt
+  use ModAdvance, ONLY: nFlux, State_VGB, Energy_GBI
+!!!  time_BLK, StateOld_VGB, State_VGB, EnergyOld_CBI, Energy_GBI
+  use BATL_lib, ONLY: nDim, nI, nJ, nK, x_, y_, z_, CellVolume_GB, CellFace_DB
+  use ModPhysics, ONLY: Gamma, InvGammaMinus1, GammaMinus1_I
+  use ModMain, ONLY: SpeedHyp
+
   implicit none
 
 contains
@@ -20,54 +21,257 @@ contains
     !$acc routine vector
     integer, intent(in):: iBlock
 
-    integer:: iVar, iFluid, i, j, k, iGang
-#ifdef OPENACC
+    integer:: i, j, k
     !--------------------------------------------------------------------------
-      iGang = iBlock
-#else
-      iGang = 1
-#endif
 
-      !$acc loop vector collapse(3)
-      do k = 1,nK; do j = 1,nJ; do i = 1,nI
-         StateOld_VGB(:,i,j,k,iBlock) = State_VGB(:,i,j,k,iBlock)
-         EnergyOld_CBI(i,j,k,iBlock,1) = Energy_GBI(i,j,k,iBlock,1)
+    !$acc loop vector collapse(3)
+    do k = 1, nK; do j = 1, nJ; do i = 1, nI
+       ! copy to original state (only needed for multistage scheme really)
+!!!       StateOld_VGB(:,i,j,k,iBlock) = State_VGB(:,i,j,k,iBlock)
+!!!       EnergyOld_CBI(i,j,k,iBlock,1) = Energy_GBI(i,j,k,iBlock,1)
 
-         State_VGB(:,i,j,k,iBlock) = StateOld_VGB(:,i,j,k,iBlock) & 
-              + Cfl*time_BLK(i,j,k,iBlock)* &
-              (Source_VCI(1:nVar,i,j,k,iGang) + &
-              ( Flux_VXI(1:nVar,i,j,k,iGang) - Flux_VXI(1:nVar,i+1,j,k,iGang)  &
-              + Flux_VYI(1:nVar,i,j,k,iGang) - Flux_VYI(1:nVar,i,j+1,k,iGang)  &
-              + Flux_VZI(1:nVar,i,j,k,iGang) - Flux_VZI(1:nVar,i,j,k+1,iGang)  ) &
-              /CellVolume_GB(i,j,k,iBlock) &
-              )
+       call do_face(1, i, j, k, iBlock)
+       call do_face(2, i, j, k, iBlock)
 
-         Energy_GBI(i,j,k,iBlock,1) = EnergyOld_CBI(i,j,k,iBlock,1) &
-              + Cfl*time_BLK(i,j,k,iBlock)* &
-              (Source_VCI(nVar+1,i,j,k,iGang) + &
-              ( Flux_VXI(nVar+1,i,j,k,iGang) - Flux_VXI(nVar+1,i+1,j,k,iGang)  &
-              + Flux_VYI(nVar+1,i,j,k,iGang) - Flux_VYI(nVar+1,i,j+1,k,iGang)  &
-              + Flux_VZI(nVar+1,i,j,k,iGang) - Flux_VZI(nVar+1,i,j,k+1,iGang)  ) &
-              /CellVolume_GB(i,j,k,iBlock) &
-              )
+       if(nDim > 1)then
+          call do_face(3, i, j, k, iBlock)
+          call do_face(4, i, j, k, iBlock)
+       end if
 
-         ! Calculate pressure from energy
-         State_VGB(p_,i,j,k,iBlock) = &
-              GammaMinus1_I(1)*( Energy_GBI(i,j,k,iBlock,1) - &
-              0.5*( &
-              ( State_VGB(RhoUx_,i,j,k,iBlock)**2 &
-              + State_VGB(RhoUy_,i,j,k,iBlock)**2 &
-              + State_VGB(RhoUz_,i,j,k,iBlock)**2 &
-              )/State_VGB(Rho_,i,j,k,iBlock)      &
-              + State_VGB(Bx_,i,j,k,iBlock)**2    &
-              + State_VGB(By_,i,j,k,iBlock)**2    &
-              + State_VGB(Bz_,i,j,k,iBlock)**2    &
-              )          )
+       if(nDim > 2)then
+          call do_face(5, i, j, k, iBlock)
+          call do_face(6, i, j, k, iBlock)
+       end if
 
-      enddo; enddo; enddo
-    
+       ! Calculate pressure from energy
+       State_VGB(p_,i,j,k,iBlock) = &
+            GammaMinus1_I(1)*( Energy_GBI(i,j,k,iBlock,1) - &
+            0.5*( &
+            ( State_VGB(RhoUx_,i,j,k,iBlock)**2 &
+            + State_VGB(RhoUy_,i,j,k,iBlock)**2 &
+            + State_VGB(RhoUz_,i,j,k,iBlock)**2 &
+            )/State_VGB(Rho_,i,j,k,iBlock)      &
+            + State_VGB(Bx_,i,j,k,iBlock)**2    &
+            + State_VGB(By_,i,j,k,iBlock)**2    &
+            + State_VGB(Bz_,i,j,k,iBlock)**2    &
+            )          )
+    enddo; enddo; enddo
+
   end subroutine update_state_gpu
   !============================================================================
+  subroutine do_face(iFace, i, j, k, iBlock)
+    !$acc routine seq
+
+    integer, intent(in):: iFace, i, j, k, iBlock
+
+    real :: Area, NormalX, NormalY, NormalZ, Un, Cmax
+    real :: StateLeft_V(nVar), StateRight_V(nVar), State_V(nVar)
+    real :: StateLeftCons_V(nFlux), StateRightCons_V(nFlux)
+    real :: FluxLeft_V(nFlux), FluxRight_V(nFlux), Flux_V(nFlux)
+    real :: DtPerDv
+    !--------------------------------------------------------------------------
+    select case(iFace)
+    case(1)
+       Area = CellFace_DB(x_,iBlock)
+       NormalX = +1.0
+       NormalY =  0.0
+       NormalZ =  0.0
+       StateLeft_V           = State_VGB(:,i-1,j,k,iBlock)
+       StateLeft_V(Ux_:Uz_)  = StateLeft_V(Ux_:Uz_)/StateLeft_V(Rho_)
+       StateRight_V          = State_VGB(:,i,j,k,iBlock)
+       StateRight_V(Ux_:Uz_) = StateRight_V(Ux_:Uz_)/StateRight_V(Rho_)
+    case(2)
+       Area = CellFace_DB(x_,iBlock)
+       NormalX = -1.0
+       NormalY =  0.0
+       NormalZ =  0.0
+       StateLeft_V           = State_VGB(:,i,j,k,iBlock)
+       StateLeft_V(Ux_:Uz_)  = StateLeft_V(Ux_:Uz_)/StateLeft_V(Rho_)
+       StateRight_V          = State_VGB(:,i+1,j,k,iBlock)
+       StateRight_V(Ux_:Uz_) = StateRight_V(Ux_:Uz_)/StateRight_V(Rho_)
+    case(3)
+       Area = CellFace_DB(y_,iBlock)
+       NormalX =  0.0
+       NormalY = +1.0
+       NormalZ =  0.0
+       StateLeft_V           = State_VGB(:,i,j-1,k,iBlock)
+       StateLeft_V(Ux_:Uz_)  = StateLeft_V(Ux_:Uz_)/StateLeft_V(Rho_)
+       StateRight_V          = State_VGB(:,i,j,k,iBlock)
+       StateRight_V(Ux_:Uz_) = StateRight_V(Ux_:Uz_)/StateRight_V(Rho_)
+    case(4)
+       Area = CellFace_DB(y_,iBlock)
+       NormalX =  0.0
+       NormalY = -1.0
+       NormalZ =  0.0
+       StateLeft_V           = State_VGB(:,i,j,k,iBlock)
+       StateLeft_V(Ux_:Uz_)  = StateLeft_V(Ux_:Uz_)/StateLeft_V(Rho_)
+       StateRight_V          = State_VGB(:,i,j+1,k,iBlock)
+       StateRight_V(Ux_:Uz_) = StateRight_V(Ux_:Uz_)/StateRight_V(Rho_)
+    case(5)
+       Area = CellFace_DB(z_,iBlock)
+       NormalX =  0.0
+       NormalY =  0.0
+       NormalZ = +1.0
+       StateLeft_V           = State_VGB(:,i,j,k-1,iBlock)
+       StateLeft_V(Ux_:Uz_)  = StateLeft_V(Ux_:Uz_)/StateLeft_V(Rho_)
+       StateRight_V          = State_VGB(:,i,j,k,iBlock)
+       StateRight_V(Ux_:Uz_) = StateRight_V(Ux_:Uz_)/StateRight_V(Rho_)
+    case(6)
+       Area = CellFace_DB(z_,iBlock)
+       NormalX =  0.0
+       NormalY =  0.0
+       NormalZ = -1.0
+       ! This could be call get_face(iFace,i,j,k)
+       StateLeft_V           = State_VGB(:,i,j,k,iBlock)
+       StateLeft_V(Ux_:Uz_)  = StateLeft_V(Ux_:Uz_)/StateLeft_V(Rho_)
+       StateRight_V          = State_VGB(:,i,j,k+1,iBlock)
+       StateRight_V(Ux_:Uz_) = StateRight_V(Ux_:Uz_)/StateRight_V(Rho_)
+    end select
+
+    ! average state
+    State_V = 0.5*(StateLeft_V + StateRight_V)
+
+    ! Calculate the Rusanov flux
+    call get_speed_max(State_V, NormalX, NormalY, NormalZ, &
+         Un, Cmax)
+    call get_physical_flux(StateLeft_V, NormalX, NormalY, NormalZ, &
+         StateLeftCons_V, FluxLeft_V)
+    call get_physical_flux(StateRight_V, NormalX, NormalY, NormalZ, &
+         StateRightCons_V, FluxRight_V)
+
+    ! Rusanov flux
+    Flux_V = Area*(0.5*(FluxLeft_V + FluxRight_V) &
+         + 0.5*Cmax*(StateLeftCons_V - StateRightCons_V))
+
+    ! Time step divided by cell volume
+!!!    DtPerDv = Cfl*time_BLK(i,j,k,iBlock)/CellVolume_GB(i,j,k,iBlock)
+    DtPerDv = Dt/CellVolume_GB(i,j,k,iBlock)
+
+    ! Update state
+    State_VGB(:,i,j,k,iBlock) = State_VGB(:,i,j,k,iBlock) &
+         + DtPerDv*Flux_V(1:nVar)
+    Energy_GBI(i,j,k,iBlock,1) = Energy_GBI(i,j,k,iBlock,1) &
+         + DtPerDv*Flux_V(nVar+1)
+
+  end subroutine do_face
+  !==========================================================================
+  subroutine get_physical_flux(State_V, NormalX, NormalY, NormalZ, &
+       StateCons_V, Flux_V)
+    !$acc routine seq
+
+    real, intent(in) :: State_V(nVar)      ! primitive state vector
+    real, intent(in) :: NormalX, NormalY, NormalZ ! face normal
+    real, intent(out):: StateCons_V(nFlux) ! conservative state vector
+    real, intent(out):: Flux_V(nFlux)      ! conservative flux
+
+    real:: Rho, Ux, Uy, Uz, Bx, By, Bz, Hyp, p, e, Un, Bn, pB, pTot
+    !------------------------------------------------------------------------
+    Rho = State_V(Rho_)
+    Ux  = State_V(Ux_)
+    Uy  = State_V(Uy_)
+    Uz  = State_V(Uz_)
+    Bx  = State_V(Bx_)
+    By  = State_V(By_)
+    Bz  = State_V(Bz_)
+    if(Hyp_ > 1) Hyp = State_V(Hyp_)
+    p   = State_V(p_)
+
+    ! Convenient variables
+    Un  = Ux*NormalX + Uy*NormalY + Uz*NormalZ
+    Bn  = Bx*NormalX + By*NormalY + Bz*NormalZ
+    pB  = 0.5*(Bx**2 + By**2 + Bz**2)
+
+    ! Hydro energy density
+    e   = InvGammaMinus1*p + 0.5*Rho*(Ux**2 + Uy**2 + Uz**2)
+
+    ! Conservative state for the Rusanov solver
+    StateCons_V(Rho_)    = Rho
+    StateCons_V(RhoUx_)  = Rho*Ux
+    StateCons_V(RhoUy_)  = Rho*Uy
+    StateCons_V(RhoUz_)  = Rho*Uz
+    StateCons_V(Bx_)     = Bx
+    StateCons_V(By_)     = By
+    StateCons_V(Bz_)     = Bz
+    if(Hyp_ > 1) StateCons_V(Hyp_) = Hyp
+    StateCons_V(p_)      = p
+    StateCons_V(Energy_) = e + pB  ! Add magnetic energy density
+
+    ! Physical flux
+    pTot = p + pB
+    Flux_V(Rho_)    = Rho*Un
+    Flux_V(RhoUx_)  = Un*Rho*Ux - Bn*Bx + NormalX*pTot
+    Flux_V(RhoUy_)  = Un*Rho*Uy - Bn*By + NormalY*pTot
+    Flux_V(RhoUz_)  = Un*Rho*Uz - Bn*Bz + NormalZ*pTot
+    if(Hyp_ > 1)then
+       Flux_V(Bx_)  =  Un*Bx - Ux*Bn + NormalX*SpeedHyp*Hyp
+       Flux_V(By_)  =  Un*By - Uy*Bn + NormalY*SpeedHyp*Hyp
+       Flux_V(Bz_)  =  Un*Bz - Uz*Bn + NormalZ*SpeedHyp*Hyp
+       Flux_V(Hyp_) =  SpeedHyp*Bn
+    else
+       Flux_V(Bx_)  =  Un*Bx - Ux*Bn
+       Flux_V(By_)  =  Un*By - Uy*Bn
+       Flux_V(Bz_)  =  Un*Bz - Uz*Bn
+    end if
+    Flux_V(p_)      =  Un*p
+    Flux_V(Energy_) =  Un*(e + p) &
+         + Flux_V(Bx_)*Bx + Flux_V(By_)*By + Flux_V(Bz_)*Bz ! Poynting flux
+
+  end subroutine get_physical_flux
+  !==========================================================================
+  subroutine get_speed_max(State_V, NormalX, NormalY, NormalZ, Un, Cmax)
+    !$acc routine seq
+
+    real, intent(in):: State_V(nVar), NormalX, NormalY, NormalZ
+    real, intent(out):: Un, Cmax
+
+    real:: InvRho, p, Bx, By, Bz, Bn, B2
+    real:: Sound2, Alfven2, Alfven2Normal, Fast2, Discr, Fast
+    !------------------------------------------------------------------------
+
+    InvRho = 1.0/State_V(Rho_)
+    Bx  = State_V(Bx_)
+    By  = State_V(By_)
+    Bz  = State_V(Bz_)
+    p   = State_V(p_)
+    Bn  = Bx*NormalX + By*NormalY + Bz*NormalZ
+    B2  = Bx**2 + By**2 + Bz**2
+
+    Sound2        = InvRho*p*Gamma
+    Alfven2       = InvRho*B2
+    Alfven2Normal = InvRho*Bn**2
+    Fast2 = Sound2 + Alfven2
+    Discr = sqrt(max(0.0, Fast2**2 - 4*Sound2*Alfven2Normal))
+    Fast  = sqrt( 0.5*(Fast2 + Discr) )
+
+    Un   = State_V(Ux_)*NormalX + State_V(Uy_)*NormalY + State_V(Uz_)*NormalZ
+    Cmax = abs(Un) + Fast
+
+#ifndef OPENACC
+    if(DoTestCell)then
+       write(*,*) &
+            ' iFluid, rho, p(face)   =', 1, State_V(Rho_), State_V(p_)
+       ! if(UseAnisoPressure) write(*,*) &
+       !     ' Ppar, Perp             =', Ppar, Pperp
+       ! if(UseElectronPressure) write(*,*) &
+       !     ' State_V(Pe_)           =', State_V(Pe_)
+       ! if(UseAnisoPe) write(*,*) &
+       !     ' State_V(Pepar_)        =', State_V(Pepar_)
+       ! if(UseWavePressure) write(*,*) &
+       !     ' GammaWave, State(Waves)=', &
+       !     GammaWave, State_V(WaveFirst_:WaveLast_)
+       write(*,*) &
+            ' Fast2, Discr          =', Fast2, Discr
+       write(*,*) &
+            ' Sound2, Alfven2       =', Sound2, Alfven2
+       write(*,*) &
+            ' FullBn, Alfven2Normal =', Bn, Alfven2Normal
+       write(*,*) &
+            ' FullBx, FullBy, FullBz=', Bx, By, Bz
+    end if
+#endif
+
+  end subroutine get_speed_max
+  !==========================================================================
 
 end module ModUpdateStateGpu
 !==============================================================================
