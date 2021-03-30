7480eed284d5d18ad2f9b61f06149c9df768e0e3
Gabor Toth <gtoth1@pfe26.nas.nasa.gov>
2021-03-30 09:39:45 -0700

    Added a CPU-friendly version.

--- src/ModUpdateStateGpu.f90
+++ src/ModUpdateStateGpu.f90
@@ -11,19 +11,258 @@ module ModUpdateStateGpu
 !!!  time_BLK, StateOld_VGB, State_VGB, EnergyOld_CBI, Energy_GBI
   use BATL_lib, ONLY: nDim, nI, nJ, nK, MinI, MaxI, MinJ, MaxJ, MinK, MaxK, &
        nBlock, Unused_B, x_, y_, z_, CellVolume_B, CellFace_DB, &
-       test_start, test_stop, iTest, jTest, kTest
+       test_start, test_stop, iTest, jTest, kTest, iBlockTest
   use ModPhysics, ONLY: Gamma, InvGammaMinus1, GammaMinus1_I
   use ModMain, ONLY: SpeedHyp
 
   implicit none
 
+  private ! except
+
+  public:: update_state_gpu
+  public:: update_state_gpu_v1
+  
   logical:: DoTestCell= .false.
 
 contains
   !============================================================================
   subroutine update_state_gpu
 
-    logical:: DoTestCell
+    integer:: i, j, k, iBlock
+
+    real:: DtPerDv, Change_V(nFlux)
+    real:: Flux_VX(nFlux,nI+1,nJ,nK)
+    real:: Flux_VY(nFlux,nI,nJ+1,nK)
+    real:: Flux_VZ(nFlux,nI,nJ,nK+1)
+
+    !$acc declare create (DtPerDv, Change_V, Flux_VX, Flux_VY, Flux_VZ)
+
+    logical:: DoTest
+    character(len=*), parameter:: NameSub = 'update_state_gpu'
+    !--------------------------------------------------------------------------
+#ifndef OPENACC
+    call test_start(NameSub, DoTest)
+#endif
+
+    !$acc parallel
+    !$acc loop gang private(Flux_VX, Flux_VY, Flux_VZ) independent
+    do iBlock = 1, nBlock
+       if(Unused_B(iBlock)) CYCLE
+
+       !$acc loop vector collapse(3) private(Change_V) independent
+       do k = 1, nK; do j = 1, nJ; do i = 1, nI+1
+#ifndef OPENACC
+          DoTestCell = DoTest .and. (i == iTest .or. i == iTest+1) .and. &
+               j == jTest .and. k == kTest .and. iBlock == iBlockTest
+#endif
+          call get_flux_x(i, j, k, iBlock, Flux_VX(:,i,j,k))
+
+       end do; end do; end do
+
+       if(nDim > 1)then
+          !$acc loop vector collapse(3) private(Change_V) independent
+          do k = 1, nK; do j = 1, nJ+1; do i = 1, nI
+#ifndef OPENACC
+             DoTestCell = DoTest .and. i == iTest .and. &
+                  (j == jTest .or. j==jTest+1) .and. k == kTest .and. iBlock == iBlockTest
+#endif
+             call get_flux_y(i, j, k, iBlock, Flux_VY(:,i,j,k))
+             
+          end do; end do; end do
+       end if
+       
+       if(nDim > 2)then
+          !$acc loop vector collapse(3) private(Change_V) independent
+          do k = 1, nK+1; do j = 1, nJ; do i = 1, nI
+#ifndef OPENACC
+             DoTestCell = DoTest .and. i == iTest .and. &
+                  j == jTest .and. (k == kTest .or. k == kTest+1) .and. iBlock == iBlockTest
+#endif
+             call get_flux_z(i, j, k, iBlock, Flux_VZ(:,i,j,k))
+             
+          end do; end do; end do
+       end if
+
+       !$acc loop vector collapse(3) private(DtPerDv, Change_V) independent
+       do k = 1, nK; do j = 1, nJ; do i = 1, nI
+          
+          Change_V              =            Flux_VX(:,i,j,k) - Flux_VX(:,i+1,j,k)
+          if(nDim > 1) Change_V = Change_V + Flux_VY(:,i,j,k) - Flux_VY(:,i,j+1,k)
+          if(nDim > 2) Change_V = Change_V + Flux_VZ(:,i,j,k) - Flux_VZ(:,i,j,k+1) 
+
+          ! Time step divided by cell volume
+!!!       DtPerDv = Cfl*time_BLK(i,j,k,iBlock)/CellVolume_GB(i,j,k,iBlock)
+          DtPerDv = Dt/CellVolume_B(iBlock)
+          
+          ! Update state
+          State_VGB(:,i,j,k,iBlock) = State_VGB(:,i,j,k,iBlock) &
+               + DtPerDv*Change_V(1:nVar)
+          Energy_GBI(i,j,k,iBlock,1) = Energy_GBI(i,j,k,iBlock,1) &
+               + DtPerDv*Change_V(nVar+1)
+
+          ! Calculate pressure from energy
+          State_VGB(p_,i,j,k,iBlock) = &
+               GammaMinus1_I(1)*( Energy_GBI(i,j,k,iBlock,1) - &
+               0.5*( &
+               ( State_VGB(RhoUx_,i,j,k,iBlock)**2 &
+               + State_VGB(RhoUy_,i,j,k,iBlock)**2 &
+               + State_VGB(RhoUz_,i,j,k,iBlock)**2 &
+               )/State_VGB(Rho_,i,j,k,iBlock)      &
+               + State_VGB(Bx_,i,j,k,iBlock)**2    &
+               + State_VGB(By_,i,j,k,iBlock)**2    &
+               + State_VGB(Bz_,i,j,k,iBlock)**2    &
+               )          )
+
+#ifndef OPENACC
+          DoTestCell = DoTest .and. i==iTest .and. j==jTest .and. k==kTest &
+               .and. iBlock == iBlockTest
+          if(DoTestCell)then
+             write(*,*)'State_VGB =', State_VGB(:,i,j,k,iBlock)
+             write(*,*)'Energy_GBI=', Energy_GBI(i,j,k,iBlock,:)
+             write(*,*)'Change_V  =', Change_V
+             write(*,*)'DtPerDv   =', DtPerDv
+          end if
+#endif
+
+       enddo; enddo; enddo
+
+    end do
+    !$acc end parallel
+
+#ifndef OPENACC
+    call test_stop(NameSub, DoTest, iBlock)
+#endif
+
+  end subroutine update_state_gpu
+  !=======================================
+  subroutine get_flux_x(i, j,  k, iBlock, Flux_V)
+    !$acc routine seq
+    
+    integer, intent(in):: i, j, k, iBlock
+    real,    intent(out):: Flux_V(nFlux)
+
+    real :: Area, NormalX, NormalY, NormalZ, Un, Cmax
+    real :: StateLeft_V(nVar), StateRight_V(nVar), State_V(nVar)
+    real :: StateLeftCons_V(nFlux), StateRightCons_V(nFlux)
+    real :: FluxLeft_V(nFlux), FluxRight_V(nFlux)
+    !--------------------------------------
+    ! if(IsCartesianGrid)then
+    Area = CellFace_DB(x_,iBlock)
+    NormalX = 1.0
+    NormalY = 0.0
+    NormalZ = 0.0
+    ! end if
+
+    ! First order left state of primitive variables
+    StateLeft_V  = State_VGB(:,i-1,j,k,iBlock)
+    StateLeft_V(Ux_:Uz_) = StateLeft_V(Ux_:Uz_)/StateLeft_V(Rho_)
+
+    ! First order right state of primitive variables
+    StateRight_V  = State_VGB(:,i,j,k,iBlock)
+    StateRight_V(Ux_:Uz_) = StateRight_V(Ux_:Uz_)/StateRight_V(Rho_)
+
+    ! average state
+    State_V = 0.5*(StateLeft_V + StateRight_V)
+
+    call get_speed_max(State_V, NormalX, NormalY, NormalZ, &
+         Un, Cmax)
+    call get_physical_flux(StateLeft_V, NormalX, NormalY, NormalZ, &
+         StateLeftCons_V, FluxLeft_V)
+    call get_physical_flux(StateRight_V, NormalX, NormalY, NormalZ, &
+         StateRightCons_V, FluxRight_V)
+
+    ! Rusanov flux
+    Flux_V = Area*0.5*((FluxLeft_V + FluxRight_V) &
+         +             Cmax*(StateLeftCons_V - StateRightCons_V))
+
+  end subroutine get_flux_x
+  !=======================================
+  subroutine get_flux_y(i, j, k, iBlock, Flux_V)
+    !$acc routine seq
+    
+    integer, intent(in):: i, j, k, iBlock
+    real,    intent(out):: Flux_V(nFlux)
+
+    real :: Area, NormalX, NormalY, NormalZ, Un, Cmax
+    real :: StateLeft_V(nVar), StateRight_V(nVar), State_V(nVar)
+    real :: StateLeftCons_V(nFlux), StateRightCons_V(nFlux)
+    real :: FluxLeft_V(nFlux), FluxRight_V(nFlux)
+    !--------------------------------------
+    ! if(IsCartesianGrid)then
+    Area = CellFace_DB(y_,iBlock)
+    NormalX = 0.0
+    NormalY = 1.0
+    NormalZ = 0.0
+    ! end if
+
+    ! First order left state of primitive variables
+    StateLeft_V  = State_VGB(:,i,j-1,k,iBlock)
+    StateLeft_V(Ux_:Uz_) = StateLeft_V(Ux_:Uz_)/StateLeft_V(Rho_)
+
+    ! First order right state of primitive variables
+    StateRight_V  = State_VGB(:,i,j,k,iBlock)
+    StateRight_V(Ux_:Uz_) = StateRight_V(Ux_:Uz_)/StateRight_V(Rho_)
+
+    ! average state
+    State_V = 0.5*(StateLeft_V + StateRight_V)
+
+    call get_speed_max(State_V, NormalX, NormalY, NormalZ, &
+         Un, Cmax)
+    call get_physical_flux(StateLeft_V, NormalX, NormalY, NormalZ, &
+         StateLeftCons_V, FluxLeft_V)
+    call get_physical_flux(StateRight_V, NormalX, NormalY, NormalZ, &
+         StateRightCons_V, FluxRight_V)
+
+    ! Rusanov flux
+    Flux_V = Area*0.5*((FluxLeft_V + FluxRight_V) &
+         +             Cmax*(StateLeftCons_V - StateRightCons_V))
+
+  end subroutine get_flux_y
+  !=======================================
+  subroutine get_flux_z(i, j, k, iBlock, Flux_V)
+    !$acc routine seq
+    
+    integer, intent(in):: i, j, k, iBlock
+    real,    intent(out):: Flux_V(nFlux)
+
+    real :: Area, NormalX, NormalY, NormalZ, Un, Cmax
+    real :: StateLeft_V(nVar), StateRight_V(nVar), State_V(nVar)
+    real :: StateLeftCons_V(nFlux), StateRightCons_V(nFlux)
+    real :: FluxLeft_V(nFlux), FluxRight_V(nFlux)
+    !--------------------------------------
+    ! if(IsCartesianGrid)then
+    Area = CellFace_DB(z_,iBlock)
+    NormalX = 0.0
+    NormalY = 0.0
+    NormalZ = 1.0
+    ! end if
+
+    ! First order left state of primitive variables
+    StateLeft_V  = State_VGB(:,i,j,k-1,iBlock)
+    StateLeft_V(Ux_:Uz_) = StateLeft_V(Ux_:Uz_)/StateLeft_V(Rho_)
+
+    ! First order right state of primitive variables
+    StateRight_V  = State_VGB(:,i,j,k,iBlock)
+    StateRight_V(Ux_:Uz_) = StateRight_V(Ux_:Uz_)/StateRight_V(Rho_)
+
+    ! average state
+    State_V = 0.5*(StateLeft_V + StateRight_V)
+
+    call get_speed_max(State_V, NormalX, NormalY, NormalZ, &
+         Un, Cmax)
+    call get_physical_flux(StateLeft_V, NormalX, NormalY, NormalZ, &
+         StateLeftCons_V, FluxLeft_V)
+    call get_physical_flux(StateRight_V, NormalX, NormalY, NormalZ, &
+         StateRightCons_V, FluxRight_V)
+
+    ! Rusanov flux
+    Flux_V = Area*0.5*((FluxLeft_V + FluxRight_V) &
+         +             Cmax*(StateLeftCons_V - StateRightCons_V))
+
+  end subroutine get_flux_z
+  !============================================================================
+  subroutine update_state_gpu_v1
+
     integer:: i, j, k, iBlock
 
     real:: Change_V(nFlux), Change_VC(nFlux,nI,nJ,nK), DtPerDv
@@ -33,7 +272,7 @@ contains
     character(len=*), parameter:: NameSub = 'update_state_gpu'
     !--------------------------------------------------------------------------
 #ifndef OPENACC
-    call test_start(NameSub, DoTest, iBlock)
+    call test_start(NameSub, DoTest)
 #endif
 
     !$acc parallel
@@ -45,7 +284,8 @@ contains
        do k = 1, nK; do j = 1, nJ; do i = 1, nI
 
 #ifndef OPENACC
-          DoTestCell = DoTest .and. i==iTest .and. j==jTest .and. k==kTest
+          DoTestCell = DoTest .and. i==iTest .and. j==jTest .and. k==kTest &
+               .and. iBlock == iBlockTest
 #endif
 
           ! Initialize change in State_VGB
@@ -93,6 +333,18 @@ contains
                + State_VGB(By_,i,j,k,iBlock)**2    &
                + State_VGB(Bz_,i,j,k,iBlock)**2    &
                )          )
+
+#ifndef OPENACC
+          DoTestCell = DoTest .and. i==iTest .and. j==jTest .and. k==kTest &
+               .and. iBlock == iBlockTest
+          if(DoTestCell)then
+             write(*,*)'State_VGB =', State_VGB(:,i,j,k,iBlock)
+             write(*,*)'Energy_GBI=', Energy_GBI(i,j,k,iBlock,:)
+             write(*,*)'Change_V  =', Change_VC(:,i,j,k)
+             write(*,*)'DtPerDv   =', DtPerDv
+          end if
+#endif
+
        enddo; enddo; enddo
 
     end do
@@ -102,7 +354,7 @@ contains
     call test_stop(NameSub, DoTest, iBlock)
 #endif
 
-  end subroutine update_state_gpu
+  end subroutine update_state_gpu_v1
   !============================================================================
   subroutine do_face(iFace, i, j, k, iBlock, Change_V)
     !$acc routine seq
