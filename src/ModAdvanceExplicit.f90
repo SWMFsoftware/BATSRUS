feb0f827c90d497492f294e6dcd7f3f6c89311c8
Gabor Toth <gtoth@umich.edu>
2021-04-01 11:21:46 -0400

    Reinstated the GPU execution for the general code.

--- src/ModAdvanceExplicit.f90
+++ src/ModAdvanceExplicit.f90
@@ -41,8 +41,8 @@ contains
     use ModResistivity, ONLY: set_resistivity, UseResistivity
     use ModFieldLineThread, ONLY: &
          UseFieldLineThreads, advance_threads, Enthalpy_
+    use ModUpdateStateFast, ONLY: update_state_gpu, update_state_cpu
     use ModUpdateState, ONLY: update_check, update_state
-    use ModUpdateStateFast
     use ModConstrainDivB, ONLY: Bface2Bcenter, get_vxb, bound_vxb, constrain_b
     use ModFixAxisCells, ONLY: fix_axis_cells
     use ModElectricField, ONLY: get_num_electric_field, correct_efield_block
@@ -137,13 +137,14 @@ contains
 #endif
        else
           ! Multi-block solution update.
-          ! acc parallel
-          ! acc loop gang
+          !$acc parallel
+          !$acc loop gang
           !$omp parallel do
           do iBlock = 1,nBlock
 
              if(Unused_B(iBlock)) CYCLE
 
+#ifndef OPENACC
              ! Calculate interface values for L/R states of each face
              ! and apply BCs for interface states as needed.
              call set_b0_face(iBlock)
@@ -153,31 +154,48 @@ contains
                 call calc_cell_flux(iBlock)
                 call timing_stop('calc_fluxes')
              end if
+#endif
 
+#ifndef OPENACC
              call timing_start('calc_facevalues')
+#endif
              call calc_face_value(iBlock, DoResChangeOnly=.false., &
                   DoMonotoneRestrict=.true.)
+#ifndef OPENACC
              call timing_stop('calc_facevalues')
              if(body_BLK(iBlock)) &
                   call set_face_boundary(iBlock, Time_Simulation,.false.)
+#endif
 
              if(.not.DoInterpolateFlux)then
                 ! Compute interface fluxes for each cell.
+#ifndef OPENACC
                 call timing_start('calc_fluxes')
+#endif
                 call calc_face_flux(.false., iBlock)
+#ifndef OPENACC
                 call timing_stop('calc_fluxes')
+#endif
              end if
 
+#ifndef OPENACC
              ! Enforce flux conservation by applying corrected fluxes
              ! to each coarse grid cell face at block edges with
              ! resolution changes.
              if(DoConserveFlux) call apply_cons_flux(iBlock)
+#endif
 
+#ifndef OPENACC
              ! Compute source terms for each cell.
              call timing_start('calc_sources')
+#endif
              call calc_source(iBlock)
-             call timing_stop('calc_sources')
 
+#ifndef OPENACC
+             call timing_stop('calc_sources')
+#endif
+
+#ifndef OPENACC
              ! With known magnetic field and electric field in the
              ! comoving frame update ion velocities at the half time-step
              if(UseFlic.and.iStage>=2)call advance_ion_current(iBlock)
@@ -199,7 +217,9 @@ contains
 
              ! Update solution state in each cell.
              call timing_start('update_state')
+#endif
              call update_state(iBlock)
+#ifndef OPENACC
              call timing_stop('update_state')
 
              if(DoCalcElectricField .and. iStage == nStage) &
@@ -211,6 +231,7 @@ contains
                 call bound_VxB(iBlock)
                 call timing_stop('constrain_B')
              end if
+#endif
 
              ! Calculate time step (both local and global
              ! for the block) used in multi-stage update
@@ -221,13 +242,14 @@ contains
                   iStage == nStage .and. DoCalcTimestep) &
                   call calc_timestep(iBlock)
 
+#ifndef OPENACC
              ! At this point the user has surely set all "block data"
-             ! NOTE: The user has the option of calling set_block_data directly.
+             ! NOTE: The user has the option of calling set_block_data directly
              call set_block_data(iBlock)
-
+#endif
           end do ! Multi-block solution update loop.
           !$omp end parallel do
-          ! acc end parallel
+          !$acc end  parallel
        end if
 
        if(DoTest)write(*,*)NameSub,' done update blocks'
