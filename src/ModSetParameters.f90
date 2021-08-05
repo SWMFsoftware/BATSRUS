2b2f9959806407e1161953bd269ccf36349fefb1
Igor V. Sokolov <igorsok@umich.edu>
2021-08-05 12:16:52 -0400

    Comments and descriptions, the development work in progress

--- src/ModSetParameters.f90
+++ src/ModSetParameters.f90
@@ -1705,7 +1705,8 @@ contains
           ! reinitialize constrained transport if needed
           DoInitConstrainB = .true.
 
-       case("#USEB0", "#DIVBSOURCE", "#USECURLB0", "#MONOPOLEB0")
+       case("#USEB0", "#DIVBSOURCE", "#USECURLB0", "#MONOPOLEB0", &
+            "#B0MOMENTUMFLUX", "#DIVFULLBSOURCE")
           if(.not.is_first_session())CYCLE READPARAM
           call read_b0_param(NameCommand)
 
