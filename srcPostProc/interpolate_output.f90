0451bd05a8bb1590ac32ed72f01d0d8a49f0d011
Timothy Keebler <tkeebler@umich.edu>
2022-05-31 20:17:28 -0400

    read time from header for trajectory files only

--- srcPostProc/interpolate_output.f90
+++ srcPostProc/interpolate_output.f90
@@ -370,16 +370,19 @@ contains
 
        if(iError /= 0) EXIT
 
-       iTime_I = 0
-       read(StringHeader(:19),'(i4,1x,i2,1x,i2,1x,i2,1x,i2,1x,i2)', &
-            iostat=iError) iTime_I(1:6)
-
-       if(iError /= 0)then
-          write(*,*) NameProgram,': could not read date from header line.', &
-               trim(NameFileIn)
-          STOP
-       end if
-       call time_int_to_real(iTime_I, Time)
+       if(IsTrajectory)then
+          iTime_I = 0
+          read(StringHeader(:19),'(i4,1x,i2,1x,i2,1x,i2,1x,i2,1x,i2)', &
+               iostat=iError) iTime_I(1:6)
+          
+          if(iError /= 0)then
+             write(*,*) NameProgram,': could not read date from header line.', &
+                  trim(NameFileIn)
+             STOP
+          end if
+          call time_int_to_real(iTime_I, Time)
+       endif
+    
        Time = Time - StartTime
 
        if(IsTrajectory)then
