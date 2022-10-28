bac1d1c45f0cb85adc3f77a3a8421b53d9e82e5b
Toth <gtoth@mesh.engin.umich.edu>
2022-10-28 16:14:23 -0400

    Make it work in less than 3D and more than 2 ghost cells

--- srcUser/ModUserOuterHelio.f90
+++ srcUser/ModUserOuterHelio.f90
@@ -1077,7 +1077,7 @@ contains
     integer,          intent(in)   :: iBlock
     character(len=*), intent(in)   :: NameVar
     logical,          intent(in)   :: IsDimensional
-    real,             intent(out)  :: PlotVar_G(-1:nI+2, -1:nJ+2, -1:nK+2)
+    real,             intent(out)  :: PlotVar_G(MinI:MaxI,MinJ:MaxJ,MinK:MaxK)
     real,             intent(out)  :: PlotVarBody
     logical,          intent(out)  :: UsePlotVarBody
     character(len=*), intent(inout):: NameTecVar
