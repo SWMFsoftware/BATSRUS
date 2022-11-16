4531531979191946a16227775e5fce558f1ca5c0
bartvand <bartvand@csrlap2021-0100.local>
2022-11-16 12:57:50 -0500

    simplified calculation.

--- srcUser/ModUserAwsom.f90
+++ srcUser/ModUserAwsom.f90
@@ -1970,13 +1970,14 @@ contains
     use ModParallel, ONLY: Unset_, DiLevel_EB
     use BATL_size, ONLY: nK, nJ
     use BATL_lib, ONLY: CellFace_DFB, Xyz_DGB
-
+    use ModGeometry, ONLY: r_GB
+    
     integer, intent(in) :: iBlock
 
     integer :: i, j, k
     real :: XyzSph_DD(3,3), dBSph_D(3), dBXyz_D(3)
     real :: BXyzJLeft_D(3), BXyzJRight_D(3), BXyzKLeft_D(3), BXyzKRight_D(3)
-    real :: BSphJLeft_D(3), BSphJRight_D(3), BSphKLeft_D(3), BSphKRight_D(3)
+    real :: BrJLeft, BrJRight, BrKLeft, BrKRight, Runit_D(3)
     !--------------------------------------------------------------------------
     ! Only add the STITCH source term in cells next to the inner boundary.
     ! Works only in spherical coordinates.
@@ -1998,16 +1999,18 @@ contains
        BXyzKRight_D = State_VGB(Bx_:Bz_,i,j,k+1,iBlock)
        if(UseB0) BXyzKRight_D = BXyzKRight_D + B0_DGB(:,i,j,k+1,iBlock)
 
-       BSphJLeft_D = matmul(BXyzJLeft_D, XyzSph_DD)
-       BSphJRight_D = matmul(BXyzJRight_D, XyzSph_DD)
-       BSphKLeft_D = matmul(BXyzKLeft_D, XyzSph_DD)
-       BSphKRight_D = matmul(BXyzKRight_D, XyzSph_DD)
+       Runit_D = Xyz_DGB(:,i,j,k,iBlock)/r_GB(i,j,k,iBlock)
+       
+       BrJLeft  = sum(BXyzJLeft_D*Runit_D)
+       BrJRight = sum(BXyzJRight_D*Runit_D)
+       BrKLeft  = sum(BXyzKLeft_D*Runit_D)
+       BrKRight = sum(BXyzKRight_D*Runit_D)
 
        dBSph_D(1) = 0
-       dBSph_D(2) = -Zeta*(BSphKRight_D(1)-BSphKLeft_D(1))/ &
-            (CellFace_DFB(2,i,j,k,iBlock)+CellFace_DFB(2,i,j+1,k,iBlock))
-       dBSph_D(3) =  Zeta*(BSphJRight_D(1)-BSphJLeft_D(1))/ &
-            (CellFace_DFB(3,i,j,k,iBlock)+CellFace_DFB(3,i,j,k+1,iBlock))
+       dBSph_D(2) = -Zeta*(BrKRight - BrKLeft)/ &
+            (CellFace_DFB(2,i,j,k,iBlock) + CellFace_DFB(2,i,j+1,k,iBlock))
+       dBSph_D(3) =  Zeta*(BrJRight - BrJLeft)/ &
+            (CellFace_DFB(3,i,j,k,iBlock) + CellFace_DFB(3,i,j,k+1,iBlock))
        dBXyz_D = matmul(XyzSph_DD, dBSph_D)
 
        Source_VC(Bx_:Bz_,i,j,k) = Source_VC(Bx_:Bz_,i,j,k) + dBXyz_D
