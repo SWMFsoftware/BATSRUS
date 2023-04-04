7cae2c8cf30c27d2af35d62e929e4f5100abe805
bartvand <bartvand@csrlap2021-0100.local>
2023-04-04 15:59:23 -0400

    changed test to include PUI (no binning yet)

--- srcEquation/ModEquationOuterHelioAwsom.f90
+++ srcEquation/ModEquationOuterHelioAwsom.f90
@@ -6,8 +6,12 @@ module ModVarIndexes
   use ModExtraVariables, &
        Redefine1 => nWave, &
        Redefine2 => WaveFirst_, &
-       Redefine3 => WaveLast_, &
-       Redefine4 => Pe_
+       Redefine3 => WaveLast_, &       
+       Redefine4 => Pe_, &
+       Redefine5 => nPui, &
+       Redefine6 => PuiFirst_, &
+       Redefine7 => PuiLast_, &
+       Redefine8 => iPparIon_I
 
   implicit none
 
@@ -16,73 +20,87 @@ module ModVarIndexes
   character (len=*), parameter :: &
        NameEquationFile = "ModEquationOuterHelioAwsom.f90"
 
-  ! This equation module contains the standard MHD equations +
-  ! Alfven wave energy + electron pressure + 4 neutrals
+  ! This equation module contains two ion fluids +
+  ! Alfven wave energy + electron pressure + pickup ions spectrum + 4 neutrals
   character (len=*), parameter :: &
-       NameEquation = 'MHD + Alfven waves + electron pressure + four neutrals'
+       NameEquation = 'MHD + Alfven waves + Pe + PUI + four neutrals'
 
-  ! Number of variables without energy:
-  integer, parameter :: nVar = 31
-
-  ! loop variable for implied do-loop over spectrum
-  integer, private :: iWave
+  ! loop variable for implied do-loop over spectrum and PUI
+  integer, private :: iWave, iPui
 
   ! Number of bins in Alfven wave spectrum
   integer, parameter :: nWave = 2
 
-  ! 1 ion fluid and 4 neutral fluids
-  integer, parameter :: nFluid    = 5
-  integer, parameter :: IonFirst_ = 1
-  integer, parameter :: IonLast_  = 1
-  logical, parameter :: IsMhd     = .true.
-  real               :: MassFluid_I(nFluid) = [ 1.0, 1.0, 1.0, 1.0, 1.0 ]
+  ! Number of PUI bins
+  integer, parameter :: nPui = 0
 
+  ! Number of variables without energy:
+  integer, parameter :: nVar = 34 + nWave + nPui
+
+  ! 2 ion fluid and 4 neutral fluids
+  integer, parameter :: nFluid    = 6
+  integer, parameter :: IonFirst_ = 1
+  integer, parameter :: IonLast_  = 2
+  logical, parameter :: IsMhd     = .false.
+  real               :: MassFluid_I(nFluid) = 1.0
+
+  ! SWH is the Solar wind hydrogen fluid, Pu3
+  ! are pickup ions produced in region 3 (see mod user),
+  ! and Neu, Ne2, Ne3, Ne4 are neutrals produced in the corresponding regions
   character (len=3), parameter :: &
-       NameFluid_I(nFluid) = ['Ion', 'Neu', 'Ne2', 'Ne3', 'Ne4']
+       NameFluid_I(nFluid) = ['SWH', 'Pu3', 'Neu', 'Ne2', 'Ne3', 'Ne4']
 
   ! Named indexes for State_VGB and other variables
   ! These indexes should go subsequently, from 1 to nVar+nFluid.
   ! The energies are handled as an extra variable, so that we can use
   ! both conservative and non-conservative scheme and switch between  them.
   integer, parameter :: &
-       Rho_       =  1,              SWHRho_   = 1, &
-       RhoUx_     =  2, Ux_ = 2,     SWHRhoUx_ = 2,  SWHUx_ = 2, &
-       RhoUy_     =  3, Uy_ = 3,     SWHRhoUy_ = 3,  SWHUy_ = 3, &
-       RhoUz_     =  4, Uz_ = 4,     SWHRhoUz_ = 4,  SWHUz_ = 4, &
-       Bx_        =  5, &
-       By_        =  6, &
-       Bz_        =  7, &
-       WaveFirst_ =  8, &
-       WaveLast_  =  9, &
-       Pe_        = 10, &
-       p_         = 11,              SWHP_     = 11
+       Rho_       = 1,                    SWHRho_   = 1, &
+       RhoUx_     = 2, Ux_ = 2,           SWHRhoUx_ = 2,  SWHUx_ = 2, &
+       RhoUy_     = 3, Uy_ = 3,           SWHRhoUy_ = 3,  SWHUy_ = 3, &
+       RhoUz_     = 4, Uz_ = 4,           SWHRhoUz_ = 4,  SWHUz_ = 4, &
+       Bx_        = 5, &
+       By_        = 6, &
+       Bz_        = 7, &
+       WaveFirst_ = 8, &
+       WaveLast_  = WaveFirst_+nWave-1, &
+       PuiFirst_  = WaveLast_+1, &
+       PuiLast_   = PuiFirst_+nPui-1, &
+       Pe_        = PuiLast_+1, &
+       p_         = Pe_+1,                SWHP_     = Pe_+1, &
+       Pu3Rho_    = p_+1, &
+       Pu3RhoUx_  = p_+2, Pu3Ux_ = p_+2, &
+       Pu3RhoUy_  = p_+3, Pu3Uy_ = p_+3, &
+       Pu3RhoUz_  = p_+4, Pu3Uz_ = p_+4, &
+       Pu3P_      = p_+5
 
   integer, parameter :: &
-       NeuRho_    = 12,              Pu3Rho_    = 12, &
-       NeuRhoUx_  = 13, NeuUx_ = 13, Pu3RhoUx_  = 13, Pu3Ux_ = 13, &
-       NeuRhoUy_  = 14, NeuUy_ = 14, Pu3RhoUy_  = 14, Pu3Uy_ = 14, &
-       NeuRhoUz_  = 15, NeuUz_ = 15, Pu3RhoUz_  = 15, Pu3Uz_ = 15, &
-       NeuP_      = 16,              Pu3P_      = 16, &
-       Ne2Rho_    = 17, &
-       Ne2RhoUx_  = 18, Ne2Ux_ = 18, &
-       Ne2RhoUy_  = 19, Ne2Uy_ = 19, &
-       Ne2RhoUz_  = 20, Ne2Uz_ = 20, &
-       Ne2P_      = 21, &
-       Ne3Rho_    = 22, &
-       Ne3RhoUx_  = 23, Ne3Ux_ = 23, &
-       Ne3RhoUy_  = 24, Ne3Uy_ = 24, &
-       Ne3RhoUz_  = 25, Ne3Uz_ = 25, &
-       Ne3P_      = 26, &
-       Ne4Rho_    = 27, &
-       Ne4RhoUx_  = 28, Ne4Ux_ = 28, &
-       Ne4RhoUy_  = 29, Ne4Uy_ = 29, &
-       Ne4RhoUz_  = 30, Ne4Uz_ = 30, &
-       Ne4P_      = 31, &
-       Energy_    = nVar+1,          SWHEnergy_ = nVar+1, &
-       NeuEnergy_ = nVar+2,          Pu3Energy_ = nVar+2, &
-       Ne2Energy_ = nVar+3, &
-       Ne3Energy_ = nVar+4, &
-       Ne4Energy_ = nVar+5
+       NeuRho_    = Pu3P_+1, &
+       NeuRhoUx_  = Pu3P_+2, NeuUx_ = Pu3P_+2, &
+       NeuRhoUy_  = Pu3P_+3, NeuUy_ = Pu3P_+3, &
+       NeuRhoUz_  = Pu3P_+4, NeuUz_ = Pu3P_+4, &
+       NeuP_      = Pu3P_+5, &
+       Ne2Rho_    = NeuP_+1, &
+       Ne2RhoUx_  = NeuP_+2, Ne2Ux_ = NeuP_+2, &
+       Ne2RhoUy_  = NeuP_+3, Ne2Uy_ = NeuP_+3, &
+       Ne2RhoUz_  = NeuP_+4, Ne2Uz_ = NeuP_+4, &
+       Ne2P_      = NeuP_+5, &
+       Ne3Rho_    = Ne2P_+1, &
+       Ne3RhoUx_  = Ne2P_+2, Ne3Ux_ = Ne2P_+2, &
+       Ne3RhoUy_  = Ne2P_+3, Ne3Uy_ = Ne2P_+3, &
+       Ne3RhoUz_  = Ne2P_+4, Ne3Uz_ = Ne2P_+4, &
+       Ne3P_      = Ne2P_+5, &
+       Ne4Rho_    = Ne3P_+1, &
+       Ne4RhoUx_  = Ne3P_+2, Ne4Ux_ = Ne3P_+2, &
+       Ne4RhoUy_  = Ne3P_+3, Ne4Uy_ = Ne3P_+3, &
+       Ne4RhoUz_  = Ne3P_+4, Ne4Uz_ = Ne3P_+4, &
+       Ne4P_      = Ne3P_+5, &
+       Energy_    = nVar+1,  SWHEnergy_ = nVar+1, &
+       Pu3Energy_ = nVar+2, &
+       NeuEnergy_ = nVar+3, &
+       Ne2Energy_ = nVar+4, &
+       Ne3Energy_ = nVar+5, &
+       Ne4Energy_ = nVar+6
 
   ! This allows to calculate RhoUx_ as RhoU_+x_ and so on.
   integer, parameter :: U_ = Ux_ - 1, RhoU_ = RhoUx_-1, B_ = Bx_-1
@@ -90,96 +108,84 @@ module ModVarIndexes
   ! These arrays are useful for multifluid
   integer, parameter :: &
        iRho_I(nFluid)   &
-       = [Rho_,   NeuRho_,    Ne2Rho_,   Ne3Rho_,   Ne4Rho_   ], &
+       = [ Rho_,   Pu3Rho_,   NeuRho_,    Ne2Rho_,   Ne3Rho_,   Ne4Rho_   ], &
        iRhoUx_I(nFluid) &
-       = [RhoUx_, NeuRhoUx_,  Ne2RhoUx_, Ne3RhoUx_, Ne4RhoUx_ ], &
+       = [ RhoUx_, Pu3RhoUx_, NeuRhoUx_,  Ne2RhoUx_, Ne3RhoUx_, Ne4RhoUx_ ], &
        iRhoUy_I(nFluid) &
-       = [RhoUy_, NeuRhoUy_,  Ne2RhoUy_, Ne3RhoUy_, Ne4RhoUy_ ], &
+       = [ RhoUy_, Pu3RhoUy_, NeuRhoUy_,  Ne2RhoUy_, Ne3RhoUy_, Ne4RhoUy_ ], &
        iRhoUz_I(nFluid) &
-       = [RhoUz_, NeuRhoUz_,  Ne2RhoUz_, Ne3RhoUz_, Ne4RhoUz_ ], &
+       = [ RhoUz_, Pu3RhoUz_, NeuRhoUz_,  Ne2RhoUz_, Ne3RhoUz_, Ne4RhoUz_ ], &
        iP_I(nFluid)     &
-       = [p_, NeuP_, Ne2P_, Ne3P_, Ne4P_]
+       = [ P_,     Pu3P_,     NeuP_,      Ne2P_,     Ne3P_,     Ne4P_     ]
+
+  integer, parameter :: iPparIon_I(IonFirst_:IonLast_) = [1,2]
 
   ! The default values for the state variables:
   ! Variables which are physically positive should be set to 1,
   ! variables that can be positive or negative should be set to 0:
   real, parameter :: DefaultState_V(nVar+nFluid) = [ &
-       1.0, & ! Rho_
-       0.0, & ! RhoUx_
-       0.0, & ! RhoUy_
-       0.0, & ! RhoUz_
-       0.0, & ! Bx_
-       0.0, & ! By_
-       0.0, & ! Bz_
+       1.0,           & ! SWHRho_
+       0.0, 0.0, 0.0, & ! SWHRhoUx_ .. SWHRhoUz_
+       0.0, 0.0, 0.0, & ! Bx_ .. Bz_
        (1.0, iWave=WaveFirst_,WaveLast_), &
-       1.0, & ! Pe_
-       1.0, & ! p_
-       1.0, & ! NeuRho_
-       0.0, & ! NeuRhoUx_
-       0.0, & ! NeuRhoUy_
-       0.0, & ! NeuRhoUz_
-       1.0, & ! NeuP_
-       1.0, & ! Ne2Rho_
-       0.0, & ! Ne2RhoUx_
-       0.0, & ! Ne2RhoUy_
-       0.0, & ! Ne2RhoUz_
-       1.0, & ! Ne2P_
-       1.0, & ! Ne3Rho_
-       0.0, & ! Ne3RhoUx_
-       0.0, & ! Ne3RhoUy_
-       0.0, & ! Ne3RhoUz_
-       1.0, & ! Ne3P_
-       1.0, & ! Ne4Rho_
-       0.0, & ! Ne4RhoUx_
-       0.0, & ! Ne4RhoUy_
-       0.0, & ! Ne4RhoUz_
-       1.0, & ! Ne4P_
-       1.0, & ! Energy_
-       1.0, & ! NeuEnergy_
-       1.0, & ! Neu2Energy_
-       1.0, & ! Neu3Energy_
-       1.0 ]  ! Neu4Energy__
+       (1.0, iPui=PuiFirst_,PuiLast_), &
+       1.0,           & ! Pe_
+       1.0,           & ! SWHP_
+       1.0,           & ! Pu3Rho_
+       0.0, 0.0, 0.0, & ! Pu3RhoUx_ .. Pu3RhoUz_
+       1.0,           & ! Pu3P_
+       1.0,           & ! NeuRho_
+       0.0, 0.0, 0.0, & ! NeuRhoUx_ .. NeuRhoUz_
+       1.0,           & ! NeuP_
+       1.0,           & ! Ne2Rho_
+       0.0, 0.0, 0.0, & ! Ne2RhoUx_ .. Ne2RhoUz_
+       1.0,           & ! Ne2P_
+       1.0,           & ! Ne3Rho_
+       0.0, 0.0, 0.0, & ! Ne3RhoUx_ .. Ne3RhoUz_
+       1.0,           & ! Ne3P_
+       1.0,           & ! Ne4Rho_
+       0.0, 0.0, 0.0, & ! Ne4RhoUx_ .. Ne4RhoUz_
+       1.0,           & ! Ne4P_
+       1.0,           & ! SWHEnergy_
+       1.0,           & ! Pu3Energy_
+       1.0,           & ! NeuEnergy_
+       1.0,           & ! Neu2Energy_
+       1.0,           & ! Neu3Energy_
+       1.0            ] ! Neu4Energy_
 
   ! The names of the variables used in i/o
   character(len=6) :: NameVar_V(nVar+nFluid) = [ &
-       'Rho   ', & ! Rho_
-       'Mx    ', & ! RhoUx_
-       'My    ', & ! RhoUy_
-       'Mz    ', & ! RhoUz_
-       'Bx    ', & ! Bx_
-       'By    ', & ! By_
-       'Bz    ', & ! Bz_
+       'Rho   ', & ! SWHRho_
+       'Mx    ', 'My    ', 'Mz    ', & ! RhoUx_ RhoUz_
+       'Bx    ', 'By    ', 'Bz    ', & ! Bx_  Bz_
        ('I??   ', iWave=WaveFirst_,WaveLast_), &
-       'Pe    ', & ! Pe_
-       'p     ', & ! p_
-       'NeuRho', & ! NeuRho_
-       'NeuMx ', & ! NeuRhoUx_
-       'NeuMy ', & ! NeuRhoUy_
-       'NeuMz ', & ! NeuRhoUz_
-       'NeuP  ', & ! NeuP_
-       'Ne2Rho', & ! Ne2Rho_
-       'Ne2Mx ', & ! Ne2RhoUx_
-       'Ne2My ', & ! Ne2RhoUy_
-       'Ne2Mz ', & ! Ne2RhoUz_
-       'Ne2P  ', & ! Ne2P_
-       'Ne3Rho', & ! Ne3Rho_
-       'Ne3Mx ', & ! Ne3RhoUx_
-       'Ne3My ', & ! Ne3RhoUy_
-       'Ne3Mz ', & ! Ne3RhoUz_
-       'Ne3P  ', & ! Ne3P_
-       'Ne4Rho', & ! Ne4Rho_
-       'Ne4Mx ', & ! Ne4RhoUx_
-       'Ne4My ', & ! Ne4RhoUy_
-       'Ne4Mz ', & ! Ne4RhoUz_
-       'Ne4P  ', & ! Ne4P_
-       'e     ', & ! Energy_
-       'NeuE  ', & ! NeuEnergy_
-       'Ne2E  ', & ! Ne2Energy_
-       'Ne3E  ', & ! Ne3Energy_
-       'Ne4E  ' ]  ! Ne4Energy_
+       ('F??   ', iPui=PuiFirst_,PuiLast_), &
+       'Pe    ',                     & ! Pe_
+       'P     ',                     & ! p_
+       'Pu3Rho',                     & ! Pu3Rho_
+       'Pu3Mx ', 'Pu3My ', 'Pu3Mz ', & ! Pu3RhoUx_ Pu3RhoUz_
+       'Pu3P  ',                     & ! Pu3P_
+       'NeuRho',                     & ! NeuRho_
+       'NeuMx ', 'NeuMy ', 'NeuMz ', & ! NeuRhoUx_ NeuRhoUz_
+       'NeuP  ',                     & ! NeuP_
+       'Ne2Rho',                     & ! Ne2Rho_
+       'Ne2Mx ', 'Ne2My ', 'Ne2Mz ', & ! Ne2RhoUx_ Ne2RhoUz_
+       'Ne2P  ',                     & ! Ne2P_
+       'Ne3Rho',                     & ! Ne3Rho_
+       'Ne3Mx ', 'Ne3My ', 'Ne3Mz ', & ! Ne3RhoUx_ Ne3RhoUz_
+       'Ne3P  ',                     & ! Ne3P_
+       'Ne4Rho',                     & ! Ne4Rho_
+       'Ne4Mx ', 'Ne4My ', 'Ne4Mz ', & ! Ne4RhoUx_ Ne4RhoUz_
+       'Ne4P  ',                     & ! Ne4P_
+       'E     ',                     & ! Energy_
+       'Pu3E  ',                     & ! Pu3Energy_
+       'NeuE  ',                     & ! NeuEnergy_
+       'Ne2E  ',                     & ! Ne2Energy_
+       'Ne3E  ',                     & ! Ne3Energy_
+       'Ne4E  '                      ] ! Ne4Energy_
 
   ! There are no extra scalars
-  integer, parameter :: ScalarFirst_ = 2, ScalarLast_ = 1
+  integer, parameter :: ScalarFirst_ = PuiFirst_, ScalarLast_ = PuiLast_
 
 end module ModVarIndexes
 !==============================================================================
