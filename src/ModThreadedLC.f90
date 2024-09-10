2b38fc162f40cc3bfc1d1dae1eadc3910977cb58
Igor V. Sokolov <igorsok@umich.edu>
2024-09-10 16:49:25 -0400

    Looks like there was a bug, potentially affecting LOS images, though no test results change after fix

--- src/ModThreadedLC.f90
+++ src/ModThreadedLC.f90
@@ -1197,7 +1197,7 @@ contains
 
     call timing_start('set_thread_bc')
     ! Start from floating boundary values
-    do k = 1, nK; do j = 1, nJ; do i = 1 - nGhost, 0
+    do k = MinK, MaxK; do j = MinJ, MaxJ; do i = 1 - nGhost, 0
        State_VG(:, i,j,k) = State_VG(:,1, j, k)
     end do; end do; end do
 
