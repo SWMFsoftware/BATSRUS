78ea188a6142ace9f6fd23a2ac931c125f3aafd4
Yuxi Chen <yuxichen@umich.edu>
2025-01-29 14:16:22 -0500

    Declare IonMassPerCharge on GPU.

--- src/ModPhysics.f90
+++ src/ModPhysics.f90
@@ -81,7 +81,8 @@ module ModPhysics
   real:: ElectronPressureRatio    = 0.0
   real:: PePerPtotal              = 0.0
   real:: IonMassPerCharge         = 1.0
-  !$acc declare create(AverageIonCharge, PePerPtotal)
+  !$acc declare create(AverageIonCharge, PePerPtotal, IonMassPerCharge)
+  
 
   ! Quantities for reduced mass and collision frequencies
   real:: MassIonElectron_I(nIonFluid+1)
